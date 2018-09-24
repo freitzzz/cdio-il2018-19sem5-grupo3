47805da888b95891d6471aed8a028c0e71dbaaf5
João Freitas <1160907@isep.ipp.pt>
2018-09-24 22:23:07 +0100

    Added new functionality to DTO interface which allows object mapping
    by their reference keys
    
    see #5
    
    [framework]

--- framework/dto/DTO.cs
+++ framework/dto/DTO.cs
@@ -1,6 +1,14 @@
 namespace framework.dto{
     public interface DTO{
         /// <summary>
+        /// Puts an object into the DTO with a certain key which is mapping it
+        /// </summary>
+        /// <param name="key">String with the object key reference</param>
+        /// <param name="data">Object with the data being inserted in the DTO</param>
+        /// <returns>boolean true if the object was inserted with success, false if not</returns>
+        bool put(string key,object data);
+        
+        /// <summary>
         /// Returns an object which is being held on the DTO by a certain key
         /// </summary>
         /// <param name="key">String with the key which references the object</param>
