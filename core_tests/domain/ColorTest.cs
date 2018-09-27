56525db50a6d4c1732f7221482d1a3a3d35a4458
Ricardina <1160941@isep.ipp.pt>
2018-09-27 09:24:01 +0100

    [us1100]Unit Test Elaboration for the color class

--- core_tests/domain/ColorTest.cs
+++ core_tests/domain/ColorTest.cs
@@ -7,6 +7,25 @@ namespace dotnet_example_unittests
 {
     public class TodoItemTest
     {
+
+        [Fact]
+        public void ensureConstructorDetectsNegativeValue()
+        {
+
+            Action act = () => Color.valueOf("test", 5, -10, 1, 2);
+
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void ensureConstructorDetectsOverValue()
+        {
+
+            Action act = () => Color.valueOf("test", 5, 255, 1, 2);
+
+            Assert.Throws<ArgumentException>(act);
+        }
+
         [Fact]
         public void EnsureNamesAreEqual()
         {
@@ -37,6 +56,13 @@ namespace dotnet_example_unittests
 
         }
 
-        
+        [Fact]
+        public void testToString()
+        {
+            Color instance = Color.valueOf("0mg Cholestherol", 255, 1, 2, 66);
+            Color other = Color.valueOf("0mg Cholestherol", 255, 1, 2, 66);
+
+            Assert.Equal(instance.ToString(), other.ToString());
+        }
     }
 }
