a635ba76ae7eff269236be8aab858078cf67ecbb
João Freitas <1160907@isep.ipp.pt>
2018-09-27 14:17:23 +0100

    DDD Repository now implements DataRepository
    see #5

--- support/domain/ddd/Repository.cs
+++ support/domain/ddd/Repository.cs
@@ -1,4 +1,5 @@
 using support.domain.ddd;
+using support.persistence.repositories;
 using System;
 namespace support.domain.ddd{
     /// <summary>
@@ -11,35 +12,7 @@ namespace support.domain.ddd{
     /// <typeparam name="ID">Generic-Type of the entity persistence identifier</typeparam>
     //TODO: We have a problem on generic identifiers
     //C# doesn't have an "anonymous generic tag" so we can't do stuff like AggregateRoot<?>
-    public interface Repository<E,ID,EID> where E:AggregateRoot<EID>{
-        /// <summary>
-        /// Saves an entity on the repository
-        /// </summary>
-        /// <param name="entity">E with the entity being saved on the repository</param>
-        /// <returns>E with the saved entity</returns>
-        E save(E entity);
-
-        /// <summary>
-        /// Updates the state of an entity on the repository
-        /// </summary>
-        /// <param name="entity">E with the entity being updated on the repository</param>
-        /// <returns>E with the updated entity</returns>
-        E update(E entity);
-        
-        /// <summary>
-        /// Removes an entity from the repository
-        /// </summary>
-        /// <param name="entity">E with the entity being removed from the repository</param>
-        /// <returns>E with the removed entity</returns>
-        E remove(E entity);
-        
-        /// <summary>
-        /// Finds an entity based on persistence identifier
-        /// </summary>
-        /// <param name="entityPersistenceID">ID with the entity persistence identifier</param>
-        /// <returns>E with the entity which is represented by a certain persistence identifier</returns>
-        E find(ID entityPersistenceID);
-
+    public interface Repository<E,ID,EID>:DataRepository<E,ID> where E:AggregateRoot<EID>{
         /// <summary>
         /// Finds an entity based on its identitifer
         /// </summary>
