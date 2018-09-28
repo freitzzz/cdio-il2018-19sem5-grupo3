fa7d8ddf06c8cee6e8e3a5f373148944d8b0b8fb
António Sousa <1161371@isep.ipp.pt>
2018-09-28 15:17:49 +0100

    Fix repository interfaces' incorrect database identifier parameters [re #4, re #15].

--- core/persistence/MaterialRepository.cs
+++ core/persistence/MaterialRepository.cs
@@ -3,13 +3,9 @@ using support.domain.ddd;
 using support.persistence.repositories;
 
 namespace core.persistence
-{
-    /** 
-    <summary>
-        Interface that represents the repository functionalities for Material entities.
-    </summary>
-    <typeparam name = "Material">Generic-Type of the repository entity</typeparam>
-    <typeparam name = "string">Generic-Type of the repository persistence ID</typeparam>
-        */
-    public interface MaterialRepository : Repository<Material, string, string>, DataRepository<Material, string> {}
+{ 
+    ///<summary>Interface that represents the repository functionalities for Material entities.</summary>
+    ///<typeparam name = "Material">Generic-Type of the repository entity</typeparam>
+    ///<typeparam name = "string">Generic-Type of the repository persistence ID</typeparam>
+    public interface MaterialRepository : Repository<Material, long, string>, DataRepository<Material, long> {}
 }
