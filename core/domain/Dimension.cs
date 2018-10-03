b946da564d7cf2c6e3f2dbdae73e8ced6423e3c8
Gil Durão <1160936@isep.ipp.pt>
2018-10-03 12:38:52 +0100

    Dimension interface now implements DTOAble.
    see #8
    see #15

--- core/domain/Dimension.cs
+++ core/domain/Dimension.cs
@@ -1,11 +1,13 @@
 using System;
+using core.dto;
+using support.dto;
 
 namespace core.domain
 {
     /// <summary>
     /// Represents a Product dimension
     /// </summary>
-    public interface Dimension
+    public interface Dimension : DTOAble<DimensionDTO>
     {
 
     }
