09356d8845d68d2b874c48e3b6a432d968ed767c
António Sousa <1161371@isep.ipp.pt>
2018-10-03 18:04:43 +0100

    Add DatabaseConfiguration class and DatabaseProviders enum used for setting the currently used relational database [re #16, re #18].

---
+++ backend/config/DatabaseProviders.cs
@@ -0,0 +1,14 @@
+namespace backend.config
+{
+    /// <summary>
+    /// Enum representing the available database providers.
+    /// </summary>
+    public enum DatabaseProviders
+    {
+        InMemory = 0,
+        MySQL = 1,
+        SQLServer = 2,
+        SQLite = 3
+
+    }
+}
