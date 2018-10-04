52455b769559f77847db55e80f043d7fcedc7b69
Ricardina <1160941@isep.ipp.pt>
2018-10-04 18:51:07 +0100

    [Configured Product DTO] Finalization of class

--- core/dto/CustomizedProductDTO.cs
+++ core/dto/CustomizedProductDTO.cs
@@ -1,18 +1,70 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
 using core.domain;
 using support.dto;
 
 namespace core.dto
 {
-    
+
     /// <summary>
     /// DTO that represents a Configured Product instance
     /// </summary>
     [DataContract]
-    public class CustomizedProductDTO : DTO,DTOParseable<CustomizedProduct,CustomizedProductDTO>
+    public class CustomizedProductDTO : DTO, DTOParseable<CustomizedProduct, CustomizedProductDTO>
     {
-        public CustomizedProduct toEntity() {
-            throw new System.NotImplementedException();
+        // <summary>
+        /// Finish's database identifier.
+        /// </summary>
+        /// <value>Gets/sets the value of the database identifier field.</value>
+        [DataMember]
+        public long id { get; set; }
+
+
+        /**
+        <summary>
+            String with the ConfiguredProduct's reference.
+        </summary>
+        */
+        public string reference { get; set; }
+
+        /** 
+        <summary>
+            String with the ConfiguredProduct's designation.
+        </summary>
+        */
+        public string designation { get; set; }
+
+
+        /**
+        <summary>
+            The CustomizedProduct Customized Material
+        </summary>
+         */
+        private readonly CustomizedMaterial customizedMaterial;
+
+        /**
+        <summary>
+            The CustomizedProduct Customized Dimensions
+        </summary>
+         */
+        private readonly CustomizedDimensions customizedDimensions;
+
+        /**
+        <summary>
+            List of Products from CustomizedProduct
+        </summary>
+         */
+        private List<Product> list;
+
+        public  CustomizedProduct toEntity()
+        {
+        
+            CustomizedProduct instanceFromDTO = CustomizedProduct.valueOf(reference, designation, customizedMaterial, customizedDimensions, list);
+            instanceFromDTO.Id = this.id;
+            return instanceFromDTO;
+            
         }
+
     }
 }
