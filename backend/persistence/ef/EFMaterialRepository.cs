1d77339df42fe4434a1cb6550c99370c4ac3d874
João Freitas <1160907@isep.ipp.pt>
2018-10-05 00:22:19 +0100

    MaterialRepository now allows the retrieval of an
    enumerable of materials through their persistence identifiers
    
    see #1
    
    see #3
    
    see #16

--- backend/persistence/ef/EFMaterialRepository.cs
+++ backend/persistence/ef/EFMaterialRepository.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
 using core.domain;
+using core.dto;
 using core.persistence;
 
 namespace backend.persistence.ef
 {
     public class EFMaterialRepository : EFBaseRepository<Material, long, string>, MaterialRepository
     {
-        public EFMaterialRepository(MyCContext dbContext) : base(dbContext)
+        public EFMaterialRepository(MyCContext dbContext) : base(dbContext){}
+
+        /// <summary>
+        /// Returns an enumerable of materials based on their identifiers
+        /// </summary>
+        /// <param name="materialsDTO">IEnumerable with the materials identifiers information</param>
+        /// <returns>IEnumerable with the materials based on their identifiers</returns>
+        public IEnumerable<Material> getMaterialsByIDS(IEnumerable<MaterialDTO> materialsDTO)
         {
+            List<long> materialsIDS=new List<long>();
+            foreach(MaterialDTO materialDTO in materialsDTO)materialsIDS.Add(materialDTO.id);
+            return (from material in base.dbContext.Set<Material>()
+                        where materialsIDS.Contains(material.Id)
+                        select material).ToList();
         }
     }
 }
--- core/persistence/MaterialRepository.cs
+++ core/persistence/MaterialRepository.cs
@@ -1,11 +1,20 @@
 using core.domain;
+using core.dto;
 using support.domain.ddd;
 using support.persistence.repositories;
+using System.Collections.Generic;
 
 namespace core.persistence
 { 
     ///<summary>Interface that represents the repository functionalities for Material entities.</summary>
     ///<typeparam name = "Material">Generic-Type of the repository entity</typeparam>
     ///<typeparam name = "string">Generic-Type of the repository persistence ID</typeparam>
-    public interface MaterialRepository : Repository<Material, long, string>{}
+    public interface MaterialRepository : Repository<Material, long, string>{
+        /// <summary>
+        /// Returns an enumerable of materials based on their identifiers
+        /// </summary>
+        /// <param name="materialsDTO">IEnumerable with the materials identifiers information</param>
+        /// <returns>IEnumerable with the materials based on their identifiers</returns>
+        IEnumerable<Material> getMaterialsByIDS(IEnumerable<MaterialDTO> materialsDTO);
+    }
 }
