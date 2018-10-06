35000ee5e179adac8386298f35417d832be24ec1
António Sousa <1161371@isep.ipp.pt>
2018-10-06 16:21:08 +0100

    Change ProductCategory's hard deletion to soft deletion [re #3].

--- backend/persistence/ef/EFProductCategoryRepository.cs
+++ backend/persistence/ef/EFProductCategoryRepository.cs
@@ -2,13 +2,37 @@ using System.Collections.Generic;
 using core.domain;
 using core.persistence;
 using support.persistence.repositories;
+using System.Linq;
 
 namespace backend.persistence.ef
 {
     public class EFProductCategoryRepository : EFBaseRepository<ProductCategory, long, string>, ProductCategoryRepository
     {
-        public EFProductCategoryRepository(MyCContext dbContext) : base(dbContext)
+        public EFProductCategoryRepository(MyCContext dbContext) : base(dbContext) { }
+
+
+        public override IEnumerable<ProductCategory> findAll()
         {
+            return dbContext.ProductCategory.Where(c => c.active).ToList();
         }
+
+        public override ProductCategory find(string entityID)
+        {
+            return dbContext.ProductCategory.Where(c => c.active).Where(c => c.sameAs(entityID)).SingleOrDefault();
+        }
+
+        public override ProductCategory find(long entityPersistenceID)
+        {
+            return dbContext.ProductCategory.Where(c => c.active).Where(c => c.Id == entityPersistenceID).SingleOrDefault();
+        }
+
+        public override ProductCategory remove(ProductCategory entity){
+
+            entity.deactivate();
+            dbContext.SaveChanges();
+
+            return entity;
+        }
+
     }
 }
