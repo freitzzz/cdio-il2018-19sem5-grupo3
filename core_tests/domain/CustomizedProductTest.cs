fd334b888d75364603f694509c24df72a0422f6f
Ricardina <1160941@isep.ipp.pt>
2018-10-06 12:19:52 +0100

    [UNIT Test] The following tests were added: ensureCustomizedDimensionsIsNull & ensureProductIsInvalid

--- core_tests/domain/CustomizedProductTest.cs
+++ core_tests/domain/CustomizedProductTest.cs
@@ -10,7 +10,7 @@ namespace core_tests.domain
     public class CustomizedProductTest
     {
         [Fact]
-        public void ensureCustomizedProductCategoryIsNull()
+        public void ensureCustomizedMaterialIsNull()
         {
             var category = new ProductCategory("Drawers");
 
@@ -71,5 +71,101 @@ namespace core_tests.domain
 
         }
 
+        
+
+        [Fact]
+        public void ensureCustomizedDimensionsIsNull()
+        {
+            var category = new ProductCategory("Drawers");
+
+            //Creating Dimensions
+            List<Double> values2 = new List<Double>();
+
+            values2.Add(500.0); //Width
+
+            DiscreteDimensionInterval d2 = DiscreteDimensionInterval.valueOf(values2);
+
+            List<Dimension> valuest = new List<Dimension>();
+            valuest.Add(d2);
+
+            IEnumerable<Dimension> heightValues = valuest;
+            IEnumerable<Dimension> widthValues = valuest;
+            IEnumerable<Dimension> depthValues = valuest;
+
+            //Creating a material
+            string reference = "1160912";
+            string designation = "FR E SH A VOCA DO";
+
+            List<Color> colors = new List<Color>();
+            Color color = Color.valueOf("AND READ-ER-BIBLE", 1, 2, 3, 0);
+            colors.Add(color);
+
+            List<Finish> finishes = new List<Finish>();
+            Finish finish = Finish.valueOf("Amém");
+            finishes.Add(finish);
+
+            Material material = new Material(reference, designation, colors, finishes);
+            List<Material> materials = new List<Material>();
+            materials.Add(material);
+
+            IEnumerable<Material> matsList = materials;
+
+            Product product = new Product("#666", "Shelf", category, matsList, heightValues, widthValues, depthValues);
+   
+            //Customized Material
+            Color color1 = Color.valueOf("Azul",1,1,1,1);
+            Finish finish2 = Finish.valueOf("Acabamento polido");
+            CustomizedMaterial custMaterial1 = CustomizedMaterial.valueOf(color1,finish2);
+            //CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(1.2, 1.5, 20.3);
+            string condition = ";";
+            try
+            {
+                CustomizedProduct cp = CustomizedProduct.valueOf("#666", "Shelf", custMaterial1, null, product);
+
+            }
+            catch (ArgumentException) // The argument was thrown
+            {
+                condition = "True";
+
+            }
+            catch (NullReferenceException)
+            {
+                condition = "True";
+
+            }
+            Assert.True(condition.Equals("True"));
+
+
+        }
+
+        [Fact]
+        public void ensureProductIsInvalid()
+        {
+           
+            //Customized Material
+            Color color1 = Color.valueOf("Azul",1,1,1,1);
+            Finish finish2 = Finish.valueOf("Acabamento polido");
+            CustomizedMaterial custMaterial1 = CustomizedMaterial.valueOf(color1,finish2);
+            CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(1.2, 1.5, 20.3);
+            string condition = ";";
+            try
+            {
+                CustomizedProduct cp = CustomizedProduct.valueOf("#666", "Shelf", custMaterial1, customizedDimensions, null);
+
+            }
+            catch (ArgumentException) // The argument was thrown
+            {
+                condition = "True";
+
+            }
+            catch (NullReferenceException)
+            {
+                condition = "True";
+
+            }
+            Assert.True(condition.Equals("True"));
+        }
+
+     
     }
 }
