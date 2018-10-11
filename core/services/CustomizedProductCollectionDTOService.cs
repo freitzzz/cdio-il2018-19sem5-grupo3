96f50c1a0bdd3485e9052703eca7e2c3523cd820
João Freitas <1160907@isep.ipp.pt>
2018-10-11 15:50:19 +0100

    Added CustomizedProductCollectionDTOService class which
    aims to service the transformation of a dto into an entity of customized
    products collection
    
    see #1

---
+++ core/services/CustomizedProductCollectionDTOService.cs
@@ -0,0 +1,25 @@
+using core.domain;
+using core.dto;
+using core.persistence;
+using System.Collections.Generic;
+
+namespace core.services{
+
+    /// <summary>
+    /// Service class that helps the transformation of CustomizedProductCollectionDTO into CustomizedProductCollection since some information needs to be accessed on the persistence context
+    /// </summary>
+    public sealed class CustomizedProductCollectionDTOService{
+        /// <summary>
+        /// Transforms a customized product collection dto into a collection of customized products via service
+        /// </summary>
+        /// <param name="customizedProductCollectionDTO">CustomizedProductCollectionDTO with the customized product collection dto being transformed</param>
+        /// <returns>CustomizedProductCollection with the collection of customized products transformed from the dto</returns>
+        public CustomizedProductCollection transform(CustomizedProductCollectionDTO customizedProductCollectionDTO){
+            string name=customizedProductCollectionDTO.name;
+            IEnumerable<CustomizedProduct> customizedProducts=PersistenceContext.repositories().createCustomizedProductRepository().findCustomizedProductsByTheirPIDS(customizedProductCollectionDTO.customizedProducts);
+            if(customizedProducts==null)
+                return new CustomizedProductCollection(name);
+            return new CustomizedProductCollection(name,customizedProducts);
+        }
+    }
+}
