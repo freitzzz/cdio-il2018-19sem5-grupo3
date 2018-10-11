38b6e3c22abe248555d0eda7473bfdd605371cd5
António Sousa <1161371@isep.ipp.pt>
2018-10-12 00:38:53 +0100

    Add measurement value conversion to toEntity() [re #25].

--- core/dto/ContinuousDimensionIntervalDTO.cs
+++ core/dto/ContinuousDimensionIntervalDTO.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 using System.Collections.Generic;
 using core.domain;
 using support.dto;
+using core.services;
 
 namespace core.dto
 {
@@ -17,21 +18,21 @@ namespace core.dto
         /// Minimum value of the interval
         /// </summary>
         /// <value>Get/Set of the value</value>
-        [DataMember (Order = 2)]
+        [DataMember(Order = 2)]
         public double minValue { get; set; }
 
         /// <summary>
         /// Maximum value of the interval
         /// </summary>
         /// <value>Get/Set of the value</value>
-        [DataMember (Order = 3)]
+        [DataMember(Order = 3)]
         public double maxValue { get; set; }
 
         /// <summary>
         /// Increment value of the interval
         /// </summary>
         /// <value>Get/Set of the value</value>
-        [DataMember (Order = 4)]
+        [DataMember(Order = 4)]
         public double increment { get; set; }
 
         /// <summary>
@@ -40,8 +41,13 @@ namespace core.dto
         /// <returns>ContinuousDimensionInterval instance</returns>
         public override Dimension toEntity()
         {
+            double minValue = MeasurementUnitService.convertFromUnit(this.minValue, unit);
+            double maxValue = MeasurementUnitService.convertFromUnit(this.maxValue, unit);
+            double increment = MeasurementUnitService.convertFromUnit(this.increment, unit);
+
             ContinuousDimensionInterval instanceFromDTO = ContinuousDimensionInterval.valueOf(minValue, maxValue, increment);
             instanceFromDTO.Id = id;
+            
             return instanceFromDTO;
         }
     }
