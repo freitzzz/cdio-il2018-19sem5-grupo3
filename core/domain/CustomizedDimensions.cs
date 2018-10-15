9476d350af7f054a251a28a139bc8de54c3029b8
Gil Durão <1160936@isep.ipp.pt>
2018-10-15 22:53:53 +0100

    Adds CustomizedDimensionsDTO.
    see #1
    see #27

--- core/domain/CustomizedDimensions.cs
+++ core/domain/CustomizedDimensions.cs
@@ -1,81 +1,78 @@
 using System;
 using System.Collections.Generic;
+using core.dto;
 using support.domain.ddd;
+using support.dto;
+
 namespace core.domain
 {
-    /**
-    <summary>
-        Class that represents a Customized Dimensions.
-        <br> Customized Dimensions is value object;
-    </summary>
-    */
-    public class CustomizedDimensions : ValueObject
+
+    ///<summary>
+    ///Class that represents a Customized Dimensions.
+    ///<br> Customized Dimensions is value object;
+    ///</summary>
+    public class CustomizedDimensions : ValueObject, DTOAble<CustomizedDimensionsDTO>
     {
-       
-        /// <summary>
-        /// Constant that represents the message that occurs if the value is NaN
-        /// </summary>
+
+        ///<summary>
+        ///Constant that represents the message that occurs if the value is NaN
+        ///</summary>
         private const string VALUE_IS_NAN_REFERENCE = "Dimension value has to be a number";
 
-        /// <summary>
-        /// Constant that represents the message that occurs if the value is infinity
-        /// </summary>
+        ///<summary>
+        ///Constant that represents the message that occurs if the value is infinity
+        ///</summary>
         private const string VALUE_IS_INFINITY_REFERENCE = "Dimension value can't be infinity";
 
-        /// <summary>
-        /// Constant that represents the message that occurs if the value is negative
-        /// </summary>
+        ///<summary>
+        ///Constant that represents the message that occurs if the value is negative
+        ///</summary>
         private const string NEGATIVE_VALUE_REFERENCE = "Dimension value can't be negative";
 
-        /// <summary>
-        /// Database identifier.
-        /// </summary>
-        /// <value></value>
-        public long Id {get; internal set;}
-/**
-        <summary>
-            The CustomizedDimensions's height.
-        </summary>
-         */
-        public double height {get; protected set;}
-        /**
-        <summary>
-            The CustomizedDimensions's width.
-        </summary>
-         */
-        public double width {get; protected set;}
-        /**
-        <summary>
-            The CustomizedDimensions's depth.
-        </summary>
-         */
-        public double depth {get; protected set;}
+        ///<summary>
+        ///Database identifier.
+        ///</summary>
+        ///<value></value>
+        public long Id { get; internal set; }
 
-        /// <summary>
-        /// Empty constructor for ORM.
-        /// </summary>
-        protected CustomizedDimensions(){}
+        ///<summary>
+        ///The CustomizedDimensions's height.
+        ///</summary>
+        public double height { get; protected set; }
 
-        /**
-        <summary>
-            Builds a new instance of CustomizedDimensions, receiving its height, width and depth.
-        </summary>
-        <param name = "height">double with the new CustomizedDimensions's height</param>
-        <param name = "width">double with the new CustomizedDimensions's width</param>
-        <param name = "depth">double with the new CustomizedDimensions's depth</param>
-         */
+        ///<summary>
+        ///The CustomizedDimensions's width.
+        ///</summary>
+        public double width { get; protected set; }
+
+        ///<summary>
+        ///The CustomizedDimensions's depth.
+        ///</summary>
+        public double depth { get; protected set; }
+
+        ///<summary>
+        ///Empty constructor for ORM.
+        ///</summary>
+        protected CustomizedDimensions() { }
+
+
+        ///<summary>
+        /// Builds a new instance of CustomizedDimensions, receiving its height, width and depth.
+        ///</summary>
+        ///<param name = "height">double with the new CustomizedDimensions's height</param>
+        ///<param name = "width">double with the new CustomizedDimensions's width</param>
+        ///<param name = "depth">double with the new CustomizedDimensions's depth</param>
         public static CustomizedDimensions valueOf(double height, double width, double depth)
         {
             return new CustomizedDimensions(height, width, depth);
         }
-        /**
-        <summary>
-            Builds a new instance of CustomizedDimensions, receiving its height, width and depth.
-        </summary>
-        <param name = "height">double with the new CustomizedDimensions's height</param>
-        <param name = "width">double with the new CustomizedDimensions's width</param>
-        <param name = "depth">double with the new CustomizedDimensions's depth</param>
-         */
+
+        ///<summary>
+        /// Builds a new instance of CustomizedDimensions, receiving its height, width and depth.
+        ///</summary>
+        ///<param name = "height">double with the new CustomizedDimensions's height</param>
+        ///<param name = "width">double with the new CustomizedDimensions's width</param>
+        ///<param name = "depth">double with the new CustomizedDimensions's depth</param>
         private CustomizedDimensions(double height, double width, double depth)
         {
             checkCustomizedDimensions(height, width, depth);
@@ -83,54 +80,51 @@ namespace core.domain
             this.width = width;
             this.depth = depth;
         }
-        /**
-        <summary>
-            Checks if the CustomizedDimensions's height, width and  depth are valid.
-        </summary>
-        <param name = "height">double with the new CustomizedDimensions's height</param>
-        <param name = "width">double with the new CustomizedDimensions's width</param>
-        <param name = "depth">double with the new CustomizedDimensions's depth</param>
-        */
+
+        ///<summary>
+        ///Checks if the CustomizedDimensions's height, width and  depth are valid.
+        ///</summary>
+        ///<param name = "height">double with the new CustomizedDimensions's height</param>
+        ///<param name = "width">double with the new CustomizedDimensions's width</param>
+        ///<param name = "depth">double with the new CustomizedDimensions's depth</param>
         private void checkCustomizedDimensions(double height, double width, double depth)
         {
             if (Double.IsNaN(height)) throw new ArgumentException(VALUE_IS_NAN_REFERENCE);
-            if (Double.IsInfinity(height))throw new ArgumentException(VALUE_IS_INFINITY_REFERENCE);
-            if (height < 0)throw new ArgumentException(NEGATIVE_VALUE_REFERENCE);
-            if (Double.IsNaN(width))throw new ArgumentException(VALUE_IS_NAN_REFERENCE);
+            if (Double.IsInfinity(height)) throw new ArgumentException(VALUE_IS_INFINITY_REFERENCE);
+            if (height < 0) throw new ArgumentException(NEGATIVE_VALUE_REFERENCE);
+            if (Double.IsNaN(width)) throw new ArgumentException(VALUE_IS_NAN_REFERENCE);
             if (Double.IsInfinity(width)) throw new ArgumentException(VALUE_IS_INFINITY_REFERENCE);
             if (width < 0) throw new ArgumentException(NEGATIVE_VALUE_REFERENCE);
-            if (Double.IsNaN(depth))throw new ArgumentException(VALUE_IS_NAN_REFERENCE);
-            if (Double.IsInfinity(depth))throw new ArgumentException(VALUE_IS_INFINITY_REFERENCE);
-            if (depth < 0)throw new ArgumentException(NEGATIVE_VALUE_REFERENCE);
+            if (Double.IsNaN(depth)) throw new ArgumentException(VALUE_IS_NAN_REFERENCE);
+            if (Double.IsInfinity(depth)) throw new ArgumentException(VALUE_IS_INFINITY_REFERENCE);
+            if (depth < 0) throw new ArgumentException(NEGATIVE_VALUE_REFERENCE);
         }
-        /**
-        <summary>
-            Returns a textual with the height, width and depth of the Customized Dimensions.
-        </summary>
-         */
+
+        ///<summary>
+        /// Returns a textual with the height, width and depth of the Customized Dimensions.
+        ///</summary>
         public override string ToString()
         {
             return string.Format("Height: {0}, Width {1}, Depth {2}", height, width, depth);
         }
-        /**
-        <summary>
-            Returns the generated hash code of the Customized Dimensions.
-        </summary>
-         */
+
+        ///<summary>
+        ///Returns the generated hash code of the Customized Dimensions.
+        ///</summary>
         public override int GetHashCode()
         {
             int hashCode = 17;
             hashCode = (hashCode * 23) + this.height.GetHashCode();
             hashCode = (hashCode * 23) + this.width.GetHashCode();
             hashCode = (hashCode * 23) + this.depth.GetHashCode();
-            return hashCode.GetHashCode();
+            return hashCode;
         }
-        /**
-        <summary>
-            Checks if a certain Customized Dimensions is the same as a received object.
-        </summary>
-        <param name = "obj">object to compare to the current Customized Dimensions</param>
-         */
+
+
+        /// <summary>
+        ///Checks if a certain Customized Dimensions is the same as a received object.
+        ///</summary>
+        ///<param name = "obj">object to compare to the current Customized Dimensions</param>
         public override bool Equals(object obj)
         {
             //Check for null and compare run-time types.
@@ -147,6 +141,14 @@ namespace core.domain
             }
         }
 
-
+        public CustomizedDimensionsDTO toDTO()
+        {
+            CustomizedDimensionsDTO customizedDimensionsDTO = new CustomizedDimensionsDTO();
+            customizedDimensionsDTO.Id = Id;
+            customizedDimensionsDTO.width = width;
+            customizedDimensionsDTO.depth = depth;
+            customizedDimensionsDTO.height = height;
+            return customizedDimensionsDTO;
+        }
     }
 }
--- core_tests/domain/CustomizedDimensionsTest.cs
+++ core_tests/domain/CustomizedDimensionsTest.cs
@@ -2,20 +2,17 @@ using System;
 using Xunit;
 using System.Collections.Generic;
 using core.domain;
+using core.dto;
+
 namespace core_tests.domain
 {
-    /**
-    <summary>
-        Tests of the class CustomizedDimensions.
-    </summary>
-    */
+
+    ///<summary>
+    /// Tests of the class CustomizedDimensions.
+    ///</summary>
     public class CustomizedDimensionsTest
     {
-        /**
-        <summary>
-            Test to ensure that the method GetHashCode works.
-         </summary>
-         */
+
         [Fact]
         public void testGetHashCode()
         {
@@ -24,11 +21,7 @@ namespace core_tests.domain
 
             Assert.Equal(cuntDimensions1.GetHashCode(), cuntDimensions2.GetHashCode());
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for two CustomizedDimensions with different heigth.
-         </summary>
-         */
+
         [Fact]
         public void testDifferentCustDimensionsHeightAreNotEqual()
         {
@@ -37,6 +30,7 @@ namespace core_tests.domain
 
             Assert.False(custDimensions1.Equals(custDimensions2));
         }
+
         [Fact]
         public void ensureConstructorDetectsValueIsNaNHeight()
         {
@@ -96,11 +90,7 @@ namespace core_tests.domain
             Action act = () => CustomizedDimensions.valueOf(4.0, 3, -2.5);
             Assert.Throws<ArgumentException>(act);
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for two CustomizedDimensions with different width.
-         </summary>
-         */
+
         [Fact]
         public void testDifferentCustDimensionsWidthAreNotEqual()
         {
@@ -109,11 +99,7 @@ namespace core_tests.domain
 
             Assert.False(custDimensions1.Equals(custDimensions2));
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for two CustomizedDimensions with different depth.
-         </summary>
-         */
+
         [Fact]
         public void testDifferentCustDimensionsDepthAreNotEqual()
         {
@@ -122,11 +108,7 @@ namespace core_tests.domain
 
             Assert.False(custDimensions1.Equals(custDimensions2));
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for two CustomizedDimensions with different depth.
-         </summary>
-         */
+
         [Fact]
         public void testEqualCustDimensionsAreEqual()
         {
@@ -135,11 +117,7 @@ namespace core_tests.domain
 
             Assert.True(custDimensions1.Equals(custDimensions2));
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for a null CustomizedDimensions.
-         </summary>
-         */
+
         [Fact]
         public void testNullCustomizedDimensionsIsNotEqual()
         {
@@ -147,11 +125,7 @@ namespace core_tests.domain
 
             Assert.False(custDimensions.Equals(null));
         }
-        /**
-        <summary>
-            Test to ensure that the method Equals works, for a CustomizedDimensions and an object of another type.
-         </summary>
-         */
+
         [Fact]
         public void testDifferentTypesAreNotEqual()
         {
@@ -161,11 +135,7 @@ namespace core_tests.domain
 
             Assert.False(finish.Equals(custDimensions));
         }
-        /**
-        <summary>
-            Test to ensure that the method ToString works.
-         </summary>
-         */
+
         [Fact]
         public void testToString()
         {
@@ -173,6 +143,18 @@ namespace core_tests.domain
             CustomizedDimensions custDimensions2 = CustomizedDimensions.valueOf(15.1, 3, 2.1);
 
             Assert.Equal(custDimensions1.ToString(), custDimensions2.ToString());
+        }
+
+        [Fact]
+        public void ensureToDTOWorks(){
+
+            CustomizedDimensions instance = CustomizedDimensions.valueOf(100,200,300);
+            CustomizedDimensionsDTO instanceDTO = instance.toDTO();
+
+            Assert.Equal(instanceDTO.height,instance.height);
+            Assert.Equal(instanceDTO.width,instance.width);
+            Assert.Equal(instanceDTO.depth,instance.depth);
+            Assert.Equal(instanceDTO.Id,instance.Id);
         }
     }
 }
