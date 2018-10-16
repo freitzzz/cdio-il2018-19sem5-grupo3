5dda5c04fa94f925504d7e61bed13276422afe3a
Ricardina <1160941@isep.ipp.pt>
2018-10-16 22:24:57 +0100

    Creation of some Unit tests

---
+++ core_tests/domain/CustomizedCatalogueTest.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using support.dto;
+using core.domain;
+using core.dto;
+using Xunit;
+namespace core_tests.domain
+{
+    public class CustomizedCatalogueTest
+    {
+        [Fact]
+        public void ensureCustomizedCatalogueContructorIsInvalid()
+        {
+            var category = new ProductCategory("Drawers");
+
+
+
+            //Creating Dimensions
+            List<Double> values2 = new List<Double>();
+
+            values2.Add(500.0); //Width
+
+            DiscreteDimensionInterval d2 = DiscreteDimensionInterval.valueOf(values2);
+
+            List<Dimension> valuest = new List<Dimension>();
+            valuest.Add(d2);
+
+            IEnumerable<Dimension> heightValues = valuest;
+            IEnumerable<Dimension> widthValues = valuest;
+            IEnumerable<Dimension> depthValues = valuest;
+
+            //Creating a material
+            string reference = "1160912";
+            string designation = "FR E SH A VOCA DO";
+
+            List<Color> colors = new List<Color>();
+            Color color = Color.valueOf("AND READ-ER-BIBLE", 1, 2, 3, 0);
+            colors.Add(color);
+
+            List<Finish> finishes = new List<Finish>();
+            Finish finish = Finish.valueOf("Amém");
+            finishes.Add(finish);
+
+            Material material = new Material(reference, designation, colors, finishes);
+            List<Material> materials = new List<Material>();
+            materials.Add(material);
+
+            IEnumerable<Material> matsList = materials;
+
+            Product product = new Product("#666", "Shelf", category, matsList, heightValues, widthValues, depthValues);
+            CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(1.2, 1.5, 20.3);
+
+            //Customized Material
+            Color color1 = Color.valueOf("Azul", 1, 1, 1, 1);
+            Finish finish2 = Finish.valueOf("Acabamento polido");
+            CustomizedMaterial custMaterial1 = CustomizedMaterial.valueOf(color1, finish2);
+
+
+
+            CustomizedProduct cp = new CustomizedProduct("#666", "Shelf", custMaterial1, customizedDimensions, product);
+            List<CustomizedProduct> list = new List<CustomizedProduct>();
+            list.Add(cp);
+
+           
+
+            CustomizedProductCollection customizedProductCollection = new CustomizedProductCollection("Mario");
+            CustomizedCatalogue customizedCatalogue = new CustomizedCatalogue(list,customizedProductCollection);
+            Assert.Throws<ArgumentException>(() =>  new CustomizedCatalogue(list, null)); 
+        }
+
+        
+        [Fact]
+        public void ensureCustomizedCatalogueContructorIsValid()
+        {
+            var category = new ProductCategory("Drawers");
+
+
+
+            //Creating Dimensions
+            List<Double> values2 = new List<Double>();
+
+            values2.Add(500.0); //Width
+
+            DiscreteDimensionInterval d2 = DiscreteDimensionInterval.valueOf(values2);
+
+            List<Dimension> valuest = new List<Dimension>();
+            valuest.Add(d2);
+
+            IEnumerable<Dimension> heightValues = valuest;
+            IEnumerable<Dimension> widthValues = valuest;
+            IEnumerable<Dimension> depthValues = valuest;
+
+            //Creating a material
+            string reference = "1160912";
+            string designation = "FR E SH A VOCA DO";
+
+            List<Color> colors = new List<Color>();
+            Color color = Color.valueOf("AND READ-ER-BIBLE", 1, 2, 3, 0);
+            colors.Add(color);
+
+            List<Finish> finishes = new List<Finish>();
+            Finish finish = Finish.valueOf("Amém");
+            finishes.Add(finish);
+
+            Material material = new Material(reference, designation, colors, finishes);
+            List<Material> materials = new List<Material>();
+            materials.Add(material);
+
+            IEnumerable<Material> matsList = materials;
+
+            Product product = new Product("#666", "Shelf", category, matsList, heightValues, widthValues, depthValues);
+            CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(1.2, 1.5, 20.3);
+
+            //Customized Material
+            Color color1 = Color.valueOf("Azul", 1, 1, 1, 1);
+            Finish finish2 = Finish.valueOf("Acabamento polido");
+            CustomizedMaterial custMaterial1 = CustomizedMaterial.valueOf(color1, finish2);
+
+
+
+            CustomizedProduct cp = new CustomizedProduct("#666", "Shelf", custMaterial1, customizedDimensions, product);
+            List<CustomizedProduct> list = new List<CustomizedProduct>();
+            list.Add(cp);
+
+           
+
+            CustomizedProductCollection customizedProductCollection = new CustomizedProductCollection("Mario");
+            CustomizedCatalogue customizedCatalogue = new CustomizedCatalogue(list,customizedProductCollection);
+            Assert.True(customizedCatalogue.Equals( new CustomizedCatalogue(list,customizedProductCollection)));
+        }
+
+         [Fact]
+        public void ensureHashCodeIsEqual()
+        {
+            var category = new ProductCategory("Drawers");
+
+
+
+            //Creating Dimensions
+            List<Double> values2 = new List<Double>();
+
+            values2.Add(500.0); //Width
+
+            DiscreteDimensionInterval d2 = DiscreteDimensionInterval.valueOf(values2);
+
+            List<Dimension> valuest = new List<Dimension>();
+            valuest.Add(d2);
+
+            IEnumerable<Dimension> heightValues = valuest;
+            IEnumerable<Dimension> widthValues = valuest;
+            IEnumerable<Dimension> depthValues = valuest;
+
+            //Creating a material
+            string reference = "1160912";
+            string designation = "FR E SH A VOCA DO";
+
+            List<Color> colors = new List<Color>();
+            Color color = Color.valueOf("AND READ-ER-BIBLE", 1, 2, 3, 0);
+            colors.Add(color);
+
+            List<Finish> finishes = new List<Finish>();
+            Finish finish = Finish.valueOf("Amém");
+            finishes.Add(finish);
+
+            Material material = new Material(reference, designation, colors, finishes);
+            List<Material> materials = new List<Material>();
+            materials.Add(material);
+
+            IEnumerable<Material> matsList = materials;
+
+            Product product = new Product("#666", "Shelf", category, matsList, heightValues, widthValues, depthValues);
+            CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(1.2, 1.5, 20.3);
+
+            //Customized Material
+            Color color1 = Color.valueOf("Azul", 1, 1, 1, 1);
+            Finish finish2 = Finish.valueOf("Acabamento polido");
+            CustomizedMaterial custMaterial1 = CustomizedMaterial.valueOf(color1, finish2);
+
+
+
+            CustomizedProduct cp = new CustomizedProduct("#666", "Shelf", custMaterial1, customizedDimensions, product);
+            List<CustomizedProduct> list = new List<CustomizedProduct>();
+            list.Add(cp);
+
+           
+
+            CustomizedProductCollection customizedProductCollection = new CustomizedProductCollection("Mario");
+            CustomizedCatalogue customizedCatalogue = new CustomizedCatalogue(list,customizedProductCollection);
+            Assert.True(customizedCatalogue.Equals( new CustomizedCatalogue(list,customizedProductCollection)));
+
+        }
+
+    }
+}
