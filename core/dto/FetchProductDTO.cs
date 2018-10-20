aa3d5ef77bb4782544783d6a43f69b58b43f6fb6
João Freitas <1160907@isep.ipp.pt>
2018-10-20 23:31:03 +0100

    Added FetchProductDTO class which holds the information that
    is going to be used to fetch a product
    
    see #1

---
+++ core/dto/FetchProductDTO.cs
@@ -0,0 +1,28 @@
+using core.dto.options;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace core.dto{
+    /// <summary>
+    /// DTO for holding the information which will be used to fetch a product
+    /// </summary>
+    [DataContract]
+    public sealed class FetchProductDTO{
+        /// <summary>
+        /// Long with the persistence ID of the product being fetched
+        /// </summary>
+        [DataMember(Name="id")]
+        public long id{get;set;}
+
+        /// <summary>
+        /// String with the reference of the product being fetched
+        /// </summary>
+        [DataMember(Name="reference")]
+        public string reference{get;set;}
+        
+        /// <summary>
+        /// ProductDTOOptions with the set of options which the product information can have 
+        /// </summary>
+        public ProductDTOOptions productDTOOptions{get;set;}
+    }
+}
