e8c63bb662e4a00056862df0555eef516064ebbc
João Freitas <1160907@isep.ipp.pt>
2018-10-20 19:43:30 +0100

    Updated the way DTOAbleOptions treats the applying options
    see #5

--- support/dto/DTOAbleOptions.cs
+++ support/dto/DTOAbleOptions.cs
@@ -5,7 +5,12 @@ namespace support.dto{
     /// A generic interface for transforming an object to a DTO with options
     /// </summary>
     /// <typeparam name="D">Generic-Type of the DTO being created</typeparam>
-    public interface DTOAbleOptions<D> where D:DTO{
-        D toDTO(Options dtoOptions);
+    public interface DTOAbleOptions<D,O> where D:DTO where O:Options{
+        /// <summary>
+        /// Returns the DTO representation of the object with set of options
+        /// </summary>
+        /// <param name="dtoOptions">O with the set of options being applied</param>
+        /// <returns>D with the DTO of the current object with the applied options</returns>
+        D toDTO(O dtoOptions);
     }
 }
