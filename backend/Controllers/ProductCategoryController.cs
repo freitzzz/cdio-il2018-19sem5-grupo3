85238443f39900deae1190fbca68d8c1ffb6c10d
Gil Durão <1160936@isep.ipp.pt>
2018-10-22 06:08:02 +0100

    Adds missing logs to ProductCategoryController
    see #33

--- backend/Controllers/ProductCategoryController.cs
+++ backend/Controllers/ProductCategoryController.cs
@@ -19,6 +19,86 @@ namespace backend.Controllers
         private const string ERROR_EMPTY_BODY = "Unable to add a category with an empty body";
 
         /// <summary>
+        /// Constant representing the log message for when a POST Request starts
+        /// </summary>
+        private const string LOG_POST_START = "POST Request started";
+
+        /// <summary>
+        /// Constant representing the log message for when a DELETE Request starts
+        /// </summary>
+        private const string LOG_DELETE_START = "DELETE Request started";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET By ID Request starts
+        /// </summary>
+        private const string LOG_GET_BY_ID_START = "GET By ID Request started";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET All Request starts
+        /// </summary>
+        private const string LOG_GET_ALL_START = "GET All request started";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET By Name Request starts
+        /// </summary>
+        private const string LOG_GET_BY_NAME_START = "GET By Name request started";
+
+        /// <summary>
+        /// Constant representing the log message for when a POST Request with an empty body occurs
+        /// </summary>
+        private const string LOG_POST_EMPTY_BODY = "POST Request with empty body";
+
+        /// <summary>
+        /// Constant representing the log message for when a POST Request is successful
+        /// </summary>
+        private const string LOG_POST_SUCCESS = "Product Category {@Category} created";
+
+        /// <summary>
+        /// Constant representing the log message for when a DELETE Request is successful
+        /// </summary>
+        private const string LOG_DELETE_SUCCESS = "Product Category {@Category} soft deleted";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET By ID Request is successful
+        /// </summary>
+        private const string LOG_GET_BY_ID_SUCCESS = "Product Category {@Category} retrieved";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET All Request is successful
+        /// </summary>
+        private const string LOG_GET_ALL_SUCCESS = "Product Categories {@list} retrieved";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET By Name Request is successful
+        /// </summary>
+        private const string LOG_GET_BY_NAME_SUCCESS = "Product Category {@Category} retrieved";
+
+        /// <summary>
+        /// Constant representing the log message for when a POST Request returns a BadRequest
+        /// </summary>
+        private const string LOG_POST_BAD_REQUEST = "POST BadRequest";
+
+        /// <summary>
+        /// Constant representing the log message for when a DELETE Request returns NotFound
+        /// </summary>
+        private const string LOG_DELETE_NOT_FOUND = "DELETE({categoryID}) NOT FOUND";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET By ID Request returns NotFound
+        /// </summary>
+        private const string LOG_GET_BY_ID_NOT_FOUND = "GETByID({categoryID}) NOT FOUND";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET All Request returns NotFound
+        /// </summary>
+        private const string LOG_GET_ALL_NOT_FOUND = "GET All NOT FOUND";
+
+        /// <summary>
+        /// Constant representing the log message for when a GET All Request returns NotFound
+        /// </summary>
+        private const string LOG_GET_BY_NAME_NOT_FOUND = "GETByName({name}) NOT FOUND";
+
+        /// <summary>
         /// Repository being used to store instances of ProductCategory.
         /// </summary>
         private readonly ProductCategoryRepository categoryRepository;
@@ -49,8 +129,10 @@ namespace backend.Controllers
         [HttpPost]
         public ActionResult addProductCategory([FromBody] ProductCategoryDTO categoryAsJson)
         {
+            logger.LogInformation(LOG_POST_START);
             if (categoryAsJson == null)
             {
+                logger.LogWarning(LOG_POST_EMPTY_BODY);
                 return BadRequest(new { error = ERROR_EMPTY_BODY });
             }
 
@@ -58,12 +140,13 @@ namespace backend.Controllers
             {
                 ProductCategoryDTO createdCategory = new core.application.ProductCategoryController().
                 addProductCategory(categoryAsJson);
-                logger.LogInformation("Category {@Category} was created on {Timestamp}",createdCategory,DateTime.Now);
+                logger.LogInformation(LOG_POST_SUCCESS, createdCategory);
                 return CreatedAtRoute("GetCategory", new { id = createdCategory.id }, createdCategory);
 
             }
             catch (ArgumentException e)
             {
+                logger.LogWarning(e, LOG_POST_BAD_REQUEST);
                 return BadRequest(new { error = e.Message });
             }
         }
@@ -77,14 +160,16 @@ namespace backend.Controllers
         [HttpDelete("{id}")]
         public ActionResult removeProductCategory(long id)
         {
+            logger.LogInformation(LOG_DELETE_START);
             try
             {
                 ProductCategoryDTO removedCategory = new core.application.ProductCategoryController().removeProductCategory(id);
-
+                logger.LogInformation(LOG_DELETE_SUCCESS, removedCategory);
                 return NoContent();
             }
             catch (ArgumentException e)
             {
+                logger.LogWarning(e, LOG_DELETE_NOT_FOUND, id);
                 return NotFound(new { error = e.Message });
             }
         }
@@ -98,15 +183,17 @@ namespace backend.Controllers
         [HttpGet("{id}", Name = "GetCategory")]
         public ActionResult findById(long id)
         {
+            logger.LogInformation(LOG_GET_BY_ID_START);
             try
             {
                 ProductCategoryDTO result = new core.application.
                     ProductCategoryController().findByDatabaseId(id);
-
+                logger.LogInformation(LOG_GET_BY_ID_SUCCESS, result);
                 return Ok(result);
             }
             catch (ArgumentException e)
             {
+                logger.LogWarning(e, LOG_GET_BY_ID_NOT_FOUND, id);
                 return NotFound(new { error = e.Message });
             }
         }
@@ -133,16 +220,18 @@ namespace backend.Controllers
         /// <returns>ActionResult with the 200 HTTP code with any instance of ProductCategory was found</returns>
         private ActionResult findAll()
         {
+            logger.LogInformation(LOG_GET_ALL_START);
             try
             {
                 List<ProductCategoryDTO> result = new core.application.
                     ProductCategoryController().findAllCategories();
-
+                logger.LogInformation(LOG_GET_ALL_SUCCESS, result);
                 return Ok(result);
 
             }
             catch (ArgumentException e)
             {
+                logger.LogInformation(LOG_GET_ALL_NOT_FOUND);
                 return NotFound(new { error = e.Message });
             }
         }
@@ -155,15 +244,17 @@ namespace backend.Controllers
         /// business identifier was found or 404 HTTP code if no ProductCategory was found.</returns>
         private ActionResult findByName(string name)
         {
+            logger.LogInformation(LOG_GET_BY_NAME_START);
             try
             {
                 ProductCategoryDTO result = new core.application.
                     ProductCategoryController().findByName(name);
-
+                logger.LogInformation(LOG_GET_BY_NAME_SUCCESS, result);
                 return Ok(result);
             }
             catch (ArgumentException e)
             {
+                logger.LogWarning(e, LOG_GET_BY_NAME_NOT_FOUND, name);
                 return NotFound(new { error = e.Message });
             }
         }
