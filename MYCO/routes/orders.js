fa59f4b1bdfbccc525c2b76f5963fbe8e7c6e6c4
Gil Durão <1160936@isep.ipp.pt>
2018-10-23 20:51:19 +0100

    Initial implementation of US2100
    see #34
    see #38

--- MYCO/routes/orders.js
+++ MYCO/routes/orders.js
@@ -4,6 +4,7 @@ const Order = require('../models/order');
 const http = require('http');
 
 //Get all orders in the database
+//TODO Pretty things up
 ordersRoute.route('/orders').get(function (req, res, next) {
     Order.find(function (err, orders) {
         if (err) {
@@ -13,16 +14,45 @@ ordersRoute.route('/orders').get(function (req, res, next) {
     })
 })
 
-ordersRoute.route('/orders/:id').get(function (req, res, next) {
+ordersRoute.route('/orders/:id').get(/*async*/ function (req, res, next) {
+    var id = req.params.id;
     //Communicate with MYCM
-    Order.findById(function (err, order) {
+    var customizedProductsInfo = '';
+    Order.findById(id, function (err, order) {
+        console.log('hello');
+
+        console.log(res.json(order));
+        for (var orderContentSchema in order.orderContents) {
+            console.log('ola');
+            //TODO dinamically change localhosts
+            http.get('http://localhost:5000/myc/api/customizedProduct/' + orderContentSchema.customizedproduct, (resp) => {
+                let data = '';
+
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                resp.on('end', () => {
+                    customizedProductsInfo += JSON.parse(data + orderContentSchema.quantity);
+                });
+
+            }).on("error", (err) => {
+                //TODO don't log on console
+                console.log("Error: " + err.message);
+            });
+
+        }
+        //customizedProductsInfo += JSON.parse(order.status);
+        console.log(customizedProductsInfo);
         if (err) {
             return next(new Error(err));
         }
-        res.status(200).json(order);
-    })
+        console.log('123');
+        //res.status(200).json(customizedProductsInfo);
+    });
 })
 
+//TODO Pretty things up
 ordersRoute.route('/orders').post(function (req, res, next) {
     Order.create(function (err, order) {
         if (err) {
