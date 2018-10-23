7ab51173071f0ff18aff4ac5433240c04ffe029c
João Freitas <1160907@isep.ipp.pt>
2018-10-23 12:11:50 +0100

    Added three initial integration + regression tests regarding
    Customized Products Collection API
    
    see #4
    
    see #9
    
    see #20

--- backend_tests/Controllers/CustomizedProductsCollectionIntegrationTests.cs
+++ backend_tests/Controllers/CustomizedProductsCollectionIntegrationTests.cs
@@ -44,11 +44,85 @@ namespace backend_tests.Controllers
                 BaseAddress = new Uri("http://localhost:5001")
             });
         }
+        
+        /// <summary>
+        /// Ensures that's not possible to create a customized product collection with an invalid name (null/empty)
+        /// </summary>
+        [Fact, TestPriority(0)]
+        public async void ensureCantCreateACustomizedProductCollectionIfItHasAnInvalidName(){
+            //We are attempting to create a customized product collection with an invalid name
+            CustomizedProductCollectionDTO customizedProductCollectionDTO=new CustomizedProductCollectionDTO();
+            //Our first attempt will have the name a "null" reference
+            customizedProductCollectionDTO.name=null;
+            var createCustomizedProductCollection=await httpClient.PostAsJsonAsync(CUSTOMIZED_PRODUCTS_COLLECTION_URI
+                                                                                ,customizedProductCollectionDTO);
+            //The result should be a bad request since it's not possible to create a customized product with a "null" name
+            Assert.Equal(HttpStatusCode.BadRequest,createCustomizedProductCollection.StatusCode);
+            
+            //Our second attempt will have the name as an empty string
+            customizedProductCollectionDTO.name="";
+            createCustomizedProductCollection=await httpClient.PostAsJsonAsync(CUSTOMIZED_PRODUCTS_COLLECTION_URI
+                                                                                ,customizedProductCollectionDTO);
+            //The result should be a bad request since it's not possible to create a customized product with an "empty" name
+            Assert.Equal(HttpStatusCode.BadRequest,createCustomizedProductCollection.StatusCode);
+            
+            Console.WriteLine("\001b[0;32m"+"ensureCantCreateACustomizedProductCollectionIfItHasAnInvalidName"+"\u001b[0m");
+        }
+
+        /// <summary>
+        /// Ensures that's not possible to create a customized product collection which already exists (name (identity) already exists)
+        /// </summary>
+        [Fact, TestPriority(1)]
+        public async void ensureCantCreateACustomizedProductCollectionIfItAlreadyExists(){
+            //We are attempting to create a customized product collection which already exists
+            //First we need to grant that a product with a certain name already exists
+            Task<CustomizedProductCollectionDTO> validCustomizedProductCollectionDTOTask=ensureCustomizedProductCollectionIsCreatedSuccesfuly();
+            validCustomizedProductCollectionDTOTask.Wait();
+            
+            CustomizedProductCollectionDTO customizedProductCollectionDTO=new CustomizedProductCollectionDTO();
+            //Then we will apply the name of the existing product to the customized product collection
+            customizedProductCollectionDTO.name=validCustomizedProductCollectionDTOTask.Result.name;
+            var createCustomizedProductCollection=await httpClient.PostAsJsonAsync(CUSTOMIZED_PRODUCTS_COLLECTION_URI
+                                                                                    ,customizedProductCollectionDTO);
+            //The result should be a bad request since it's not possible to create a customized product which already exists
+            Assert.Equal(HttpStatusCode.BadRequest,createCustomizedProductCollection.StatusCode);
+            
+            Console.WriteLine("\001b[0;32m"+"ensureCantCreateACustomizedProductCollectionIfItAlreadyExists"+"\u001b[0m");
+        }
+
+        /// <summary>
+        /// Ensures that's not possible to create a customized product collection which aggregated customized products are invalid (empty/dont exist)
+        /// </summary>
+        [Fact, TestPriority(2)]
+        public async void ensureCantCreateACustomizedProductCollectionIfItAggregatesInvalidCustomizedProducts(){
+            //We are attempting to create a customized product collection which aggregated customized products are invalid
+            CustomizedProductCollectionDTO customizedProductCollectionDTO=new CustomizedProductCollectionDTO();
+            //We first need to generate an atomic valid name for the customized products collection
+            customizedProductCollectionDTO.name=Guid.NewGuid().ToString("n");
+
+            //In our first attempt we will try to create a customized product collection with "empty" customized products
+            customizedProductCollectionDTO.customizedProducts=new List<CustomizedProductDTO>();
+
+            var createCustomizedProductCollection=await httpClient.PostAsJsonAsync(CUSTOMIZED_PRODUCTS_COLLECTION_URI
+                                                                                    ,customizedProductCollectionDTO);
+            //The result should be a bad request since it's not possible to create a customized product collection with "empty" customized products
+            Assert.Equal(HttpStatusCode.BadRequest,createCustomizedProductCollection.StatusCode);
+
+            //In our second and last attempt we will try to create a customized product collection with customized products which don't exist (resources don't exist)
+            customizedProductCollectionDTO.customizedProducts=new List<CustomizedProductDTO>(new []{new CustomizedProductDTO()});
+
+            createCustomizedProductCollection=await httpClient.PostAsJsonAsync(CUSTOMIZED_PRODUCTS_COLLECTION_URI
+                                                                                    ,customizedProductCollectionDTO);
+            //The result should be a bad request since it's not possible to create a customized product collection with customized products which don't exist (resources don't exist)
+            Assert.Equal(HttpStatusCode.BadRequest,createCustomizedProductCollection.StatusCode);
+            
+            Console.WriteLine("\001b[0;32m"+"ensureCantCreateACustomizedProductCollectionIfItAggregatesInvalidCustomizedProducts"+"\u001b[0m");
+        }
 
         /// <summary>
         /// Ensures that a customized product collection is created succesfuly
         /// </summary>
-        [Fact, TestPriority(1)]
+        [Fact, TestPriority(3)]
         public async Task<CustomizedProductCollectionDTO> ensureCustomizedProductCollectionIsCreatedSuccesfuly()
         {
             CustomizedProductCollectionDTO customizedProductCollectionDTO = new CustomizedProductCollectionDTO();
@@ -66,7 +140,7 @@ namespace backend_tests.Controllers
         }
 
 
-        [Fact, TestPriority(2)]
+        [Fact, TestPriority(4)]
         public async Task<CustomizedProductCollectionDTO> ensureCustomizedProductCollectionWithCustomizedProductsIsCreatedSuccessfully()
         {
             //Create a new CustomizedProduct that will be added to the Collection
