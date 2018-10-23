3c2006a682c66236a77542ee2fc5908228cd1e8a
António Sousa <1161371@isep.ipp.pt>
2018-10-23 16:55:31 +0100

    Minor changes to DTO's related to CommercialCatalogue [re #4].

--- core/dto/CatalogueCollectionDTO.cs
+++ core/dto/CatalogueCollectionDTO.cs
@@ -3,25 +3,21 @@ using System.Runtime.Serialization;
 using System.Collections.Generic;
 using core.domain;
 using support.dto;
+using System.Linq;
+
 namespace core.dto
 {
     public class CatalogueCollectionDTO : DTO, DTOParseable<CatalogueCollection, CatalogueCollectionDTO>
     {
-        /// <summary>
-        /// CatalogueCollection's database identifier.
-        /// </summary>
-        /// <value></value>
-        [DataMember]
-        public long Id { get; set; }
         /// <summary>
         /// CatalogueCollection's list of customized products.
         /// </summary>
         /// <value>Gets/sets the value of the customized products field.</value>
         [DataMember]
 
-        public List<CustomizedProductDTO> customizedProductsDTO { get; set; }
+        public List<CustomizedProductDTO> customizedProductDTOs { get; set; }
 
-       /// <summary>
+        /// <summary>
         /// CatalogueCollection's customizedProductCollectionDTO.
         /// </summary>
         /// <value></value>
@@ -34,15 +30,9 @@ namespace core.dto
         /// <returns>DTO's equivalent Entity</returns>
         public CatalogueCollection toEntity()
         {
-            List<CustomizedProduct> custProducts = new List<CustomizedProduct>();
+            List<CustomizedProduct> custProducts = DTOUtils.reverseDTOS(customizedProductDTOs).ToList();
 
-            foreach (CustomizedProductDTO dto in this.customizedProductsDTO)
-            {
-                custProducts.Add(dto.toEntity());
-            }
-
-            CatalogueCollection catalogueCollection = new CatalogueCollection(custProducts, this.customizedProductCollectionDTO.toEntity());
-            catalogueCollection.Id = this.Id;
+            CatalogueCollection catalogueCollection = new CatalogueCollection(this.customizedProductCollectionDTO.toEntity(), custProducts);
 
             return catalogueCollection;
         }
