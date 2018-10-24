38902e1140a4e9eba1cf5f04b57cfbca5d25a98f
João Freitas <1160907@isep.ipp.pt>
2018-10-24 15:15:23 +0100

    Closet Faces and Closet Slot Faces positions and axes are now
    updated sucessfuly
    
    see #31

--- SGRAI/US4050/js/ProductDrawer.js
+++ SGRAI/US4050/js/ProductDrawer.js
@@ -1,9 +1,27 @@
-var camera, controls, scene, renderer;
+/**
+ * Global Variables for Graphic Control (Camera, Rendering, Scene, etc...)
+ */
+var camera, controls, scene, renderer,group;
 
+/**
+ * Global variable with the current closet
+ */
 var closet=null;
 
-var faces=[base_face_dimensions_axes,top_face_dimensions_axes,left_face_dimensions_axes,right_face_dimensions_axes,back_face_dimensions_axes];
-var faces_ids=[];
+/**
+ * Global variable with the current closet faces ids (Mesh IDS from Three.js)
+ */
+var closet_faces_ids=[];
+
+/**
+ * Global variable with the current closet slots faces ids (Mesh IDS from Three.js)
+ */
+var closet_slots_faces_ids=[];
+
+var material = new THREE.MeshNormalMaterial( );
+
+
+
 
 /**
  * Initial Product Draw function
@@ -13,14 +31,139 @@ function main() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.y = -400;
-    camera.position.z = 400;
-    camera.rotation.x = .70;
+    initCamera();
 
+    initControls();
+
+    initCloset();
+
+    scene.add(group);
+
+    addSlotNumbered(5);
+
+    
+
+    animate();
+
+}
+
+/**
+ * Initiates the closet
+ */
+function initCloset(){
+    scene=new THREE.Scene();
+    group=new THREE.Group();
+    closet=new Closet();
+    var faces=closet.closet_faces;
+    
+    for(var i=0;i<faces.length;i++){
+        closet_faces_ids.push(generateParellepiped(faces[i][0],faces[i][1],faces[i][2]
+                                    ,faces[i][3],faces[i][4],faces[i][5]
+                                    ,material,group));
+    }
+    scene.add(group);
+}
+
+/**
+ * Updates current closet graphical view
+ */
+function updateClosetGV(){
+    
+    for(var i=0;i<closet_faces_ids.length;i++){
+        var closet_face=group.getObjectById(closet_faces_ids[i]);
+        closet_face.scale.x=getNewScaleValue(closet.initial_closet_faces[i][0],closet.closet_faces[i][0],closet_face.scale.x);
+        closet_face.scale.y=getNewScaleValue(closet.initial_closet_faces[i][1],closet.closet_faces[i][1],closet_face.scale.y);
+        closet_face.scale.z=getNewScaleValue(closet.initial_closet_faces[i][2],closet.closet_faces[i][2],closet_face.scale.z);
+        closet_face.position.x=closet.closet_faces[i][3];
+        closet_face.position.y=closet.closet_faces[i][4];
+        closet_face.position.z=closet.closet_faces[i][5];
+    }
+
+    for(var i=0;i<closet_slots_faces_ids.length;i++){
+        var closet_face=group.getObjectById(closet_slots_faces_ids[i]);
+        closet_face.scale.x=getNewScaleValue(closet.initial_closet_slots_faces[i][0],closet.closet_slots_faces[i][0],closet_face.scale.x);
+        closet_face.scale.y=getNewScaleValue(closet.initial_closet_slots_faces[i][1],closet.closet_slots_faces[i][1],closet_face.scale.y);
+        closet_face.scale.z=getNewScaleValue(closet.initial_closet_slots_faces[i][2],closet.closet_slots_faces[i][2],closet_face.scale.z);
+        closet_face.position.x=closet.closet_slots_faces[i][3];
+        closet_face.position.y=closet.closet_slots_faces[i][4];
+        closet_face.position.z=closet.closet_slots_faces[i][5];
+    }
+}
+
+/**
+ * Adds a slot to the current closet
+ */
+function addSlot(){
+    addSlotNumbered(1);
+}
+
+/**
+ * Adds a specified number of slots to the current closet
+ */
+function addSlotNumbered(slotsToAdd){
+    for(var i=0;i<slotsToAdd;i++){
+        var slotFace=closet.addSlot();
+        closet_slots_faces_ids.push(generateParellepiped(slotFace[0],slotFace[1],slotFace[2]
+                                    ,slotFace[3],slotFace[4],slotFace[5]
+                                    ,material,group));
+    }
+    updateClosetGV();
+}
+
+/**
+ * Removes a slot from the current closet
+ */
+function removeSlot(){
+    closet.removeSlot();
+    closet_slots_faces_ids.pop();
+    updateClosetGV();
+}
+
+/**
+ * Generates a parellepiped with a certain dimensions (width, height, depth) and on a certain position relatively to axes (x,y,z)
+ * @param {number} width Number with the parellepiped width
+ * @param {number} height Number with the parellepiped height
+ * @param {number} depth Number with the parellepiped depth
+ * @param {number} x Number with the parellepiped position relatively to the X axe
+ * @param {number} y Number with the parellepiped position relatively to the Y axe
+ * @param {number} z Number with the parellepiped position relatively to the Z axe
+ * @param {THREE.Material} materia1 THREE.Material with the parellepiped material
+ * @param {THREE.Group} group THREE.Group with the group where the parellepied will be putted
+ */
+function generateParellepiped(width,height,depth,x,y,z,materia1,group){
+    var cubeGeometry=new THREE.CubeGeometry(width,height,depth);
+    var cube=new THREE.Mesh(cubeGeometry,materia1);
+    cube.add(new THREE.AxesHelper(200));
+    cube.position.x=x;
+    cube.position.y=y;
+    cube.position.z=z;
+    group.add(cube);
+    return cube.id;
+}
+
+/**
+ * Animates the scene
+ */
+function animate() {
+    requestAnimationFrame(animate);
+    controls.update();
+    render();
+}
+
+/**
+ * Renders the scene
+ */
+function render() {
+    renderer.render(scene, initCamera);
+}
+
+/**
+ * Initializes the graphic representation controls
+ */
+function initControls(){
     // controls
 
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls = new THREE.OrbitControls(initCamera, renderer.domElement);
 
     //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
 
@@ -33,57 +176,25 @@ function main() {
     controls.maxDistance = 500;
 
     controls.maxPolarAngle = Math.PI / 2;
-
-    scene=new THREE.Scene();
-
-    var group=new THREE.Group();
-
-    closet=new Closet();
-
-    faces=closet.closet_faces;
-
-    for(var i=0;i<faces.length;i++){
-        closet.closet_faces_ids.push(generateCube(faces[i][0],faces[i][1],faces[i][2]
-                                    ,faces[i][3],faces[i][4],faces[i][5]
-                                    ,new THREE.MeshNormalMaterial(),group));
-    }
-    var face=closet.addSlot();
-
-    console.log("->>>>>>>>>>>> "+face);
-
-    closet.closet_slots_faces_ids.push(generateCube(face[0],face[1],face[2]
-                                                ,face[3],face[4],face[5]
-                                                ,new THREE.MeshNormalMaterial(),group));
-
-                                                face=closet.addSlot();
-                                                closet.closet_slots_faces_ids.push(generateCube(face[0],face[1],face[2]
-                                                    ,face[3],face[4],face[5]
-                                                    ,new THREE.MeshNormalMaterial(),group));
-
-    scene.add(group);
-
-    animate();
-
 }
 
-
-function generateCube(width,height,depth,x,y,z,materia1,group){
-    var cubeGeometry=new THREE.CubeGeometry(width,height,depth);
-    var cube=new THREE.Mesh(cubeGeometry,materia1);
-    cube.add(new THREE.AxesHelper(200));
-    cube.position.x=x;
-    cube.position.y=y;
-    cube.position.z=z;
-    group.add(cube);
-    return cube.id;
+/**
+ * Initializes the graphic representation camera
+ */
+function initCamera(){
+    initCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+    initCamera.position.y = -400;
+    initCamera.position.z = 400;
+    initCamera.rotation.x = .70;
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-    render();
-}
-
-function render() {
-    renderer.render(scene, camera);
+/**
+ * Computes the new scale value based on the initial scale value, new scale value and the current scale value
+ * @param {number} initialScaleValue Number with the initial scale value
+ * @param {number} newScaleValue Number with the new scale value
+ * @param {number} currentScaleValue Number with the current scale value
+ */
+function getNewScaleValue(initialScaleValue,newScaleValue,currentScaleValue){
+    if(initialScaleValue==0)return 0;
+    return (newScaleValue*currentScaleValue)/initialScaleValue;
 }
