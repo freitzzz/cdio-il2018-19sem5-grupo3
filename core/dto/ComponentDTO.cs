d249dffb77565df3c5322273c29538ec0e27f49c
João Moreira <1160928@isep.ipp.pt>
2018-10-25 15:21:02 +0100

    Added mandatory attribute to Component and ComponentDTO classes.

--- core/dto/ComponentDTO.cs
+++ core/dto/ComponentDTO.cs
@@ -3,35 +3,38 @@ using System.Runtime.Serialization;
 using core.domain;
 using support.dto;
 
-namespace core.dto
-{
+namespace core.dto {
     /// <summary>
     /// Represents a Component's Data Transfer Object.
     /// </summary>
     /// <typeparam name="Component">Type of entity</typeparam>
     /// <typeparam name="ComponentDTO">Type of DTO</typeparam>
     [DataContract]
-    public class ComponentDTO : DTO, DTOParseable<Component, ComponentDTO>
-    {
+    public class ComponentDTO : DTO, DTOParseable<Component, ComponentDTO> {
         /// <summary>
         /// Component's database identifier.
         /// </summary>
         /// <value>Gets/sets the value of the database identifier field.</value>
-        [DataMember(Name="id")]
-        public long id {get; set;}
+        [DataMember(Name = "id")]
+        public long id { get; set; }
+        /// <summary>
+        /// Component's obligatoriness.
+        /// </summary>
+        /// <value>Gets/sets the value of obligatoriness.</value>
+        [DataMember(Name = "id")]
+        public bool mandatory { get; set; }
         /// <summary>
         /// Component's product.
         /// </summary>
-        [DataMember(Name="complementedProduct")]
-        public ProductDTO product {get; set;}
+        [DataMember(Name = "complementedProduct")]
+        public ProductDTO product { get; set; }
         /// <summary>
         /// Component's list restrictions.
         /// </summary>
         /// <value>Gets/sets the value of the restrictions field.</value>
-        [DataMember(Name="restrictions",EmitDefaultValue = false)]  //no data field data is presented if it's null
-        public List<RestrictionDTO> restrictions {get; set;}
-        public Component toEntity()
-        {
+        [DataMember(Name = "restrictions", EmitDefaultValue = false)]  //no data field data is presented if it's null
+        public List<RestrictionDTO> restrictions { get; set; }
+        public Component toEntity() {
             throw new System.NotImplementedException();
         }
     }
