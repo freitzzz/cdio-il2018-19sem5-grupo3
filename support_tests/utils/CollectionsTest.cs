237bd391ef795e4ab59c0f32e4421ba4e7542e8c
Rita <1160912@isep.ipp.pt>
2018-10-29 17:42:18 +0000

    Added unit testing for class Collections
    [see #4]

--- support_tests/utils/CollectionsTest.cs
+++ support_tests/utils/CollectionsTest.cs
@@ -5,19 +5,14 @@ using System.Collections.Generic;
 
 namespace support_tests.utils
 {
-    /**
-    <summary>
-      Unit testing class for class Lists.
-    </summary>
-    */
+    /// <summary>
+    /// Unit testing class for class Lists.
+    /// </summary>
     public class CollectionsTest
     {
-
-        /**
-        <summary>
-            Test to ensure that the method isListEmpty detects an empty list.
-        </summary>
-         */
+        /// <summary>
+        /// Test to ensure that the method isListEmpty detects an empty list.
+        /// </summary>
         [Fact]
         public void ensureIsListEmptyDetectsEmptyList()
         {
@@ -28,26 +23,22 @@ namespace support_tests.utils
             Assert.True(Collections.isListEmpty(test));
         }
 
-        /**
-        <summary>
-            Test to ensure that the method isListEmpty detects a null list.
-        </summary>
-         */
+        /// <summary>
+        /// Test to ensure that the method isListEmpty detects a null list.
+        /// </summary>
         [Fact]
         public void ensureIsListEmptyDetectsNullList()
         {
-            Console.WriteLine("ensureIsListEmptyDetectsNullObject");
+            Console.WriteLine("ensureIsListEmptyDetectsNullList");
 
             List<Object> test = null;
 
             Assert.True(Collections.isListEmpty(test));
         }
 
-        /**
-        <summary>
-            Test to ensure that the method isListNull detects a null list.
-        </summary>
-         */
+        /// <summary>
+        /// Test to ensure that the method isListNull detects a null list.
+        /// </summary>
         [Fact]
         public void ensureIsListNullDetectsNullList()
         {
@@ -58,11 +49,9 @@ namespace support_tests.utils
             Assert.True(Collections.isListNull(test));
         }
 
-        /**
-        <summary>
-            Test to ensure that the method isEnumerableNullOrEmpty detects an empty enumerable.
-        </summary>
-         */
+        /// <summary>
+        ///Test to ensure that the method isEnumerableNullOrEmpty detects an empty enumerable.
+        /// </summary>
         [Fact]
         public void ensureIsEnumerableNullOrEmptyDetectsEmptyEnumerable()
         {
@@ -73,11 +62,9 @@ namespace support_tests.utils
             Assert.True(Collections.isEnumerableNullOrEmpty(test));
         }
 
-        /**
-        <summary>
-            Test to ensure that the method isEnumerableNullOrEmpty detects a null enumerable.
-        </summary>
-         */
+        /// <summary>
+        /// Test to ensure that the method isEnumerableNullOrEmpty detects a null enumerable.
+        /// </summary>
         [Fact]
         public void ensureIsEnumerableNullOrEmptyDetectsNullEnumerable()
         {
@@ -86,6 +73,56 @@ namespace support_tests.utils
             IEnumerable<Object> test = null;
 
             Assert.True(Collections.isEnumerableNullOrEmpty(test));
+        }
+
+        /// <summary>
+        /// Test to ensure that the expected size is returned for a list.
+        /// </summary>
+        [Fact]
+        public void ensureGetEnumerableSizeWorksForList(){
+            Console.WriteLine("ensureGetEnumerableSizeWorksForList");
+
+            List<string> test = new List<string>();
+            test.Add("The cake is a lie");
+
+            Assert.Equal(1, Collections.getEnumerableSize(test));
+        }
+
+        /// <summary>
+        /// Test to ensure that the expected size is returned for an enumerable other than a list.
+        /// </summary>
+        [Fact]
+        public void ensureGetEnumerableSizeWorksForAnEnumerableOtherThanAList(){
+            Console.WriteLine("ensureGetEnumerableSizeWorksForAnEnumerableOtherThanAList");
+
+            HashSet<string> test = new HashSet<string>();
+            test.Add("The cake is a lie");
+
+            Assert.Equal(1, Collections.getEnumerableSize(test));
+        }
+
+        [Fact]
+        public void ensureEnumerableAsListWorksForList(){
+            Console.WriteLine("ensureEnumerableAsListWorksForList");
+
+            List<string> test = new List<string>();
+            test.Add("The cake is a lie");
+
+            Assert.Equal(test, Collections.enumerableAsList(test));
+        }
+
+        [Fact]
+        public void ensureEnumerableAsListWorksForAnEnumerableOtherThanAList(){
+            Console.WriteLine("ensureEnumerableAsListWorksForAnEnumerableOtherThanAList");
+
+            HashSet<string> test = new HashSet<string>();
+            List<string> expected = new List<string>();
+
+            string element = "The cake is a lie";
+            test.Add(element);
+            expected.Add(element);
+
+            Assert.Equal(expected, Collections.enumerableAsList(test));
         }
     }
 }
