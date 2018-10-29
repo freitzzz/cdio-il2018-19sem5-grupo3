0d4d90bce23eb3cd63d81e1db58ca18e1ce0da0e
João Freitas <1160907@isep.ipp.pt>
2018-10-29 19:30:10 +0000

    Fixed problem on ProductDTOService
    which was not checking the length of the fetched materials and
    components
    
    see #1
    
    see #9

--- core/services/ProductDTOService.cs
+++ core/services/ProductDTOService.cs
@@ -2,6 +2,7 @@ using core.domain;
 using core.dto;
 using core.persistence;
 using support.dto;
+using support.utils;
 using System.Collections.Generic;
 using System;
 
@@ -11,6 +12,17 @@ namespace core.services{
     /// </summary>
     public sealed class ProductDTOService{
         /// <summary>
+        /// Constant that represents the message that occures if the materials being fetched 
+        /// are invalid
+        /// </summary>
+        private const string INVALID_MATERIALS_FETCH = "The materials being fetched are invalid";
+        /// <summary>
+        /// Constant that represents the message that occures if the components being fetched 
+        /// are invalid
+        /// </summary>
+        private const string INVALID_COMPONENTS_FETCH = "The components being fetched are invalid";
+
+        /// <summary>
         /// Transforms a product dto into a product via service
         /// </summary>
         /// <param name="productDTO">ProductDTO with the product dto being transformed</param>
@@ -19,12 +31,14 @@ namespace core.services{
             string reference=productDTO.reference;
             string designation=productDTO.designation;
             IEnumerable<Product> productComplementedProducts=null;
-            if(productDTO.complements!=null)
+            if(productDTO.complements!=null){
                 productComplementedProducts=new ComponentDTOService().transform(productDTO.complements);
+                ensureProductsComponentsFetchWasSuccesful(productDTO.complements,productComplementedProducts);
+            }
             ProductCategory productCategory=PersistenceContext.repositories().createProductCategoryRepository().find(productDTO.productCategory.id);
-            //TODO:Check if the length of product materials is equal to product materials dtos
             IEnumerable<Material> productMaterials=PersistenceContext.repositories().createMaterialRepository().getMaterialsByIDS(productDTO.productMaterials);
-            //TODO:Currently DTO Mapping for dimensions isn't working
+            ensureMaterialsFetchWasSuccessful(productDTO.productMaterials,productMaterials);
+
             IEnumerable<Dimension> productHeightDimensions=DTOUtils.reverseDTOS(productDTO.dimensions.heightDimensionDTOs);
             IEnumerable<Dimension> productWidthDimensions=DTOUtils.reverseDTOS(productDTO.dimensions.widthDimensionDTOs);
             IEnumerable<Dimension> productDepthDimensions=DTOUtils.reverseDTOS(productDTO.dimensions.depthDimensionDTOs);
@@ -76,6 +90,26 @@ namespace core.services{
                                             ,productHeightDimensions
                                             ,productWidthDimensions
                                             ,productDepthDimensions); 
+        }
+        
+        /// <summary>
+        /// Ensures that the materials fetch was successful
+        /// </summary>
+        /// <param name="materialsToFetch">IEnumerable with the materials dtos to fetch</param>
+        /// <param name="fetchedMaterials">IEnumerable with the fetched materials</param>
+        private void ensureMaterialsFetchWasSuccessful(IEnumerable<MaterialDTO> materialsToFetch, IEnumerable<Material> fetchedMaterials) {
+            if (Collections.isEnumerableNullOrEmpty(materialsToFetch) || Collections.getEnumerableSize(materialsToFetch) != Collections.getEnumerableSize(fetchedMaterials))
+                throw new InvalidOperationException(INVALID_MATERIALS_FETCH);
+        }
+
+        /// <summary>
+        /// Ensures that the produts components fetch was successful
+        /// </summary>
+        /// <param name="componentsToFetch">IEnumerable with the components dtos to fetch</param>
+        /// <param name="fetchedComponents">IEnumerable with the fetched components</param>
+        private void ensureProductsComponentsFetchWasSuccesful(IEnumerable<ComponentDTO> componentsToFetch, IEnumerable<Product> fetchedComponents) {
+            if (Collections.isEnumerableNullOrEmpty(componentsToFetch) || Collections.getEnumerableSize(componentsToFetch) != Collections.getEnumerableSize(fetchedComponents))
+                throw new InvalidOperationException(INVALID_COMPONENTS_FETCH);
         }
     }
 }
