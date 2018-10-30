12b45d718e7b429cf3f47536c1c544218baaf6e7
António Sousa <1161371@isep.ipp.pt>
2018-10-30 13:03:23 +0000

    Add unit testing for CatalogueCollectionProduct [re #4].

---
+++ core_tests/domain/CatalogueCollectionProductTest.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using core.domain;
+using Xunit;
+
+namespace core_tests.domain
+{
+    public class CatalogueCollectionProductTest
+    {
+
+        [Fact]
+        private void ensureArgumentExceptionIsThrownIfBothParametersAreNull()
+        {
+            Action action = () => new CatalogueCollectionProduct(null, null);
+
+            Assert.Throws(typeof(ArgumentException), action);
+        }
+
+        [Fact]
+        private void ensureArgumentExceptionIsThrownIfCatalogueCollectionIsNull()
+        {
+            Action action = () => new CatalogueCollectionProduct(null, buildCustomizedProduct());
+
+            Assert.Throws(typeof(ArgumentException), action);
+        }
+
+        [Fact]
+        private void ensureArgumentExceptionIsThrownIfCustomizedProductIsNull()
+        {
+            Action action = () => new CatalogueCollectionProduct(buildCatalogueCollection(), null);
+
+            Assert.Throws(typeof(ArgumentException), action);
+        }
+
+        [Fact]
+        private void ensureInstanceIsCreated()
+        {
+            CatalogueCollectionProduct catalogueCollectionProduct = new CatalogueCollectionProduct(buildCatalogueCollection(), buildCustomizedProduct());
+
+            Assert.NotNull(catalogueCollectionProduct);
+        }
+
+
+        private CatalogueCollection buildCatalogueCollection()
+        {
+            CustomizedProductCollection customizedProductCollection = new CustomizedProductCollection("Awesome Products");
+
+            return new CatalogueCollection(customizedProductCollection);
+        }
+
+        private CustomizedProduct buildCustomizedProduct()
+        {
+            Finish finish = Finish.valueOf("Glossy");
+            Color color = Color.valueOf("Deep Purple", 153, 50, 204, 0);
+
+            Material material = new Material("materialid", "Metal", new List<Color>() { color }, new List<Finish>() { finish });
+
+            ProductCategory productCategory = new ProductCategory("Bands");
+
+            Dimension heightDimension = new SingleValueDimension(21.0);
+            Dimension depthDimension = new SingleValueDimension(15.6);
+            Dimension widthDimension = new SingleValueDimension(19);
+
+            List<Dimension> heightDimensions = new List<Dimension>() { heightDimension };
+            List<Dimension> depthDimensions = new List<Dimension>() { depthDimension };
+            List<Dimension> widthDimensions = new List<Dimension>() { widthDimension };
+
+            Product product = new Product("productid", "Awesome shelf", productCategory, new List<Material>() { material }, heightDimensions, widthDimensions, depthDimensions);
+
+
+            CustomizedMaterial customizedMaterial = CustomizedMaterial.valueOf(material, color);
+            CustomizedDimensions customizedDimensions = CustomizedDimensions.valueOf(21.0, 19, 15.6);
+
+            return new CustomizedProduct("customizedproductid", "Customized Awesome Shelf", customizedMaterial, customizedDimensions, product);
+        }
+    }
+}
