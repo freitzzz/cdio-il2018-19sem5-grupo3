b961be962f739623dd58d58a99a9c312fd192e7f
António Sousa <1161371@isep.ipp.pt>
2018-11-03 18:32:52 +0000

    Add class MeasurementDTO [re #15].

---
+++ core/dto/MeasurementDTO.cs
@@ -0,0 +1,24 @@
+using System.Runtime.Serialization;
+using core.domain;
+using support.dto;
+
+namespace core.dto
+{
+    [DataContract]
+    public class MeasurementDTO : DTO, DTOParseable<Measurement, MeasurementDTO>
+    {
+        [DataMember(Name = "height")]
+        public DimensionDTO height { get; set; }
+
+        [DataMember(Name = "width")]
+        public DimensionDTO width { get; set; }
+
+        [DataMember(Name = "depth")]
+        public DimensionDTO depth { get; set; }
+
+        public Measurement toEntity()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
