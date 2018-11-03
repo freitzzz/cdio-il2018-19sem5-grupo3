723863cfc2472bdf2dbb5846695cf3b7e850393f
Gil Durao <1160936@isep.ipp.pt>
2018-11-03 17:29:57 +0000

    Adds PostCustomizedProductToSlotModelView and PostCustomizedProductToSlotModelViewService
    see #1
    see #15

---
+++ core/dto/PostCustomizedProductToSlotModelView.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace core.dto{
+    /// <summary>
+    /// ModelView used for the POST Request to add a customized product to a Slot
+    /// </summary>
+    [DataContract]
+    public class PostCustomizedProductToSlotModelView : PostCustomizedProductModelView{
+        /// <summary>
+        /// Slot Identifier
+        /// </summary>
+        /// <value>Gets/Sets the identifer</value>
+        [DataMember(Name="addToSlot", EmitDefaultValue = false)]
+        public long slotId {get; set;}
+
+        /// <summary>
+        /// Identifier of the CustomizedProducts base
+        /// </summary>
+        /// <value>Gets/Sets the identifier</value>
+        [DataMember(Name="baseId", EmitDefaultValue = false)]
+        public long baseId {get; set;}
+    }
+}
