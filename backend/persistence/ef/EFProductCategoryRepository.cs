3c0f322943a5e0558aad0bdcb4464eac35ddce72
António Sousa <1161371@isep.ipp.pt>
2018-11-04 21:00:38 +0000

    Add isLeaf() method [re #4].

--- backend/persistence/ef/EFProductCategoryRepository.cs
+++ backend/persistence/ef/EFProductCategoryRepository.cs
@@ -10,26 +10,22 @@ namespace backend.persistence.ef
     {
         public EFProductCategoryRepository(MyCContext dbContext) : base(dbContext) { }
 
-        /// <summary>
-        /// Finds all ProductCategories' sub-categories (ProductCategories that have the received category as a parent).
-        /// </summary>
-        /// <param name="category">Category to search</param>
-        /// <returns>List with all ProductCategory sub-categories</returns>
         public IEnumerable<ProductCategory> findSubCategories(ProductCategory category)
         {
             return dbContext.ProductCategory.Where(c => c.parentId == category.Id).Where(c => c.activated).ToList();
         }
 
-        /// <summary>
-        /// Finds all ProductCategories that are leaves (that aren't parent of any other ProductCategory).
-        /// </summary>
-        /// <returns>List with all ProductCategory leaves</returns>
         public IEnumerable<ProductCategory> findLeaves()
         {
             //fetch all active parent categories' ids
             var ids = dbContext.ProductCategory.Where(c => c.parentId != null).Select(c => c.parentId).Distinct().ToList();
             
             return dbContext.ProductCategory.Where(c => !ids.Contains(c.Id)).Where(c => c.parentId != null).ToList();
+        }
+
+        public bool isLeaf(ProductCategory category)
+        {
+           return !findSubCategories(category).Any();
         }
     }
 }
