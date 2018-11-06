54b8d6b1558c13ecfc1e62f9736a6ec70c092019
João Freitas <1160907@isep.ipp.pt>
2018-11-06 22:51:49 +0000

    Fixed json_object declaration for distance JSON notation
    see #48

--- MYCL/json_algorithms.pl
+++ MYCL/json_algorithms.pl
@@ -1,20 +1,36 @@
-% JSON Object com a informação basica de um algoritmo
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+
+% JSON Object with the basic information regarding an algorithm
 :- json_object
         basic_algorithm(id:integer, name:string).
 
-% JSON Object com a informação detalhada de um algoritmo
+% JSON Object with the detailed information regarding an algorithm
 :- json_object
         detailed_algorithm(id:integer, name:string).
 
-% JSON Object com a informação sobre uma cidade
+% JSON Object with the detailed information regarding a city
 :- json_object
-        city_object(name:string, latitude:number,longitude:number).
+        city_object(name:atom, latitude:number,longitude:number).
 
-% JSON Object com a informacao sobre um pedido de computação de um circuito sobre cidades
+% JSON Object with the basic information regarding a city
+:- json_object
+        basic_city_object(name:atom).
+
+% JSON Object with the information regarding the request cities computation body request
 :- json_object
         cities_body_request(algorithmID:integer,cities:list).
 
+% JSON Object with the information regarding the distance of a circuit
+:- json_object
+        distance_object(value:number,unit:atom).
 
+% JSON Object with the information regarding the request cities computation body response
+:- json_object
+        cities_body_response(algorithmID:integer,citiesToTravel:list,distance:distance_object/2).
+
+
+% Available algorithms
 algorithm(1,"Branch & Bound").
 algorithm(2,"Shortest Path Greedy Heuristic").
 algorithm(3,"Shortest Path Greedy Heuristic Intersections Optimization").
