60aa254738aa9647b8fefb703917556c8bd02bd1
António Sousa <1161371@isep.ipp.pt>
2018-11-06 22:53:31 +0000

    Establish inheritance between AddMeasurementModelView and AddMeasurementToProductModelView.

--- core/modelview/product/AddMeasurementToProductModelView.cs
+++ core/modelview/product/AddMeasurementToProductModelView.cs
@@ -1,5 +1,6 @@
 using core.dto;
 using core.modelview.dimension;
+using core.modelview.measurement;
 using System.Runtime.Serialization;
 
 namespace core.modelview.product{
@@ -7,28 +8,10 @@ namespace core.modelview.product{
     /// <summary>
     /// A Model View DTO representation for the add dimension to a product context
     /// </summary>
-    public sealed class AddMeasurementToProductModelView{
+    public sealed class AddMeasurementToProductModelView : AddMeasurementModelView{
         /// <summary>
         /// Long with the resource ID of the product which will be complemented
         /// </summary>
         public long productID{get;set;}
-        
-        /// <summary>
-        /// DimensionDTO with the width dimension which will be added to the product
-        /// </summary>
-        [DataMember(Name="width")]
-        public AddDimensionModelView widthDimension{get;set;}
-
-        /// <summary>
-        /// DimensionDTO with the height dimension which will be added to the product
-        /// </summary>
-        [DataMember(Name="height")]
-        public AddDimensionModelView heightDimension{get;set;}
-
-        /// <summary>
-        /// DimensionDTO with the depth dimension which will be added to the product
-        /// </summary>
-        [DataMember(Name="depth")]
-        public AddDimensionModelView depthDimension{get;set;}
     }
 }
