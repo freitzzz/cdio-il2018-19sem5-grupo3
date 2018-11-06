9e7f51a4d272558176ff40dd2df93583e66bf507
João Freitas <1160907@isep.ipp.pt>
2018-11-06 23:01:05 +0000

    Added predicates to compute a city circuit to all available algorithms
    see #48

--- MYCL/algorithm_computation.pl
+++ MYCL/algorithm_computation.pl
@@ -1,11 +1,39 @@
+% Computes B&B for a provided list of cities
 compute_algorithm(1,Cities,CitiesToTravel,Distance):-
     assert_cities(Cities),
-    Cities=[N|_].
-    %tsp1(N,CitiesToTravel,Distance),
-    %retractall(Cities).
+    Cities=[city(N,_,_)|_],
+    tsp1(N,CitiesToTravel,Distance),
+    retract_cities(Cities).
 
+% Computes Greedy Heuristic for a provided list of cities
+compute_algorithm(2,Cities,CitiesToTravel,Distance):-
+    assert_cities(Cities),
+    Cities=[city(N,_,_)|_],
+    tsp2(N,CitiesToTravel,Distance),
+    retract_cities(Cities).
 
+% Computes Greedy Heuristic 2 OPT for a provided list of cities
+compute_algorithm(3,Cities,CitiesToTravel,Distance):-
+    assert_cities(Cities),
+    Cities=[city(N,_,_)|_],
+    tsp3(N,CitiesToTravel,Distance),
+    retract_cities(Cities).
+
+% Computes Genetic Algorithm for a provided list of cities
+compute_algorithm(4,Cities,CitiesToTravel,Distance):-
+    assert_cities(Cities),
+    Cities=[city(N,_,_)|_],
+    tsp4(N,CitiesToTravel,Distance),
+    retract_cities(Cities).
+
+% Asserts a list of cities into the current knowledge base
 assert_cities([]).
 assert_cities([H|T]):-
     assert(H),
     assert_cities(T).
+
+% Retracts a list of cities from the current knowledge base
+retract_cities([]).
+retract_cities([H|T]):-
+    retract(H),
+    retract_cities(T).
