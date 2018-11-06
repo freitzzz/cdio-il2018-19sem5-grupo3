88d7c14a1f0e2d0603a3f25ad40046ced8fa332d
António Sousa <1161371@isep.ipp.pt>
2018-11-06 16:50:50 +0000

    Add CustomizedDimensions ModelViews [re #15].

---
+++ core/modelview/customizeddimensions/AddCustomizedDimensionsModelView.cs
@@ -0,0 +1,39 @@
+using System.Runtime.Serialization;
+
+namespace core.modelview.customizeddimensions
+{
+    /// <summary>
+    /// Class representing the ModelView used for adding CustomizedDimensions.
+    /// </summary>
+    [DataContract]
+    public class AddCustomizedDimensionsModelView
+    {
+        /// <summary>
+        /// CustomizedDimensions' height value.
+        /// </summary>
+        /// <value>Gets/sets the height value.</value>
+        [DataMember]
+        public double height { get; set; }
+
+        /// <summary>
+        /// CustomizedDimensions' width value.
+        /// </summary>
+        /// <value>Gets/sets the width value.</value>
+        [DataMember]
+        public double width { get; set; }
+
+        /// <summary>
+        /// CustomizedDimensions' depth value.
+        /// </summary>
+        /// <value>Gets/sets the depth value.</value>
+        [DataMember]
+        public double depth { get; set; }
+
+        /// <summary>
+        /// CustomizedDimensions' measurement unit.
+        /// </summary>
+        /// <value>Gets/sets the measurement unit.</value>
+        [DataMember]
+        public string unit { get; set; }
+    }
+}
