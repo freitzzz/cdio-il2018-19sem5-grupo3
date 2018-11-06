07625552829d2dc399097be2046d9077c6f79716
António Sousa <1161371@isep.ipp.pt>
2018-11-06 17:01:09 +0000

    Add Measurement modelviews [re #15].

---
+++ core/modelview/measurement/GetMeasurementModelView.cs
@@ -0,0 +1,40 @@
+using System.Runtime.Serialization;
+using core.modelview.dimension;
+
+namespace core.modelview.measurement
+{
+    /// <summary>
+    /// Class representing the ModelView being used for fetching instances of Measurement.
+    /// </summary>
+    [DataContract]
+    public class GetMeasurementModelView
+    {
+        /// <summary>
+        /// Measurement's database identifier.
+        /// </summary>
+        /// <value>Gets/sets the database identifier.</value>
+        [DataMember(Name = "id")]
+        public long measurementId { get; set; }
+
+        /// <summary>
+        /// The Measurement's height Dimension ModelView.
+        /// </summary>
+        /// <value>Gets/sets the height Dimension ModelView.</value>
+        [DataMember]
+        public GetDimensionModelView height { get; set; }
+
+        /// <summary>
+        /// The Measurement's width Dimension ModelView.
+        /// </summary>
+        /// <value>Gets/sets the width Dimension ModelView.</value>
+        [DataMember]
+        public GetDimensionModelView width { get; set; }
+
+        /// <summary>
+        /// The Measurement's depth Dimension ModelView.
+        /// </summary>
+        /// <value>Gets/sets the depth Dimension ModelView.</value>
+        [DataMember]
+        public GetDimensionModelView depth { get; set; }
+    }
+}
