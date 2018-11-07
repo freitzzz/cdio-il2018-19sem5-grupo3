d941c6197a1895a9d7ea584566895ee0f844ee9b
João Freitas <1160907@isep.ipp.pt>
2018-11-07 17:13:15 +0000

    Fixed typos on Location Schema properties and added validations
    for its latitude and longitude
    
    see #38
    
    see #51

--- MYCO/models/Location.js
+++ MYCO/models/Location.js
@@ -5,12 +5,33 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 /**
+ * Validates a location latitude
+ */
+var latitudeValidator={
+    validator:function(latitude){
+        return latitude>=-90 && latitude<=90;
+    },
+    message:props=>`${props.value} is not a valid latitude`
+}
+
+/**
+ * Validates a location longitudde
+ */
+var longitudeValidator={
+    validator:function(longitude){
+        return longitude>=-180 && longitude<=180;
+    },
+    message:props=>`${props.value} is not a valid longitude`
+}
+
+
+/**
  * Represents a Location Schema
  */
 var locationSchema=new Schema({
-    latitude:{type:number,required=true},
-    longitude:{type:number,required=true},
-    _id=false
+    latitude:{type:Number,validate:latitudeValidator,required:true},
+    longitude:{type:Number,validate:longitudeValidator,required:true},
+    _id:false
 });
 
 /**
