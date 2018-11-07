5da574683bd3b7dee7ee122c2632e98f9b272f93
João Freitas <1160907@isep.ipp.pt>
2018-11-07 15:52:17 +0000

    Fixed invalid Factory Schema values
    see #38
    
    see #51

--- MYCO/models/Factory.js
+++ MYCO/models/Factory.js
@@ -5,14 +5,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 /**
- * Represents a schema for a Factory
+ * Represents a Factory Schema
  */
 var factorySchema=new Schema({
-    reference:{type: string, required=true},
-    designation:{type: string, required=true}
+    reference:{type: String, required:true},
+    designation:{type: String, required:true}
 });
 
 /**
  * Exports Factory Data Model required by Mongoose
  */
-var Factory = module.exports = mongoose.model('Factory', factorySchema);
+var Factory = module.exports = mongoose.model('factory', factorySchema);
