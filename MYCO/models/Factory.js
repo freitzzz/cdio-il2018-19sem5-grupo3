695f0aaea1f443ac7b4bf97caa4f45f4adb4a863
João Freitas <1160907@isep.ipp.pt>
2018-11-07 16:36:13 +0000

    Added validators for Factory properties
    see #38
    
    see #51

--- MYCO/models/Factory.js
+++ MYCO/models/Factory.js
@@ -5,12 +5,34 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 /**
+ * Validates a factory reference
+ */
+var referenceValidator={
+    validator:function(reference){
+        return reference.trim().length>0;
+    },
+    message:props=>`${props.value} is not a valid reference`
+}
+
+/**
+ * Validates a factory designation
+ */
+var designationValidator={
+    validator:function(designation){
+        return designation.trim().length>0;
+    },
+    message:props=>`${props.value} is not a valid designation`
+}
+
+/**
  * Represents a Factory Schema
  */
 var factorySchema=new Schema({
-    reference:{type: String, required:true},
-    designation:{type: String, required:true}
+    reference:{type: String, validate:referenceValidator, required:true},
+    designation:{type: String, validate: designationValidator, required:true}
 });
+
+
 
 /**
  * Exports Factory Data Model required by Mongoose
