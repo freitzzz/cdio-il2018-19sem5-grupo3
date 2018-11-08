ad91bba722449d399a81b29bdfe9fe1c2cdd8a97
Gil Durao <1160936@isep.ipp.pt>
2018-11-08 05:26:22 +0000

    Adds heritage from BasicPriceTableEntryModelView to AddFinishPriceTableEntryModelView
    see #4
    see #15

--- core/modelview/pricetableentries/AddFinishPriceTableEntryModelView.cs
+++ core/modelview/pricetableentries/AddFinishPriceTableEntryModelView.cs
@@ -2,9 +2,16 @@ using System.Runtime.Serialization;
 
 namespace core.modelview.pricetableentries
 {
+    /// <summary>
+    /// ModelView that represents the information for creating a finish price table entry
+    /// </summary>
     [DataContract]
-    public class AddFinishPriceTableEntryModelView : AddPriceTableEntryModelView
+    public class AddFinishPriceTableEntryModelView : BasicPriceTableEntryModelView
     {
+        /// <summary>
+        /// Finish's PID
+        /// </summary>
+        /// <value>Gets/Sets the identifier</value>
         [DataMember(Name = "finishId")]
         public long finishId {get; set;}
     }
