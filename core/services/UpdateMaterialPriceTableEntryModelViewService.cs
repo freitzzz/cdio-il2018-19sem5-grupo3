6e2fe9d8dced6765cbf7f5e16cb836c301128cda
Gil Durao <1160936@isep.ipp.pt>
2018-11-08 04:36:59 +0000

    Adds UpdateMaterialPriceTableEntryModelViewService class
    see #1
    see #47

---
+++ core/services/UpdateMaterialPriceTableEntryModelViewService.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using core.domain;
+using core.dto;
+using core.modelview.pricetableentries;
+using core.persistence;
+using NodaTime;
+using NodaTime.Text;
+
+namespace core.services
+{
+    /// <summary>
+    /// Service to help update a material's price table entry
+    /// </summary>
+    public static class UpdateMaterialPriceTableEntryModelViewService
+    {
+        /// <summary>
+        /// Message that occurs if the requested material for the price table entry isn't found
+        /// </summary>
+        private const string MATERIAL_NOT_FOUND = "Requested material wasn't found";
+
+        /// <summary>
+        /// Message that occurs if the requested price table entry to update isn't found
+        /// </summary>
+        private const string ENTRY_NOT_FOUND = "Requested price table entry wasn't found";
+
+        /// <summary>
+        /// Message that occurs if the request price table entry to update isn't updated successfully
+        /// </summary>
+        private const string UPDATE_NOT_SUCCESSFUL = "Update of the price table entry wasn't successful";
+
+        /// <summary>
+        /// Message the occurs if the request material doesn't have any price table entries
+        /// </summary>
+        private const string NO_ENTRIES_FOUND = "The inserted material has no price table entries yet";
+
+        /// <summary>
+        /// Message that occurs if one of the dates of the time period doesn't follow the General ISO format
+        /// </summary>
+        private const string DATES_WRONG_FORMAT = "Make sure all dates follow the General ISO Format: ";
+
+        public static bool update(UpdatePriceTableEntryModelView modelView)
+        {
+            MaterialRepository materialRepository = PersistenceContext.repositories().createMaterialRepository();
+            long materialId = modelView.entityId;
+
+            bool performedAtLeastOneUpdate = false;
+
+            Material material = materialRepository.find(materialId);
+
+            if (material == null)
+            {
+                throw new NullReferenceException(MATERIAL_NOT_FOUND);
+            }
+
+            MaterialPriceTableRepository materialPriceTableRepository = PersistenceContext.repositories().createMaterialPriceTableRepository();
+            long materialPriceTableEntryId = modelView.tableEntryId;
+
+            IEnumerable<MaterialPriceTableEntry> allEntries = materialPriceTableRepository.findAll();
+
+            if (allEntries == null || !allEntries.Any())
+            {
+                throw new NullReferenceException(NO_ENTRIES_FOUND);
+            }
+
+            MaterialPriceTableEntry tableEntryToUpdate = materialPriceTableRepository.find(materialPriceTableEntryId);
+
+            if (tableEntryToUpdate == null)
+            {
+                throw new NullReferenceException(ENTRY_NOT_FOUND);
+            }
+
+            if (modelView.priceTableEntry.price != null)
+            {
+                //TODO Take into account currency and area conversion
+                //!For now we are considering all prices are in €/m2
+                Price newPrice = Price.valueOf(modelView.priceTableEntry.price.value);
+                tableEntryToUpdate.changePrice(newPrice);
+                performedAtLeastOneUpdate = true;
+            }
+
+            //TODO check what's more efficient: always create 2 VOs or chain ifs to know what dates can be ignored
+            if (modelView.priceTableEntry.startingDate != null)
+            {
+                LocalDateTime newStartingDate;
+                try
+                {
+                    string newStartingDateAsString = modelView.priceTableEntry.startingDate;
+                    newStartingDate = LocalDateTimePattern.GeneralIso.Parse(newStartingDateAsString).GetValueOrThrow();
+                    tableEntryToUpdate.changeTimePeriod(TimePeriod.valueOf(newStartingDate, tableEntryToUpdate.timePeriod.endingDate));
+                    performedAtLeastOneUpdate = true;
+                }
+                catch (UnparsableValueException unparsableValueException)
+                {
+                    throw new UnparsableValueException(DATES_WRONG_FORMAT + LocalDateTimePattern.GeneralIso.PatternText);
+                }
+            }
+
+            if (modelView.priceTableEntry.endingDate != null)
+            {
+                LocalDateTime newEndingDate;
+                try
+                {
+                    string newEndingDateAsString = modelView.priceTableEntry.endingDate;
+                    newEndingDate = LocalDateTimePattern.GeneralIso.Parse(newEndingDateAsString).GetValueOrThrow();
+                    tableEntryToUpdate.changeTimePeriod(TimePeriod.valueOf(tableEntryToUpdate.timePeriod.startingDate, newEndingDate));
+                    performedAtLeastOneUpdate = true;
+                }
+                catch (UnparsableValueException unparsableValueException)
+                {
+                    throw new UnparsableValueException(DATES_WRONG_FORMAT + LocalDateTimePattern.GeneralIso.PatternText);
+                }
+            }
+
+
+            MaterialPriceTableEntry updatedTableEntry = materialPriceTableRepository.update(tableEntryToUpdate);
+
+            if (updatedTableEntry == null)
+            {
+                throw new InvalidOperationException(UPDATE_NOT_SUCCESSFUL);
+            }
+
+            return performedAtLeastOneUpdate;
+        }
+    }
+}
