a774c07deede3fb1776a5732b12a68dc47d7d8fb
Ricardina <1160941@isep.ipp.pt>
2018-11-08 09:40:43 +0000

    #22 Creation of several mode views

---
+++ core/modelview/commercialcatalogue/CommercialCatalogueModelViewService.cs
@@ -0,0 +1,27 @@
+using core.domain;
+
+namespace core.modelview.commercialcatalogue
+{
+    /// <summary>
+    /// Service class used for converting instances of Commercial Catalogues to ModelViews.
+    /// </summary>
+    public class CommercialCatalogueModelViewService
+    {
+          /// <summary>
+        /// Builds an instance of GetCommercialCatalogueModelView from an instance of CommercialCatalogue.
+        /// </summary>
+        /// <param name="commercialCatalogue">Instance of CommercialCatalogue from which the ModelView will be built.</param>
+        /// <returns>An instance of GetCommercialCatalogueModelView.</returns>
+        public static GetCommercialCatalogueModelView fromEntity(CommercialCatalogue commercialCatalogue)
+        {
+            GetCommercialCatalogueModelView commercialCatalogueView = new GetCommercialCatalogueModelView();
+
+            commercialCatalogueView.id = commercialCatalogue.Id;
+            commercialCatalogueView.reference = commercialCatalogue.reference;
+            commercialCatalogueView.designation = commercialCatalogue.designation;
+            commercialCatalogueView.collectionList = commercialCatalogue.catalogueCollectionList;
+            return commercialCatalogueView;
+        }
+
+    }
+}
