ce755afc5c69f20c0811bd984301e2ab3a642872
João Freitas <1160907@isep.ipp.pt>
2018-11-08 23:00:59 +0000

    When creating a new order, a production factory which is the shortest
    to the order city is provided
    
    see #38
    
    see #52

--- MYCO/routes/orders.js
+++ MYCO/routes/orders.js
@@ -4,6 +4,8 @@ const Order = require('../models/order');
 const Factory=require('../models/Factory');
 const http = require('http');
 const City = require('../models/City');
+const axios=require('axios');
+const config=require('../config');
 
 //Get all orders in the database
 //Handle errors by using the ones available in the mongoose schema
@@ -97,75 +99,104 @@ function getCustomizedProduct(customizedProductId) {
 }
 
 
-//Creates a new Order
-//TODO Handle errors by using the ones available in the mongoose schema
-ordersRoute.route('/orders').post(function (req, res, next) {
-    if(!req.body.cityToDeliverId.match(/^[0-9a-fA-F]{24}$/)){
-        res.status(400).json({
-            Error : 'Invalid City Id. Please try again'
-        })
-
-
-
-
-
-
-
-
-
-
 function get_shortest_factory_between_city(city){
     Factory
         .find()
             .then(function(availableFactories){
                 availableFactories.forEach(function(factory){
                     if(factory.isLocated(city)){
-                        console.log(city);
+                        return city;
                     }
                 })
             })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-    City.findById(req.body.cityToDeliverId).then(function (error, city) {
+//Creates a new Order
+//TODO Handle errors by using the ones available in the mongoose schema
+ordersRoute.route('/orders').post(function (req, res, next) {
+    /* if(!req.body.cityToDeliverId.match(/^[0-9a-fA-F]{24}$/)){
+        res.status(400).json({
+            Error : 'Invalid City Id. Please try again'
+        })
+    } */
+    City.findById(req.body.cityToDeliverId).then(function (city,error) {
         if (error) {
+            console.log(city);
             res.status(404).json({
                 Error: 'City not found. Please try again'
             });
 
         } else {
-            Order.create({
-                    orderContents: req.body.orderContents,
-                    cityToDeliver: city
-                },
-                function (error, order) {
-                    if (error) {
-                        res.status(400).json({
-                            Error: 'The order body is invalid. Please try again'
-                        });
-                    } else {
-                        res.status(201).json(order);
-                    }
-                })
+            Factory
+                .find()
+                    .then(async function(availableFactories){
+                        let factories=[];
+                        availableFactories.forEach(function(factory){
+                            if(factory.isLocated(city)){factories.push(factory);}
+                        })
+                        let shortestFactory;
+                        if(factories.length==1){
+                            shortestFactory=factories.pop();
+                        }else{
+                            fetchShortestFactory(city,factories)
+                                .then(function(body){
+                                    let _factory=factoryByName(factories,body.factory.name);
+                                    Order.create({
+                                        orderContents:req.body.orderContents,
+                                        cityToDeliver:city,
+                                        factoryOfProduction:_factory
+                                    })
+                                    .then(function(createdOrder){
+                                        return res.status(201).json(createdOrder);
+                                    }).catch(function(_error){
+                                        return res.status(400).json(_error);
+                                    })
+                                })
+                        }
+                        
+                    })
         }
     })
 })
+
+async function fetchShortestFactory(city,factories){
+    return new Promise((resolve,reject)=>{
+        let _requestBody={city:serializeCity(city),factories:serializeFactories(factories)};
+        axios.post(config.MYCL_URL+"mycl/api/factories",_requestBody)
+                .then(function(response){
+                    resolve(response.data);
+                })
+                .catch(function(_error){
+                    reject(_error);
+                })
+    })
+}
+
+function serializeCity(city){
+    return {
+        name:city.name,
+        latitude:city.location.latitude,
+        longitude:city.location.longitude
+    }
+}
+
+function serializeFactory(factory){
+    return {
+        name:factory.reference,
+        latitude:factory.location.latitude,
+        longitude:factory.location.longitude
+    }
+}
+
+function serializeFactories(factories){
+    let serializedFactories=[];
+    factories.forEach((factory)=>{serializedFactories.push(serializeFactory(factory));});
+    return serializedFactories;
+}
+
+function factoryByName(factories,name){
+    for(let i=0;i<factories.length;i++)
+        if(factories[i].reference==name)return factories[i];
+}
+
 module.exports = ordersRoute;
