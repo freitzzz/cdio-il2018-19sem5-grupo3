61ef01dff2eb5cb12c0b33b9a6cf88e6abe71baa
João Freitas <1160907@isep.ipp.pt>
2018-11-08 12:18:52 +0000

    Added entity framework implementations for the Material and Finish Price Tables
    repositories
    
    see #16
    
    see #49

--- backend/persistence/ef/EFFinishPriceTableRepository.cs
+++ backend/persistence/ef/EFFinishPriceTableRepository.cs
@@ -1,5 +1,8 @@
 using core.domain;
+using core.dto;
 using core.persistence;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace backend.persistence.ef
 {
@@ -12,5 +15,18 @@ namespace backend.persistence.ef
     public class EFFinishPriceTableRepository : EFBaseRepository<FinishPriceTableEntry, long, string>, FinishPriceTableRepository
     {
         public EFFinishPriceTableRepository(MyCContext dbContext) : base(dbContext) { }
+
+        /// <summary>
+        /// Fetches the price history of a material finish
+        /// </summary>
+        /// <param name="fetchMaterialFinishPriceHistoryDTO">FetchMaterialFinishPriceHistoryDTO with the information about the fetch</param>
+        /// <returns>IEnumerable with the material finish price history</returns>
+        public IEnumerable<FinishPriceTableEntry> fetchMaterialFinishPriceHistory(FetchMaterialFinishPriceHistoryDTO fetchMaterialFinishPriceHistoryDTO){
+            return (from finishPriceTableEntry in base.dbContext.FinishPriceTable
+                    from materialPriceTableEntry in dbContext.MaterialPriceTable
+                    where materialPriceTableEntry.Id==fetchMaterialFinishPriceHistoryDTO.materialID
+                    where finishPriceTableEntry.Id==fetchMaterialFinishPriceHistoryDTO.finishID
+                    select finishPriceTableEntry);
+        }
     }
 }
