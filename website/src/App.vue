95a0bf614a2eeeb20942bbdfeb0cee054efc5b91
António Sousa <1161371@isep.ipp.pt>
2018-11-15 09:42:39 +0000

    Add Vue website's initial commit.

---
+++ website/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+import Product from "./components/Product.vue";
+import Customizer from "./components/Customizer.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Make Your Closet"
+    };
+  },
+  methods: {
+    loadCustomizer(){
+    }
+  },
+  components: {
+    Customizer
+  }
+};
+</script>
---
+++ website/src/components/Customizer.vue
@@ -0,0 +1,65 @@
+<template>
+</template>
+
+<script>
+export default {
+  data() {},
+  //*Change the functions so that they don't access the DOM
+  methods: {
+    loadCustomizer() {
+      alert("load the customizer canvas");
+    },
+    reloadCube() {
+      var event = new CustomEvent("changeDimensions", {
+        detail: {
+          height: height,
+          width: width,
+          depth: depth
+        }
+      });
+      document.dispatchEvent(event);
+    },
+    reloadClosetSlots() {
+      var slotDiv = document.getElementById("slotDiv");
+      var length = slotDiv.childNodes.length - 1;
+      var list = [];
+      while (length >= 1) {
+        list.push(slotDiv.childNodes[length].childNodes[1].nodeValue);
+        length--;
+      }
+      var eventX = new CustomEvent("changeSlots", {
+        detail: {
+          slots: document.getElementById("slotsInput").value,
+          slotWidths: list
+        }
+      });
+      document.dispatchEvent(eventX);
+      manageSlotSliders();
+    },
+    reloadMaterial(elementId) {
+      var event = new CustomEvent("changeMaterial", {
+        detail: {
+          //this should the be image's link
+          material: document.getElementById(elementId).getAttribute("src")
+        }
+      });
+      document.dispatchEvent(event);
+    },
+    changeShininess(shininess) {
+      var event = new CustomEvent("changeShininess", {
+        detail: {
+          shininess: shininess
+        }
+      });
+      document.dispatchEvent(event);
+    },
+    changeColor(color) {
+      var event = new CustomEvent("changeColor", {
+        detail: {
+          color: color
+        }
+      });
+    }
+  }
+};
+</script>
