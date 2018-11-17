5a3e74047fcb2fb72748e6ba409154627eedd8c1
João Freitas <1160907@isep.ipp.pt>
2018-11-17 21:00:25 +0000

    Added initial MYCL SPA
    see #48

---
+++ MYCL/frontend/lexpress/src/components/UIComponents/ClickableTable.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="header">
+            <slot name="header">
+                <h4 class="title">{{title}}</h4>
+            </slot>
+        </div>
+        <div class="content table-responsive table-full-width">
+            <b-table
+                :columns="columns"
+                :data="data"
+                >
+            </b-table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        columns: Array,
+        data: Array,
+        title: String
+    }
+}
+</script>
