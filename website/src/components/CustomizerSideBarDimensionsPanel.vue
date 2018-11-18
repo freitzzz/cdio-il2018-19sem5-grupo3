815ec3904e5307d483573e5ee42c86c7d4eadefb
António Sousa <1161371@isep.ipp.pt>
2018-11-18 18:11:11 +0000

    Add rudimentary sidebar panel for dimension configuration

--- website/src/components/CustomizerSideBarDimensionsPanel.vue
+++ website/src/components/CustomizerSideBarDimensionsPanel.vue
@@ -1,12 +1,28 @@
 <template>
-    
+    <div>
+      <!--Fetch minimums from server-->
+      <input type="number" min="1" name="height" v-model="height">
+      <input type="number" min="1" name="width" v-model="width">
+      <input type="number" min="1" name="depth" v-model="depth">
+      <select>
+        <option value="mm">Milimeters</option>
+        <option value="cm">Centimeters</option>
+        <option value="dm">Decimeters</option>
+        <option value="m">Meters</option>
+      </select>
+    </div>
 </template>
 
 <script>
 export default {
   name: "CustomizerSideBarDimensionsPanel",
   data() {
-    return {};
+    return {
+      //TODO: replace hardcoded values ASAP
+      height: 50,
+      width: 409,
+      depth: 100
+    };
   }
 };
 </script>
