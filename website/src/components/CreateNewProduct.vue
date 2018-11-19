3b43ed74688ea0eb13d5df48f8318e600cda6b80
João Freitas <1160907@isep.ipp.pt>
2018-11-19 18:39:59 +0000

    CreateNewProduct modal can now add components

--- website/src/components/CreateNewProduct.vue
+++ website/src/components/CreateNewProduct.vue
@@ -1,6 +1,5 @@
 <template>
     <b-modal :active.sync="active" has-modal-card>
-        <form action="">
                 <div class="modal-card" style="width: auto">
                     <header class="modal-card-head">
                         <p class="modal-card-title">New Product</p>
@@ -38,21 +37,38 @@
                             </b-select>
                         </b-field>
                         <b-field label="Materials">
-                            <b-input
-                                type="String"
-                                :value.sync="name"
-                                placeholder="Select"
-                                icon="brush"
-                                required>
-                            </b-input>
+                            <b-select placeholder="MDF" icon="brush">
+                                <option value="flint">MDF</option>
+                                <option value="flint">Cherry</option>
+                                <option value="flint">Orange</option>
+                            </b-select>
+                            <div class="has-text-centered">
+                                <button class="button is-danger" @click="showMaterials">
+                                    <b-icon icon="magnify"/>
+                                </button>
+                            </div>
                         </b-field>
-                        <b-checkbox>Components</b-checkbox>
+                        <b-checkbox @input="enableComponents()">Components</b-checkbox>
+                        <div v-if="components">
+                            <b-field label="Components">
+                                <b-select placeholder="Kelf" icon="archive">
+                                    <option value="flint">MDF</option>
+                                </b-select>
+                                <div class="has-text-centered">
+                                    <button class="button is-danger" @click="showMaterials">
+                                        <b-icon icon="magnify"/>
+                                    </button>
+                                </div>
+                            </b-field>
+                        </div>
                     </section>
                     <footer class="modal-card-foot">
-                        <button class="button is-primary" @click="create($parent)">Create</button>
+                        <div class="has-text-centered">
+                            <button class="button is-primary" @click="emitProduct($parent)">Create</button>
+                        </div>
                     </footer>
                 </div>
-            </form>
+   
     </b-modal>
 </template>
 
@@ -62,9 +78,30 @@ export default {
         active:{
             type: Boolean,
             default: false
+        },
+        reference:String,
+        designation:String,
+        category:String,
+    },
+    data(){
+        return{
+            components:false
         }
     },
     methods:{
+        emitProduct(modal){
+            //modal.close();
+            this.$emit('emitProduct',{});
+        },
+        enableComponents(){
+            this.components=!this.components;
+        },
+        showMaterials(){
+            //TODO: Open modal with all available materials
+        },
+        showComponents(){
+            //TODO: Open modal with all available components
+        }
     }
 }
 </script>
