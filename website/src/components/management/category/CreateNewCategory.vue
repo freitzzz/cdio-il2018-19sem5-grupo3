2ddc088fc2372c42da04b4c9117ea95badb79307
桜ちゃん <1160941@isep.ipp.pt>
2018-11-20 00:00:06 +0000

    REfractor and added possibility of parameter v-model
    see #60

--- website/src/components/management/category/CreateNewCategory.vue
+++ website/src/components/management/category/CreateNewCategory.vue
@@ -7,28 +7,30 @@
                     </header>
                     <section class="modal-card-body">
                         <b-field label="Name">
-                            <b-input
+                            <b-input 
+                                v-model="nameCategory"
                                 type="String"
-                                :value.sync="reference"
-                                placeholder="#666"
+                                placeholder="#Category"
                                 icon="pound"
                                 required>
                             </b-input>
-                        </b-field>
-    
-                        <b-field label="Parent Category">
-                            <b-select placeholder="Select a category" icon="tag">
-                                <optgroup label="Wardrobes">
-                                    <option value="flint">Classy</option>
-                                    <option value="flint">Devil</option>
-                                    <option value="flint">Professional</option>
+                        </b-field>                     
+                        <b-field label="Category">
+                            <b-select v-model="parentCategory" placeholder="Select a category" icon="tag" >
+                                <optgroup label="Parent Categories">
+                                <!--  <ul id = "example-1"> <li v-for = "item in items">     {{ item.message }} </li > </ul > -->
+
+                                    <option value="flint">---</option>
+                                    <option value="flint">Category 1</option>
+                                    <option value="flint">Category 2</option>
+                                    <option value="flint">Category 3</option>
                                 </optgroup>
                             </b-select>
                         </b-field>
                      
                     </section>
                     <footer class="modal-card-foot">
-                        <button class="button is-primary" @click="create($parent)">Create</button>
+                        <button class="button is-primary" @click="postCategory">Create</button>
                     </footer>
                 </div>
             </form>
@@ -36,15 +38,57 @@
 </template> 
 
 <script>
+import Axios from "axios";
 export default {
-    name:"CreateNewCategory",
-    props:{
-        active:{
-            type: Boolean,
-            default: false
-        }
-    },
-    methods:{
+  name: "CreateNewCategory",
+  data() {
+    return {
+      nameCategory: "",
+      parentCategory: ""
+    };
+  },
+  props: {
+    active: {
+      type: Boolean,
+      default: false
     }
-}
+  },
+
+  /*  */
+  methods: {
+    postCategory() {
+      if (new String(parentCategory).equals("---")) {
+        /* Post with just a name */
+        Axios.post("http://localhost:5000/mycm/api/categories", {
+          name: nameCategory
+        })
+          .then(function(response) {
+            alert("Vinho");
+            console.log(response);
+          })
+          .catch(function(error) {
+            alert("Vinho Branco");
+            console.log(error);
+          });
+      } else {
+        /* Post with just a name */
+        Axios.post(
+          "http://localhost:5000/mycm/api//categories/parentCategory/subcategories",
+          {
+            name: nameCategory
+          }
+        )
+          .then(function(response) {
+            console.log(response);
+          })
+          .catch(function(error) {
+            console.log(error);
+          });
+      }
+    }
+  }
+  /*  created() {
+    this.getCategories();
+  } */
+};
 </script>
