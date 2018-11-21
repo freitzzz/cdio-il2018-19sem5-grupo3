ef8543c5052e018b33291264706830858081c285
桜ちゃん <1160941@isep.ipp.pt>
2018-11-21 16:56:53 +0000

    Added the possibility of showing the name of the category
    see #60

--- website/src/components/management/category/EditCategory.vue
+++ website/src/components/management/category/EditCategory.vue
@@ -8,28 +8,26 @@
                     <section class="modal-card-body">
                                             
                         <b-field label="Category">
-                            <b-select placeholder="Select a category" icon="tag" v-model="nameCategory">
+                            <b-select placeholder="Select a category" icon="tag" v-model="categoryData">
                                   <option :value="null"></option>
                                   <option v-for="category in availableCategories" 
-                                    :key="category.id" :value="category.name">{{category.name}}</option>
+                                    :key="category.id" :value="category.id">{{category.name}} </option>
                             </b-select>
+                            
                         </b-field>
-                        <b-field label="Name">
+                        <b-field label="Name" >
                             <b-input 
-                                v-bind:value = "nameCategory"
+                                v-model="nameCategory"
                                 type="String"
                                 placeholder="Category"
                                 icon="pound"
                                 >
-                                
                             </b-input>
                             
                         </b-field> 
-                     
                     </section>
                     <footer class="modal-card-foot">
-                        <button class="button is-primary" @click="postCategory">Create</button>
-                        
+                        <button class="button is-primary" @click="editCategory">Edit</button>                    
                     </footer>
                 </div>
             </form>
@@ -39,10 +37,11 @@
 <script>
 import Axios from "axios";
 export default {
-  name: "CreateNewCategory",
+  name: "EditCategory",
   data() {
     return {
-      nameCategory: "",
+      categoryData: "",
+      nameCategory:"",
       //categoryId: null, //this value needs to be null for the placeholder to work
       availableCategories: []
     };
@@ -56,31 +55,51 @@ export default {
 
   /*  */
   methods: {
-    postCategory() {
-      if (this.categoryId === null) {
-        /* Post with just a name */
-        Axios.post("http://localhost:5000/mycm/api/categories", {
-          name: this.nameCategory
-        })
-          .then(response => {})
-          .catch(error => {});
-      } else {
-        /* Post with just a name */
-        Axios.post(
-          `http://localhost:5000/mycm/api/categories/${
-            this.categoryId
-          }/subcategories`,
-          {
-            name: this.nameCategory
-          }
-        )
-          .then(response => {})
-          .catch(error => {});
-      }
-    },
-    getName(){
-        return this.nameCategory;
+ /*    getCategory() {
+      alert("entrou");
+      Axios.get(
+        `http://localhost:5000/mycm/api/categories/${this.categoryData}`
+      )
+        .then(response => nameCategory.value) //push all elements onto the array
+        .catch(function(error) {
+          //TODO: inform an error occured while fetching categories
+        });
+    }, */
+    editCategory() {
+      alert(this.categoryData);
+      Axios.delete(
+        `http://localhost:5000/mycm/api/categories/?name=${this.categoryData}`
+      )
+        .then(response => {})
+        .catch(error => {
+          console.log("NAO DEU");
+        });
+      /*Post with just a name */
+      /* Axios.put(
+        `http://localhost:5000/mycm/api/categories/${this.categoryData}`,
+         {
+           name: this.categoryData.value
+         }
+       )
+         .then(response => {})
+         .catch(error => {}); */
     }
+    /*  putCategory: function() {
+      var formData = new FormData();
+      formData.append("name", this.categoryData.value);
+      this.$http.put(
+        `http://localhost:5000/mycm/api/categories/$this.categoryData.text`,
+        formData
+      );
+      success(function() {
+        this.modalName.showMod = false;
+        location.reload();
+      }).error(function(data) {
+        this.errors.title = data.title;
+        this.errors.body = data.body;
+        this.modalName.showMod = true;
+      });
+    } */
   },
   created() {
     Axios.get("http://localhost:5000/mycm/api/categories")
