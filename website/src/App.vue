da7a7e8d2bb765b732f47511efa21418fe68ba05
桜ちゃん <1160941@isep.ipp.pt>
2018-11-22 18:32:51 +0000

    Added side bar

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,14 +1,20 @@
 <template>
     <div id="app">
-       <customizer v-if="customizerEnabled"></customizer>
-      <button v-else @click="enableCustomizer">Get Started</button> 
+      <!--  <customizer v-if="customizerEnabled"></customizer>
+      <button v-else @click="enableCustomizer">Get Started</button>  -->
       <!--create-new-category :active=true></create-new-category-->
+      <side-bar></side-bar>
+      <!-- <edit-category :active="true"></edit-category> -->
+     <!--  <remove-category :active="true"></remove-category> -->
     </div>
 </template>
 
 <script>
 import Customizer from "./components/Customizer.vue";
 import CreateNewCategory from "./components/management/category/CreateNewCategory.vue";
+import EditCategory from "./components/management/category/EditCategory.vue";
+import RemoveCategory from "./components/management/category/RemoveCategory.vue";
+import SideBar from "./components/SideBar.vue";
 export default {
   name: "app",
   data() {
@@ -23,7 +29,10 @@ export default {
   },
   components: {
     Customizer,
-    CreateNewCategory
+    CreateNewCategory,
+    SideBar ,
+    EditCategory,
+    RemoveCategory
   }
 };
 </script> 
