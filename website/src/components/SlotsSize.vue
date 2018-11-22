dad473adbb1e22031a29af2728193327d9cde163
João Freitas <1160907@isep.ipp.pt>
2018-11-22 00:35:27 +0000

    Added SlotsSize Vue Component
    see #39
    
    see #61

---
+++ website/src/components/SlotsSize.vue
@@ -0,0 +1,46 @@
+<template>
+    <section>
+        <b-field :label="slotName"/>
+        <b-field>
+            <b-field label="Width">
+                <b-input
+                    type="Number"
+                    :value.sync="minSlotSize"
+                    placeholder="200"
+                    icon="pound"
+                    required>
+                </b-input>
+            </b-field>
+            <b-field label="Height">
+                <b-input
+                    type="Number"
+                    :value.sync="minSlotSize"
+                    placeholder="200"
+                    icon="pound"
+                    required>
+                </b-input>
+            </b-field>
+            <b-field label="Depth">
+                <b-input
+                    type="Number"
+                    :value.sync="minSlotSize"
+                    placeholder="200"
+                    icon="pound"
+                    maxlength="1"
+                    required>
+                </b-input>
+            </b-field>
+        </b-field>
+    </section>
+</template>
+
+<script>
+export default {
+    props:{
+        slotName:{
+            type:String,
+            required:true
+        }
+    }
+};
+</script>
