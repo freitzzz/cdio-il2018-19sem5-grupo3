ccf0d5ad833d2b87f413f7b8522f08602a1cd535
桜ちゃん <1160941@isep.ipp.pt>
2018-11-26 17:55:43 +0000

    Created Home + Intro page for website
    see #60

---
+++ website/src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="currentComp" @switch-to-customizer="switchPage()"></component>
+</template>
+<script>
+import Customizer from "./Customizer.vue";
+import Intro from "./Intro.vue";
+export default {
+  name: "home",
+  data() {
+    return {
+
+      currentComp:Intro
+    };
+  },
+  methods: {
+    switchPage() {
+  
+      this.currentComp = Customizer;
+    }
+  },
+  components: {
+    Customizer,
+    Intro
+  }
+};
+</script>
+
