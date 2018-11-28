42777c390a4bbbd38eb63e46020305fee9de40eb
João Freitas <1160907@isep.ipp.pt>
2018-11-28 18:01:57 +0000

    MYCA can now process the POST of a new simple auth
    see #64
    
    see #65

--- MYCA/myca_auth/src/controller/myca_auth_auth_controller.erl
+++ MYCA/myca_auth/src/controller/myca_auth_auth_controller.erl
@@ -10,4 +10,14 @@
 % By accessing /auth/default we get the defined output
 default('GET', []) ->
     L=boss_db:find(simpleauth,[]), % Finds all simpleauth records
-    {json, [{auths,L}]}. % Serializes a JSON with the available simpleauth records
+    {json, L}. % Serializes a JSON with the available simpleauth records
+
+% Routes a POST method to a "default2" endpoint
+post('POST',[]) ->
+    ReqBody=Req:request_body(),
+    JSON=jsx:decode(ReqBody),
+    Username=proplists:get_value(<<"username">>,JSON),
+    Password=proplists:get_value(<<"password">>,JSON),
+    SimpleAuth=simpleauth:new(id,Username,Password),
+    {ok,SavedSimpleAuth}=SimpleAuth:save(),
+    {json,SavedSimpleAuth}.
