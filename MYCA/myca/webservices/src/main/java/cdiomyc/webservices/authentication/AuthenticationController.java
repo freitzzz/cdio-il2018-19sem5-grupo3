624319ea2c11d8b7bef8c0e88ee73db6050530d0
João Freitas <1160907@isep.ipp.pt>
2018-11-29 19:17:34 +0000

    AuthenticationController can now process authenticate request
    see #64
    
    see #65

--- MYCA/myca/webservices/src/main/java/cdiomyc/webservices/authentication/AuthenticationController.java
+++ MYCA/myca/webservices/src/main/java/cdiomyc/webservices/authentication/AuthenticationController.java
@@ -1,6 +1,11 @@
 package cdiomyc.webservices.authentication;
 
 import cdiomyc.core.mv.authentication.AuthenticationMV;
+import cdiomyc.core.mv.authentication.AuthenticationMVService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.lang.reflect.Type;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,14 +21,34 @@ public final class AuthenticationController {
     
     /**
      * Processes the authenticate into MYC API's request
-     * @param authenticationModelView AuthenticationMV with the authentication 
+     * @param authenticationDetails String with the authentication 
      * details
      * @return HTTP Response 200; OK with the API token
      * <br>    HTTP Response 401; Unauthorized if the authentication is invalid
      */
     @POST
     @Consumes(value = MediaType.APPLICATION_JSON)
-    public Response authenticate(AuthenticationMV authenticationModelView){
+    public Response authenticate(String authenticationDetails){
         throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * Deserializes the authentication details to the respective model view
+     * @param authenticationDetails String with the authentication details
+     * @return AuthenticationMV with the deserialized authentication model view
+     */
+    private static AuthenticationMV deserializeAuthenticationDetails(String authenticationDetails){
+        return (AuthenticationMV)new Gson()
+                .fromJson(authenticationDetails
+                        ,AuthenticationMVService
+                                .classFromType(new Gson()
+                                        .fromJson(authenticationDetails,AuthenticationType.class)
+                                            .type));
+    }
+    /**
+     * Simple inner static class to deserialize the type of an authentication request body
+     */
+    private static class AuthenticationType{
+        public String type;
     }
 }
