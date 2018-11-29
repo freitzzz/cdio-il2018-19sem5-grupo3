18e3f79194828dd5a7aa5dd40d455b8ac6fccb6d
桜ちゃん <1160941@isep.ipp.pt>
2018-11-29 12:45:51 +0000

    Centralized the components of the dimension panel
    see #61

--- website/src/components/CustomizerSideBarDimensionsPanel.vue
+++ website/src/components/CustomizerSideBarDimensionsPanel.vue
@@ -1,24 +1,52 @@
 <template>
-  
-      <div class = "containerDimensions">
-      <!--Fetch minimums from server-->
-      <input class="baseContainer" type="range" min="1" name="height" v-model="height" @change="updateHeight">
-      <input class="baseContainer" type="range" min="1" name="width" v-model="width" @change="updateWidth">
-      <input class="baseContainer" type="range" min="1" name="depth" v-model="depth" @change="updateDepth">
-        
-      <select v-model="unit" @change="updateUnit" >
-        <option value="mm">Milimeters</option>
-        <option value="cm">Centimeters</option>
-        <option value="dm">Decimeters</option>
-        <option value="m">Meters</option>
-      </select>
- 
-      </div>
+  <div class="containerDimensions" style="margin-left:30%;margin-right:auto;">
+    <div class="icon-div-top">
+      <i class="material-icons md-12 md-blue btn">help</i>
+      <span
+        class="tooltiptext">In this step, you must choose one of our base products in order to start customizing it.</span>
+    </div>
+    <select v-model="dimensionOp" @change="updateUnit" style="margin-bottom:10%">
+      <option  v-for="option in availableOptions" :key="option.id" :value="option">{{"Option: "+option.id}}</option>
+    </select>
+    <!--Fetch minimums from server-->
+    <input
+      class="slider"
+      type="range"
+      min="1"
+      name="height"
+      v-model="height"
+      @change="updateHeight"
+    >
+    <input
+      class="slider"
+      type="range"
+      min="1"
+      name="width"
+      v-model="width"
+      @change="updateWidth"
+    >
+    <input
+      class="slider"
+      type="range"
+      min="1"
+      name="depth"
+      v-model="depth"
+      @change="updateDepth"
+    >
+    
+    <select class="dropdown" v-model="unit" @change="updateUnit">
+      <option value="mm">Milimeters</option>
+      <option value="cm">Centimeters</option>
+      <option value="dm">Decimeters</option>
+      <option value="m">Meters</option>
+    </select>
+  </div>
 </template>
 
 
 <script>
 import store from "./../store";
+import Axios from "axios";
 import {
   SET_CUSTOMIZED_PRODUCT_WIDTH,
   SET_CUSTOMIZED_PRODUCT_HEIGHT,
@@ -36,7 +64,9 @@ export default {
       height: 100,
       width: 100,
       depth: 100,
-      unit: "cm"
+      unit: "cm",
+      dimensionOp: "",
+      availableOptions: []
     };
   },
   created() {
@@ -44,6 +74,15 @@ export default {
     store.dispatch(SET_CUSTOMIZED_PRODUCT_HEIGHT, { height: this.height });
     store.dispatch(SET_CUSTOMIZED_PRODUCT_DEPTH, { depth: this.depth });
     store.dispatch(SET_CUSTOMIZED_PRODUCT_UNIT, { unit: this.unit });
+
+    /*Get all available dimensions of the array*/
+    Axios.get(
+      `http://localhost:5000/mycm/api/products/${
+        store.state.product.id
+      }/dimensions`
+    )
+      .then(response => this.availableOptions.push(...response.data))
+      .catch(console.log());
   },
   methods: {
     updateHeight(e) {
@@ -57,20 +96,54 @@ export default {
     },
     updateUnit(e) {
       store.dispatch(SET_CUSTOMIZED_PRODUCT_UNIT, { unit: e.target.value });
-    }
+    },
+ /*    //Get all available options
+    populateAvailableOptions() {} */
   }
 };
 </script>
 <style>
 .containerDimensions {
-  margin: 5%;
-  margin-left:22.5%;
-  margin-right: 22.5%;
-  align-content: left;
-  font-family: 'Roboto', sans-serif;  
+  margin: 5%;/*
+  margin-left: 22.5%;
+  margin-right: 22.5%; */
+ border-bottom: 10%;
+  font-family: "Roboto", sans-serif;
 }
 
+.icon-div-center {
+  text-align: center;
+}
 
+.icon-div-top .tooltiptext {
+  visibility: hidden;
+  width: 120px;
+  background-color: #797979;
+  color: #fff;
+  border-radius: 6px;
+  font-size: 12px;
+  padding: 10%;
+  position: absolute;
+}
+
+.icon-div-top:hover .tooltiptext {
+  visibility: visible;
+}
+
+.icon-div-top {
+  top: 16.5px;
+  left: 27px;
+  margin-left: 100px;
+  position: absolute;
+}
+.dropdown{
+  width: 60%;
+  margin-bottom:10%;
+}
+.slider{
+  width:60%;
+  margin-bottom:5%;
+}
 </style>
 
 
