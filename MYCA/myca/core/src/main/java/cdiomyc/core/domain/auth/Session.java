e1208dede18248536d00ec2902391448b43e0b5e
João Freitas <1160907@isep.ipp.pt>
2018-11-29 23:26:14 +0000

    Session class now has a token that represents the session token
    see #64
    
    see #65

--- MYCA/myca/core/src/main/java/cdiomyc/core/domain/auth/Session.java
+++ MYCA/myca/core/src/main/java/cdiomyc/core/domain/auth/Session.java
@@ -1,19 +1,34 @@
 package cdiomyc.core.domain.auth;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  * Represents an authentication session
  * @author <a href="https://github.com/freitzzz">freitzzz</a>
  */
-@Embeddable
-public class Session {
+@Entity
+@SequenceGenerator(name = "sessionSeq",initialValue = 1, allocationSize = 1)
+@Table(name = "MYCA_USER_SESSION")
+public class Session implements Serializable{
+    
     /**
      * Constant that represents the message that occurs if the session being created 
      * has an invalid end date time
      */
     private static final String INVALID_SESSION_END_DATE_TIME="Invalid session end date time!";
+    /**
+     * Long with the session persistence identifier
+     */
+    @Id
+    @GeneratedValue(generator = "sessionSeq",strategy = GenerationType.SEQUENCE)
+    private long id;
     /**
      * LocalDateTime with the session start date time
      */
@@ -22,26 +37,27 @@ public class Session {
      * LocalDateTime with the sesion end date time
      */
     private LocalDateTime sessionEndDateTime;
-    
     /**
-     * Creates a new session with a specified end date time
-     * @param sessionEndDateTime LocalDateTime with the session end date time
-     * @return Session with the created session
+     * String with the session token
      */
-    public static Session valueOf(LocalDateTime sessionEndDateTime){
-        return new Session(sessionEndDateTime);
-    }
+    private String sessionToken;
     
     /**
      * Builds a new session
      * @param sessionEndDateTime LocalDateTime with the session end date time 
      */
-    private Session(LocalDateTime sessionEndDateTime){
+    public Session(LocalDateTime sessionEndDateTime){
         LocalDateTime sessionStart=LocalDateTime.now();
         checkSessionEndDateTime(sessionStart,sessionEndDateTime);
         this.sessionStartDateTime=sessionStart;
         this.sessionEndDateTime=sessionEndDateTime;
     }
+    
+    /**
+     * Checks if the current session is active
+     * @return boolean true if the session is active, false if not
+     */
+    public boolean isActive(){return this.sessionEndDateTime.isAfter(this.sessionStartDateTime);}
     
     /**
      * Checks if the session end date time is valid
