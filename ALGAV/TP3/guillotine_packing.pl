0029753349e23e27333e1f3f5af313f600e51ba2
João Moreira <1160928@isep.ipp.pt>
2018-11-30 17:04:31 +0000

    Fixed guillotine heuristic and simulated annealing. [see #62]

--- ALGAV/TP3/guillotine_packing.pl
+++ ALGAV/TP3/guillotine_packing.pl
@@ -24,8 +24,25 @@ startup([CP|RP], Container, MC, PO, NPP) :-
 
 %Non recursive guillotine_packaging predicate. Used to insert the last package
 guillotine_packaging([CP], Container, RC, MC, NPP, RNPP) :-
-    (   (_,X,Y,Z) = CP,
-        guil((X,Y,Z), Container, RC, MC), !,
+    (CD, CW, _)=Container,
+    (   (_, X, Y, Z)=CP,
+        (   (   guil((X, Y, Z),
+                     Container,
+                     RC,
+                     MC)   %Try to insert package vertically (along the z axis)
+            ;   addPackage((X, Y, Z),
+                           Container,              %Add package along the container's width (y axis)
+                           (CD, CW, 0, CW, CD),
+                           RC,
+                           MC)
+            )
+        ;   addFront((X, Y, Z),
+                     Container,
+                     (CD, CW, 0, CW, CD),
+                     RC,
+                     (CD, CW, 0, CW, CD),
+                     MC)
+        ), !,
         RNPP=NPP
     ;   MC=RC,
         append(NPP, [CP], RNPP)
@@ -34,14 +51,24 @@ guillotine_packaging([CP], Container, RC, MC, NPP, RNPP) :-
 %Recursive guillotine_packaging predicate
 %RNPP - List of packages that weren't inserted in the container
 guillotine_packaging([CP|RP], Container, RC, MC, NPP, RNPP) :-
-    (CD, CW, _)=Container,(_,X,Y,Z) = CP,
-    (   (   
-            guil((X,Y,Z), Container, RC, LC)   %Try to insert package vertically (along the z axis)
-        ;   addPackage((X,Y,Z),               
-                       Container,              %Add package along the container's width (y axis)
-                       (CD, CW, 0, CW, CD),
-                       RC,
-                       LC)
+    (CD, CW, _)=Container,
+    (_, X, Y, Z)=CP,
+    (   (   (   guil((X, Y, Z),
+                     Container,
+                     RC,
+                     LC)   %Try to insert package vertically (along the z axis)
+            ;   addPackage((X, Y, Z),
+                           Container,              %Add package along the container's width (y axis)
+                           (CD, CW, 0, CW, CD),
+                           RC,
+                           LC)
+            )
+        ;   addFront((X, Y, Z),
+                     Container,
+                     (CD, CW, 0, CW, CD),
+                     RC,
+                     (CD, CW, 0, CW, CD),
+                     LC)
         ), !,
         SNPP=NPP    %If the package is inserted, the not placed list doesn't suffer changes
     ;   LC=RC,      %If the package is inserted, update the list with inserted products
@@ -64,7 +91,7 @@ percentage_occupied(LP, NPP, CVol, PO) :-
 
 %Non recursive spacial_occupation predicate. Used to calculate the last package's volume
 spacial_occupation([CP], NPP, PVol, RVol) :-
-    (_,D, W, H)=CP,
+    (_, D, W, H)=CP,
     (   not(member(CP, NPP)),
         Vol is D*W*H,
         RVol is Vol+PVol
@@ -74,7 +101,7 @@ spacial_occupation([CP], NPP, PVol, RVol) :-
 
 %Recursive spacial_occupation predicate. Used to calculate all package's volumes except for the last
 spacial_occupation([CP|RP], NPP, PVol, RVol) :-
-    (_,D, W, H)=CP,
+    (_, D, W, H)=CP,
     (   not(member(CP, NPP)),
         Vol is D*W*H,
         SVol is Vol+PVol
@@ -84,106 +111,117 @@ spacial_occupation([CP|RP], NPP, PVol, RVol) :-
     spacial_occupation(RP, NPP, SVol, RVol).
 
 %Creates First Cut
-guil(P, C, [], [[((D, W, H, W, D), [])]]) :-
-    (D, W, H)=P,
+guil(P, C, [], [(((D, W, H, W, D), []), [])]) :-
+    !,(D, W, H)=P,
     (CD, CW, CH)=C,
     D=<CD,
     W=<CW,
     H=<CH.  
 
 %Tries to place a package along the z axis
-guil(P, C, [[((CutD, CutW, CutH, RW, RD), [])]|CT], [[((CutD, CutW, CutH, RW, RD), [[((D, W, TH, W, D), [])]])]|CT]) :-
-    (D, W, H)=P,
+guil(P, C, [(((CutD, CutW, CutH, RW, RD), []), T)|CT], [(((CutD, CutW, CutH, RW, RD), [(((ND, NW, TH, W, D), []), [])]), T)|CT]) :-
+    !,(D, W, H)=P,
     (_, _, CH)=C, !,
-    D=<CutD,
+    D=<RD,
     W=<RW,
     TH is CutH+H,
-    TH=<CH.
+    TH=<CH,
+    (   CutW==RW,
+        NW=W
+    ;   CutW\==RW,
+        NW is CutW-RW+W
+    ),
+    (   CutD==RD,
+        ND=D
+    ;   CutD\==RD,
+        ND is CutD-RD+D
+    ).
     
 %Predicate used to know whether a package is going to be inserted along the x,y or z axes
-guil(P, C, [[((CutD, CutW, CutH, RW, RD), [Child|T])]|CT], [[((CutD, CutW, CutH, RW, RD), NL)]|CT]) :-
-    (D, _, _)=P,
-    D=<CutD,
-    (   (   guil(P, C, [Child|T], NL) % Tries to insert a package along the z axis
-        ;   addPackage(P,                           
+guil(P, C, [(((CutD, CutW, CutH, RW, RD), [Child|T]), E)|CT], [(((CutD, CutW, CutH, RW, RD), TL), E)|CT]) :-
+    !,(D, _, _)=P,
+    D=<RD,
+    (   (   guil(P, C, [Child|T], TL) % Tries to insert a package along the z axis
+        ;   addPackage(P,
                        C,
                        (CutD, CutW, CutH, RW, RD), % Tries to insert a package along the y axis
-                       [Child],
-                       NL)
+                       [Child|T],
+                       TL)
         )
     ;   addFront(P,
                  C,
                  (CutD, CutW, CutH, RW, RD), % Tries to insert a package along the x axis
                  [Child|T],
-                 NL)
+                 (CutD, CutW, CutH, RW, RD),
+                 TL)
     ).
 
 %Non recursive addFront predicate. Adds a package to the container along the x axis
-addFront(P, C,  (_, _, CutH, RW, RD), [Child], [Child, [((TD, W, H, W, D), [])]]) :-
-    (D, W, H)=P,
-    [((X, _, _, _, _), _)]=Child,
+addFront(P, C,  (_, _, CutH, RW, RD), [(((X, Y, Z, NRW, RX), TP), SP)], Previous, NL) :-
+    !,(D, W, H)=P,
+    (_, _, _, _, PD)=Previous,
     (_, _, CH)=C,
+    TW is Y-NRW + W,
     TD is D+X,
+    TD=<PD,
     TD=<RD,
     W=<RW,
     TH is H+CutH,
-    TH=<CH.
+    TH=<CH,
+    NL=[(((X, Y, Z, NRW, RX), TP), SP),  (((TD, TW, TH, W, D), []), [])].
 
 %Recursive addFront predicate. Tries to add a package to the container along the x axis
-addFront(P, C,  (CutD, CutW, CutH, RW, RD), [Child|RC], RL) :-
-    RC\==[],
+addFront(P, C,  (CutD, CutW, CutH, RW, RD), [(((X, Y, Z, RY, RX), TP), SP)|RC], _, [(((X, Y, Z, RY, RX), TP), SP)|NL]) :-
+    !,RC\==[],
     (D, _, _)=P,
-    (   [((X, _, _, _, _), _)]=Child
-    ;   [[((X, _, _, _, _), _)]]=Child
-    ),
     (   D=<X,
-        (   guil(P, C, [Child], NL),
-            RL=[NL|RC]
-        ;   addPackage(P,
+        (   RC\==[],
+            guil(P, C, RC, NL)
+        ;   RC\==[],
+            addPackage(P,
                        C,
                        (CutD, CutW, CutH, RW, RD),
                        RC,
-                       NL),
-            RL=[Child|NL]
+                       NL)
         )
-    ;   addFront(P,
+    ;   RC\==[],
+        addFront(P,
                  C,
                  (_, _, CutH, RW, RD),
                  RC,
-                 NL),
-        RL=[Child|NL]
+                 (_, _, CutH, RW, RD),
+                 NL)
     ).
-
 %Non recursive addPackage predicate. Adds a package to the container along the y axis
-addPackage(P, C,  (_, _, CutH, RW, _), [Child], NL) :-
-    (   guil(P, C, [Child], NL)
-    ;   (D, W, H)=P,
-        [((X, Y, _, _, _), _)]=Child,
-        (_, _, CH)=C,
-        D=<X,
-        TW is W+Y,
-        TW=<RW,
-        TH is H+CutH,
-        TH=<CH,
-        append([Child],
-               [[((D, TW, TH, W, D), [])]],
-               NL)
-    ).
+addPackage(P, C,  (_, _, CutH, RW, _), [(((X, Y, Z, NRW, RX), T), [])|FP], NL) :-
+    !,(D, W, H)=P,
+    (_, _, CH)=C,
+    D=<RX,
+    TW is W+Y,
+    TW=<RW,
+    TH is H+CutH,
+    TH=<CH,
+    TD is X-RX+D,
+    [(((X, Y, Z, NRW, RX), T), [(((TD, TW, TH, W, D), []), [])])|FP]=NL.
 
 %Recursive addPackage predicate. Tries to add a package to the container along the y axis
-addPackage(P, C,  (CutD, CutW, CutH, RW, RD), [Child|RC], RL) :-
-    RC\==[],
-    (D, _, _)=P,
-    (   [((X, _, _, _, _), _)]=Child
-    ;   [[((X, _, _, _, _), _)]]=Child
-    ),
-    D=<X,
-    (   guil(P, C, [Child], NL),
-        RL=[NL|RC]
-    ;   addPackage(P,
-                   C,
-                   (CutD, CutW, CutH, RW, RD),
-                   RC,
-                   NL),
-        RL=[Child|NL]
+addPackage(P, C,  (CutD, CutW, CutH, RW, RD), [(((X, Y, Z, NRW, RX), TC), SC)|RC], [(((X, Y, Z, NRW, RX), TC), NL)|RC]) :-
+    !,(D, _, _)=P,
+    D=<RX,
+    (   (   SC\==[],
+            guil(P, C, SC, NL)
+        ;   SC\==[],
+            addPackage(P,
+                       C,
+                       (CutD, CutW, CutH, RW, RD),
+                       SC,
+                       NL)
+        )
+    ;   SC\==[],
+        addFront(P,
+                 C,
+                 (CutD, CutW, CutH, RW, RD),
+                 SC,
+                 (X, Y, Z, NRW, RX),
+                 NL)
     ).
