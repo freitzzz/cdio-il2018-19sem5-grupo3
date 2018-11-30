b8dda70f666e6a5f87ac934a3a436c4caf47cb41
João Freitas <1160907@isep.ipp.pt>
2018-11-30 14:41:51 +0000

    Added missing MYCA files
    see #65

---
+++ MYCA/myca/persistence/src/main/java/cdiomyc/persistence/impl/jpa/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package that contains all persistence JPA implementations
+ */
+package cdiomyc.persistence.impl.jpa;
---
+++ MYCA/myca/support/src/main/java/cdiomyc/support/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package that contains all MYCA support functionalities
+ */
+package cdiomyc.support;
