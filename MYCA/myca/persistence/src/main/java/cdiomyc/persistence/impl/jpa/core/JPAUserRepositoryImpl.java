fb2d7f8b03eb31a5d326a70ee3c41f978ade9be9
João Moreira <1160928@isep.ipp.pt>
2018-11-30 16:02:28 +0000

    Created JPA implementation of RepositoryFactory and UserRepository. [see #65]

---
+++ MYCA/myca/persistence/src/main/java/cdiomyc/persistence/impl/jpa/core/JPAUserRepositoryImpl.java
@@ -0,0 +1,60 @@
+package cdiomyc.persistence.impl.jpa.core;
+
+import cdiomyc.core.domain.User;
+import cdiomyc.core.domain.auth.Auth;
+import cdiomyc.core.mv.authentication.AuthenticationMV;
+import cdiomyc.core.mv.authentication.AuthenticationMVService;
+import cdiomyc.core.mv.authentication.CredentialsAuthenticationMV;
+import cdiomyc.core.persistence.UserRepository;
+import cdiomyc.persistence.impl.jpa.BaseJPARepository;
+import javax.persistence.Query;
+
+/**
+ * JPAUserRepositoryImpl class, represents the JPA implementation of the User repository
+ * @author João
+ */
+public class JPAUserRepositoryImpl extends BaseJPARepository<User, Long> implements UserRepository {
+
+    /**
+     * Creates a new instance of JPAUserRepositoryImpl
+     */
+    public JPAUserRepositoryImpl() {
+        super(PersistenceUnitName.VALUE);
+    }
+
+    /**
+     * Finds a User by its identifier
+     *
+     * @param domainEntityIdentifier EID with the User identifier
+     * @return User which is identified by a certain identifier
+     */
+    @Override
+    public User findEID(Auth domainEntityIdentifier) {
+        Query query = super.getEntityManager().createQuery("SELECT u FROM User u WHERE u.auth = :auth")
+                .setParameter("auth", domainEntityIdentifier).setMaxResults(1);
+        return (User) query.getSingleResult();
+    }
+
+    /**
+     * Finds User by Authentication details
+     *
+     * @param authenticationDetails
+     * @return user corresponding to the authentication details
+     * @throws IllegalArgumentException if no user was found
+     */
+    @Override
+    public User findUserByAuthenticationDetails(AuthenticationMV authenticationDetails) {
+        if (authenticationDetails instanceof CredentialsAuthenticationMV) {
+            Query query = super.getEntityManager().createQuery("SELECT u FROM User u, CredentialsAuth ca "
+                    + "WHERE u.auth = ca "
+                    + "AND ca.username.value = :username "
+                    + "AND ca.password.value = :password")
+                    .setParameter("username", ((CredentialsAuthenticationMV) authenticationDetails).username)
+                    .setParameter("password", ((CredentialsAuthenticationMV) authenticationDetails).password)
+                    .setMaxResults(1);
+            return (User) query.getSingleResult();
+        }
+        throw new IllegalArgumentException("No User found with the given authentication details!");
+    }
+
+}
