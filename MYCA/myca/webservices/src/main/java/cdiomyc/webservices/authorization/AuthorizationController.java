e57fbac845a5a85dffb2c63c1579a06d1d54ea98
João Freitas <1160907@isep.ipp.pt>
2018-12-02 19:03:15 +0000

    Fully implemented isAuthorized request
    see #64
    
    see #65

--- MYCA/myca/webservices/src/main/java/cdiomyc/webservices/authorization/AuthorizationController.java
+++ MYCA/myca/webservices/src/main/java/cdiomyc/webservices/authorization/AuthorizationController.java
@@ -1,5 +1,8 @@
 package cdiomyc.webservices.authorization;
 
+import cdiomyc.core.mv.authorization.IsUserAuthorizedMV;
+import cdiomyc.webservices.dataservices.json.SimpleJSONMessageService;
+import com.google.gson.Gson;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
@@ -7,6 +10,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 /**
  * Framework controller that processes authorization requests
@@ -25,6 +29,22 @@ public class AuthorizationController {
     @Produces(value = MediaType.APPLICATION_JSON)
     public Response isAuthorized(@HeaderParam(value = "Authorization: Basic ")String sessionAPIToken, 
             @QueryParam(value = "contentmanager") boolean asContentManager){
-        throw new UnsupportedOperationException();
+        IsUserAuthorizedMV userAuthorizationDetails=new IsUserAuthorizedMV();
+        userAuthorizationDetails.sessionAPIToken=sessionAPIToken;
+        userAuthorizationDetails.isContentManager=asContentManager;
+        try{
+            cdiomyc.core.application.autho.AuthorizationController.isAuthorized(userAuthorizationDetails);
+            return Response.noContent().build();
+        }catch(IllegalArgumentException|IllegalStateException notAuthorizedException){
+            return Response
+                    .status(Status.BAD_REQUEST)
+                    .entity(new Gson().toJson(new SimpleJSONMessageService(notAuthorizedException.getMessage())))
+                    .build();
+        }catch(Exception internalErrorException){
+            return Response
+                    .status(Status.INTERNAL_SERVER_ERROR)
+                    .entity(new Gson().toJson(new SimpleJSONMessageService("An internal error has occurd :(")))
+                    .build();
+        }
     }
 }
