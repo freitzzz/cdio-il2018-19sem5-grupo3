4f8df339d137e21ad5ebc981e12b16cac3203dd2
桜ちゃん <1160941@isep.ipp.pt>
2018-12-04 10:49:59 +0000

    Creation of method that determines min,max and step for each slider.
    see #39 #15 #60

--- website/src/components/CustomizerSideBarDimensionsPanel.vue
+++ website/src/components/CustomizerSideBarDimensionsPanel.vue
@@ -5,7 +5,7 @@
       <i class="material-icons md-12 md-blue btn">help</i>
       <span class="tooltiptext">Please choose a option for the different type of dimensions.</span>
     </div>
-    <select class="dropdown" v-model="dimensionOp" @change="populateAvailableOptions">
+    <select class="dropdown" v-model="dimensionOp" @click="populateAvailableOptions">
       <option
         v-for="option in availableOptionsDimensions"
         :key="option.id"
@@ -14,13 +14,13 @@
     </select>
     <!--Fetch minimums from server-->
     <div class="text-entry">Height:</div>
-    <vue-slider class="slider" v-model="height" @change="updateHeight"></vue-slider>
+    <vue-slider class="slider" v-model="height" @change="updateHeight" ></vue-slider>
     <div class="text-entry">Width:</div>
     <vue-slider class="slider" v-model="width" @change="updateWidth"></vue-slider>
-    <div class="text-entry">Depth:</div>
+    <div class="text-entthisry">Depth:</div>
     <vue-slider class="slider" v-model="depth" @change="updateDepth"></vue-slider>
 
-    <div class="text-entry">Choose the available units:</div>
+    <div class="text-entry">Choose the available unit:</div>
     <select class="dropdown" v-model="unit" @change="updateUnit">
       <option
         v-for="optionUnit in availableOptionsUnits"
@@ -36,7 +36,7 @@
 import store from "./../store";
 import Axios from "axios";
 import { MYCM_API_URL } from "./../config.js";
-import vueSlider from 'vue-slider-component';
+import vueSlider from "vue-slider-component";
 import {
   SET_CUSTOMIZED_PRODUCT_WIDTH,
   SET_CUSTOMIZED_PRODUCT_HEIGHT,
@@ -56,15 +56,18 @@ export default {
       depth: 100,
       unit: "cm",
       dimensionOp: "",
-      availableOptionsDimensions:[],
-      availableOptionsUnits:[],
-      availableDimensionsHLD:[],
-      DISCRETE_INTERVAL:0,
-      CONTINUOUS_INTERVAL:1,
-      DISCRETE_VALUE:2
+      availableOptionsDimensions: [],
+      availableOptionsUnits: [],
+      availableDimensionsHLD: [],
+      DISCRETE_INTERVAL: 0,
+      CONTINUOUS_INTERVAL: 1,
+      DISCRETE_VALUE: 2,
+      ERROR_DIMENSION_TYPE:
+        "No available dimension please try the other option.",
+      
     };
   },
-  components:{
+  components: {
     vueSlider
   },
   created() {
@@ -74,15 +77,18 @@ export default {
     store.dispatch(SET_CUSTOMIZED_PRODUCT_UNIT, { unit: this.unit });
 
     /*Get all available dimensions of the given product of the array*/
-    Axios.get(`${MYCM_API_URL}/products/${store.state.product.id}/dimensions`
-    )
+    Axios.get(`${MYCM_API_URL}/products/${store.state.product.id}/dimensions`)
       .then(response => this.availableOptionsDimensions.push(...response.data))
-      .catch(console.log(error.response));
+      .catch(error => {
+        this.$toast.open(error.response.status + "An error occurred");
+      });
 
-   /*Get all available units of measurement*/
+    /*Get all available units of measurement*/
     Axios.get(`${MYCM_API_URL}/units`)
-    .then(response => this.availableOptionsUnits.push(...response.data))
-    .catch(console.log(error.response));
+      .then(response => this.availableOptionsUnits.push(...response.data))
+      .catch(error => {
+        this.$toast.open(error.response.status + "An error occurred");
+      });
   },
   methods: {
     updateHeight(e) {
@@ -97,25 +103,29 @@ export default {
     updateUnit(e) {
       store.dispatch(SET_CUSTOMIZED_PRODUCT_UNIT, { unit: e.target.value });
     },
-    
-    identifyTypeDimensions(dimensionObj){
- 
-      if( dimensionObj.values !=null){ //Discrete interval
+
+    identifyTypeDimensions(dimensionObj) {
+      if (dimensionObj.values != null) {
+        //Discrete interval
         return this.DISCRETE_INTERVAL;
-      }else if(dimensionObj.value !=null){ //DIscrete value
+      } else if (dimensionObj.value != null) {
+        //DIscrete value
         return this.DISCRETE_VALUE;
-      }else if(dimensionObj.minValue !=null && dimensionObj.maxValue!=null && dimensionObj.increment !=null){
+      } else if (
+        dimensionObj.minValue != null &&
+        dimensionObj.maxValue != null &&
+        dimensionObj.increment != null
+      ) {
         return this.CONTINUOUS_INTERVAL;
-      }//Not yet implemented dimension
-      return -1;
-      
+      } //Not yet implemented dimension
+      return ERROR_DIMENSION_TYPE;
     },
     //Get all available options
     populateAvailableOptions() {
-
       //Get information of the chosed option
       var op = this.dimensionOp;
-      var heightType,widthType,depthType;
+      var heightType, widthType, depthType;
+
       //Create array of different dimensions h,l,d
       //Create for to identify each type of dimension
       //Set slider of different dimension
@@ -123,15 +133,81 @@ export default {
 
       //Populate Height:
       heightType = this.identifyTypeDimensions(op.height);
-      alert(heightType);
+ 
+      if(heightType == this.DISCRETE_INTERVAL){
+        this.organizeCrescentOrder(op.height.values);
+      }
+
       //Populate Width
       widthType = this.identifyTypeDimensions(op.width);
-      alert(widthType);
+      if(widthType == this.DISCRETE_INTERVAL){
+        this.organizeCrescentOrder(op.width.values);
+      }
+      /* alert(widthType); */
       //Populate Depth:
       depthType = this.identifyTypeDimensions(op.depth);
-      alert(depthType);
-      
-    } 
+      /* alert(depthType); */
+      if(depthType == this.DISCRETE_INTERVAL){
+        this.organizeCrescentOrder(op.depth.values);
+      }
+    },
+    //The following methods determine the min,max and increment to populate the height,width and depth slider
+    determineMinOfInterval(typeOfInterval,dimensionJson){
+      var min;
+      if(typeOfInterval == this.DISCRETE_INTERVAL){
+         return 0; //index of 
+      }else if(typeOfInterval==this.CONTINUOUS_INTERVAL){
+        min = dimensionJson.minValue;
+      }else{//DISCRETE VALUE
+        min = dimensionJson.value;
+      }
+      return min;
+    },
+    determineMaxOfInterval(typeOfInterval,dimensionJson){
+      var max=-1;
+      if(typeOfInterval == this.DISCRETE_INTERVAL){
+        /* for(var i=0; i< dimensionJson.values.length;i++){
+          if(max < dimensionJson.values[i]){
+            max = dimensionJson.values[i];
+          }
+        } */
+        return dimensionJson.length-1;
+      }else if (typeOfInterval==this.CONTINUOUS_INTERVAL){
+        min = dimensionJson.maxValue;
+      }else{
+        max= dimensionJson.value;
+      }
+      return max;
+    },
+    determineIncrementOfInterval(typeOfInterval,dimensionJson){
+      var increment;
+      if(typeOfInterval == this.DISCRETE_INTERVAL){
+        increment = 1;
+      }else if(typeOfInterval==this.CONTINUOUS_INTERVAL){
+        increment = dimensionJson.increment;
+      }else{//DISCRETE VALUE
+        increment = 0;
+      }
+    },
+    //Organizes vector to crescent order.
+    organizeCrescentOrder(vec){
+      var tmp,minTmp;
+
+      for(var i = 0; i< vec.length; i++){
+        tmp = vec[i];
+        for(var j = i+1; j< vec.length; j++){
+          if(tmp > vec[j]){
+            tmp = vec[j];
+          }
+        }
+        if(tmp!=vec[i]){
+          minTmp = vec[i];
+          vec[i] = tmp;
+          vec[j] = minTmp;
+        }
+      }
+    }
+
   }
 };
 </script>
@@ -176,8 +252,8 @@ export default {
   margin-right: 15%;
 }
 .slider {
-  margin-bottom: 5%;
-  width: 15px;
+  margin-left: 15%;
+  margin-right: 15%;
 }
 </style>
 
