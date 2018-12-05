6bd76e09596a650fa989283a9c6177652c72b51c
João Freitas <1160907@isep.ipp.pt>
2018-12-05 15:19:50 +0000

    City model now validates if a given city is valid
    see #38
    
    see #66

--- MYCO/models/City.js
+++ MYCO/models/City.js
@@ -21,10 +21,8 @@ var nameValidator={
  * Represents a City Schema
  */
 var citySchema = new Schema({
-
     name:{type: String, validate: nameValidator, required:true},
     location:{type: location.schema, required:true},
-
 });
 
 /**
@@ -48,6 +46,26 @@ citySchema.statics.createCity=function (name,locationLatitude,locationLongitude)
         location:location.createLocation(locationLatitude,locationLongitude)
     }
 }
+
+/**
+ * Grants that a city model is valid
+ * @param {Object} cityModel Object with the city model being validated
+ */
+citySchema.statics.validateCityModel=function(cityModel){
+    if(!cityModel)throw 'Invalid city details';
+    grantCityNameIsValid(cityModel.name);
+    location.validateCityModel();
+};
+
+/**
+ * Grants that a city name is valid
+ * @param {String} name String with the city name
+ */
+function grantCityNameIsValid(name){
+    if(!name || !checkNameBusinessRule(name))
+        throw name+' is not a valid name';
+}
+
 /**
  * Checks if a name is valid according to business rules
  * @param {String} name String with the name being checked
