0e62b1e6fb66247bb2753b27e47f0c4ff653029b
Gil Durao <1160936@isep.ipp.pt>
2018-12-05 21:57:01 +0000

    Updates mutations.spec
    see #4
    see #39

--- website/tests/mutations.spec.js
+++ website/tests/mutations.spec.js
@@ -1,6 +1,38 @@
 import { mutations } from '../src/store/mutations'
 
-test('INIT_PRODUCT updates state with correct product id', () => {
+describe('product mutations', () => {
+    describe('product-id', () => {
+        test('INIT_PRODUCT-updates-state-with-correct-product-id',
+            ensureInitProductUpdatesStateCorrectly);
+    })
+});
+
+describe('customized product mutations', () => {
+    describe('customized-product-dimensions', () => {
+        test('SET_CUSTOMIZED_PRODUCT_WIDTH-updates-state-with-correct-width',
+            ensureSetCustomizedProductWidthUpdatesStateCorrectly);
+        test('SET_CUSTOMIZED_PRODUCT_HEIGHT-updates-state-with-correct-height',
+            ensureSetCustomizedProductHeightUpdatesStateCorrectly);
+        test('SET_CUSTOMIZED_PRODUCT_DEPTH-updates-state-with-correct-depth',
+            ensureSetCustomizedProductDepthUpdatesStateCorrectly);
+        test('SET_CUSTOMIZED_PRODUCT_UNIT-updates-state-with-correct-unit',
+            ensureSetCustomizedProductUnitUpdatesStateCorrectly)
+    });
+    describe('customized-product-slots', () => {
+        describe('slot-dimensions', () => {
+            test('SET_SLOT_WIDTH-updates-state-with-correct-width',
+                ensureSetSlotWidthUpdatesStateCorrectly);
+            test('SET_SLOT_HEIGHT-updates-state-with-correct-height',
+                ensureSetSlotHeightUpdatesStateCorrectly);
+            test('SET_SLOT_DEPTH-updates-state-with-correct-depth',
+                ensureSetSlotDepthUpdatesStateCorrectly);
+            test('SET_SLOT_UNIT-updates-state-with-correct-uni',
+                ensureSetSlotUnitUpdatesStateCorrectly);
+        })
+    })
+})
+
+function ensureInitProductUpdatesStateCorrectly() {
     const state = {
         product: {
             "id": ""
@@ -13,28 +45,92 @@ test('INIT_PRODUCT updates state with correct product id', () => {
     };
     mutations.init_product(state, payload);
     expect(state.product.id).toBe(payload.product.id);
-})
+}
 
-test('SET_CUSTOMIZED_PRODUCT_WIDTH updates state with correct width', () => {
+function ensureSetCustomizedProductWidthUpdatesStateCorrectly() {
     const state = {
-        
+        customizedProduct: {
+            customizedDimensions: {
+                width: "",
+                height: "",
+                depth: "",
+                unit: ""
+            }
+        }
     }
-})
+    const payload = {
+        width: 100
+    }
+    mutations.set_customized_product_width(state, payload);
+    expect(state.customizedProduct.customizedDimensions.width).toBe(payload.width);
+}
 
-test('SET_CUSTOMIZED_PRODUCT_HEIGHT updates state with correct height', () => {
+function ensureSetCustomizedProductHeightUpdatesStateCorrectly() {
     const state = {
-
+        customizedProduct: {
+            customizedDimensions: {
+                width: "",
+                height: "",
+                depth: "",
+                unit: ""
+            }
+        }
     }
-})
+    const payload = {
+        height: 100
+    }
+    mutations.set_customized_product_height(state, payload);
+    expect(state.customizedProduct.customizedDimensions.height).toBe(payload.height);
+}
 
-test('SET_CUSTOMIZED_PRODUCT_DEPTH updates state with correct depth', () => {
+function ensureSetCustomizedProductDepthUpdatesStateCorrectly() {
     const state = {
-
+        customizedProduct: {
+            customizedDimensions: {
+                width: "",
+                height: "",
+                depth: "",
+                unit: ""
+            }
+        }
     }
-})
+    const payload = {
+        depth: 100
+    }
+    mutations.set_customized_product_depth(state, payload);
+    expect(state.customizedProduct.customizedDimensions.depth).toBe(payload.depth);
+}
 
-test('SET_CUSTOMIZED_PRODUCT_UNIT updates state with correct unit', () => {
+function ensureSetCustomizedProductUnitUpdatesStateCorrectly() {
     const state = {
-
+        customizedProduct: {
+            customizedDimensions: {
+                width: "",
+                height: "",
+                depth: "",
+                unit: ""
+            }
+        }
     }
-})
+    const payload = {
+        unit: "cm"
+    }
+    mutations.set_customized_product_unit(state, payload);
+    expect(state.customizedProduct.customizedDimensions.unit).toBe(payload.unit);
+}
+
+function ensureSetSlotWidthUpdatesStateCorrectly() {
+    //!Implement test after mutation is fixed
+}
+
+function ensureSetSlotHeightUpdatesStateCorrectly() {
+    //!Implement test after mutation is fixed
+}
+
+function ensureSetSlotDepthUpdatesStateCorrectly() {
+    //!Implement test after mutation is fixed
+}
+
+function ensureSetSlotUnitUpdatesStateCorrectly() {
+    //!Implement test after mutation is fixed
+}
