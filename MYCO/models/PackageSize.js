b55119db2ea78120fa1bbdb14885579de609f215
João Freitas <1160907@isep.ipp.pt>
2018-12-05 10:30:10 +0000

    Added PackageSize which represents a enum class for Package Sizes
    see #38
    
    see #66

---
+++ MYCO/models/PackageSize.js
@@ -0,0 +1,24 @@
+/**
+ * Constant that represents the small package size
+ */
+const SMALL="S";
+
+/**
+ * Constant that represents the medium package size
+ */
+const MEDIUM="M";
+
+/**
+ * Constant that represents the large package size
+ */
+const LARGE="L";
+
+/**
+ * Constant that represents the enum representation of the packages size
+ */
+const PackageSizeEnum={
+    SMALL:SMALL,
+    MEDIUM:MEDIUM,
+    LARGE:LARGE,
+    values:[SMALL,MEDIUM,LARGE]
+}
