ee977d009aacca3970cb545679634fc639e56993
João Freitas <1160907@isep.ipp.pt>
2018-12-06 00:43:38 +0000

    Package Data Model can now create a model with its weight and dimensions
    see #38
    
    see #66

--- MYCO/models/Package.js
+++ MYCO/models/Package.js
@@ -11,18 +11,31 @@ var Tag = require('../models/Tag');
 var PackageSize=require('./PackageSize');
 
 /**
+ * Requires Dimensions Schema Model
+ */
+var Dimensions=require('./Dimensions');
+
+/**
  * Represents a Package Schema
  */
 var packageSchema = new Schema({
     size: {
         type: String,
-        enum: ["S", "M", "L"],
+        enum: PackageSize.values,
         default: PackageSize.MEDIUM,
         required: true
     },
     tag: {
         type: Tag.schema,
         required: true
+    },
+    weight:{
+        type: Number,
+        required: true
+    },
+    dimensions:{
+        type:Dimensions.schema,
+        required:true
     }
 });
 
@@ -30,10 +43,15 @@ var packageSchema = new Schema({
  * Creates a new package
  * @param {PackageSize} size PackageSize with the package size 
  * @param {Array} contents Array with the package contents
+ * @param {Number} weight Number with the package weight
+ * @param {Object} dimensions Dimensions model with the package dimensions
  */
-packageSchema.statics.createPackage = function (size, contents) {
+packageSchema.statics.createPackage = function (size, contents,weight,dimensions) {
     if(!PackageSize.values.includes(size))
-        throw `{size} is not a valid package size`;
+        throw `${size} is not a valid package size`;
+
+    if(weight<=0)
+        throw `${weight} is not a valid package weight`;
     let tag = size + "-" + S4() + "CS";
 
     contents.forEach(element => {
@@ -42,8 +60,10 @@ packageSchema.statics.createPackage = function (size, contents) {
     console.log(tag);
     return  {
         size: size,
-        tag: Tag.createTag(tag)
-    }
+        tag: Tag.createTag(tag),
+        weight:weight,
+        dimensions:Dimensions.createDimensions(dimensions.width,dimensions.height,dimensions.depth)
+    };
 }
 
 function S4() {
