3246cc1bed404b91aa17ac96da227dcf777f0dc8
桜ちゃん <1160941@isep.ipp.pt>
2018-12-07 13:17:31 +0000

    Creation of payment side bar
    see #35 #15 #67

--- website/src/components/CustomizerCheckOut.vue
+++ website/src/components/CustomizerCheckOut.vue
@@ -1,177 +1,179 @@
 <template>
-     <div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-
-        <div class="row">
-          <div class="col-50">
-            <h3>Billing Address</h3>
-            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
-            <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
-            <label for="email"><i class="fa fa-envelope"></i> Email</label>
-            <input type="text" id="email" name="email" placeholder="john@example.com">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
+  <div class="row">
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <div class="col-75">
+      <div class="container">
+        <form action="/action_page.php">
+  
+          <div class="row">
+            <div class="col-50">
+              <h3>Billing Address</h3>
+              <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+              <label for="email"><i class="fa fa-envelope"></i> Email</label>
+              <input type="text" id="email" name="email" placeholder="john@example.com">
+              <label for="adr"> <!-- <i class="fa fa-address-card-o"></i>  -->Address</label>
+              <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+              <label for="city"><!-- <i class="fa fa-institution"> --><!-- </i> --> City</label>
+              <input type="text" id="city" name="city" placeholder="New York">
+              <label for="zip">Zip</label>
+              <input type="text" id="zip" name="zip" placeholder="10001">
+  
+  
+            </div>
+  
+            <div class="col-50">
+  
+              <label for="cname">Name on Card</label>
+              <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+              <label for="ccnum">Credit card number</label>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+              <label for="expmonth">Exp Month</label>
+              <input type="text" id="expmonth" name="expmonth" placeholder="September">
+  
+              <div class="row">
+                <div class="col-50">
+                  <label for="expyear">Exp Year</label>
+                  <input type="text" id="expyear" name="expyear" placeholder="2018">
+                </div>
+                <div class="col-50">
+                  <label for="cvv">CVV</label>
+                  <input type="text" id="cvv" name="cvv" placeholder="352">
+                </div>
               </div>
             </div>
+  
           </div>
+  
+        </form>
+  
+ 
+      </div>
+     
+    </div>
+  
+    <div class="col-25">
+      <div class="container">
+        <h4>Cart
+          <span class="price" style="color:black">
+                <i class="fa fa-shopping-cart"></i>
+                <b>4</b>
+              </span>
+        </h4>
+        <p><a>Pole By Stephano</a> <span class="price">$499.99</span></p>
+        <p><a>Drawer</a> <span class="price">$5</span></p>
+        <p><a>Closet</a> <span class="price">$5</span></p>
+        <p><a>Matte Finish</a> <span class="price">$1.99</span></p>
+        <hr>
+        <p>Total <span class="price" style="color:black"><b>$511.98</b></span></p>
+      </div>
+          <input type="checkbox" name="checkbox" value="check" id="agree" /> I have read and agree to the Terms and Conditions and Privacy Policy
+        <p></p>
+        <i class="btn btn-primary">Continue to checkout</i>
 
-          <div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:red;"></i>
-              <i class="fa fa-cc-discover" style="color:orange;"></i>
-            </div>
-            <label for="cname">Name on Card</label>
-            <input type="text" id="cname" name="cardname" placeholder="John More Doe">
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
-          </div>
-
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
-        </label>
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
     </div>
   </div>
-
-  <div class="col-25">
-    <div class="container">
-      <h4>Cart
-        <span class="price" style="color:black">
-          <i class="fa fa-shopping-cart"></i>
-          <b>4</b>
-        </span>
-      </h4>
-      <p><a>Pole By Stephano</a> <span class="price">$499.99</span></p>
-      <p><a>Drawer</a> <span class="price">$5</span></p>
-      <p><a>Closet</a> <span class="price">$5</span></p>
-      <p><a>Shiny</a> <span class="price">$1.99</span></p>
-      <hr>
-      <p>Total <span class="price" style="color:black"><b>$511.98</b></span></p>
-    </div>
-  </div>
-</div> 
 </template>
+
 <script>
-export default {
-    
-}
+  import Vue from 'vue';
+  /* import { library } from '@fortawesome/fontawesome-svg-core';
+  import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; */
+  
+  export default {
+  
+  }
 </script>
+
 <style>
- .row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-}
-
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
-}
-
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
-
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 16px;
-}
-
-.container {
-  background-color: #f2f2f2;
-  padding: 5px 20px 15px 20px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
-
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-
-.icon-container {
-  margin-bottom: 20px;
-  padding: 7px 0;
-  font-size: 24px;
-}
-
-.btn {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px;
-  margin: 10px 0;
-  border: none;
-  width: 100%;
-  border-radius: 3px;
-  cursor: pointer;
-  font-size: 17px;
-}
-
-.btn:hover {
-  background-color: #45a049;
-}
-
-span.price {
-  float: right;
-  color: grey;
-}
-
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
-@media (max-width: 800px) {
   .row {
-    flex-direction: column-reverse;
+    display: -ms-flexbox;
+    /* IE10 */
+    display: flex;
+    -ms-flex-wrap: wrap;
+    /* IE10 */
+    flex-wrap: wrap;
+    margin: 13px;
+    font-size: 10px;
+    height:70%;
   }
+  
   .col-25 {
-    margin-bottom: 20px;
+    -ms-flex: 25%;
+    /* IE10 */
+    flex: 25%;
   }
-}
+  
+  .col-50 {
+    -ms-flex: 50%;
+    /* IE10 */
+    flex: 50%;
+  }
+  
+  .col-75 {
+    -ms-flex: 75%;
+    /* IE10 */
+    flex: 75%;
+  }
+  
+  .col-25,
+  .col-50,
+  .col-75 {
+    padding: 0 5px;
+  }
+  
+  .container {
+    background-color: #f2f2f2;
+    padding: 5px 20px 15px 20px;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
+    width: 100%;
+  }
+  
+  input[type=text] {
+    width: 100%;
+    margin-bottom: 5px;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+  
+  label {
+    margin-bottom: 10px;
+    display: block;
+  }
+  
+  .icon-container {
+    margin-bottom: 20px;
+    padding: 7px 0;
+  }
+  
+  .btn {
+    background-color: #0ba2db;
+    color: white;
+    margin: 10px 0;
+    border-radius: 3px;
+  }
+  
+  .btn:hover {
+    background-color: #0ba2db;
+  }
+  
+  span.price {
+    float: right;
+    color: grey;
+  }
+  
+  
+  /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
+  
+  @media (max-width: 300px) {
+    .row {
+      flex-direction: column-reverse;
+    }
+    .col-25 {
+      margin-bottom: 20px;
+    }
+  }
 </style>
--- website/src/components/CustomizerSideBar.vue
+++ website/src/components/CustomizerSideBar.vue
@@ -1,183 +1,235 @@
 <template>
-    <div>
-      <i ref="menuButton" @click="openNav()" class="material-icons md-36 md-grey btn" style="visibility:hidden">menu</i>
-      <div ref="customizerSidenav" class="sidenav">
-        <i class="closebtn material-icons md-18 md-grey" @click="closeNav()">close</i>
-        <h3>{{panels[currentPanelIndex].title}}</h3>
-        <!--The child component changes dinamically depending on tthe currently selected component.-->
-        <!--The Sidebar component listens out for any event that the child may trigger-->
-        <customizer-side-bar-products-panel v-if="currentPanelIndex == 0" @advance="selectProduct"></customizer-side-bar-products-panel>
-        <customizer-side-bar-dimensions-panel v-if="currentPanelIndex == 1"></customizer-side-bar-dimensions-panel>
-        <customizer-side-bar-slots-panel v-if="currentPanelIndex == 2"></customizer-side-bar-slots-panel>
-        <customizer-side-bar-materials-panel v-if="currentPanelIndex == 3"></customizer-side-bar-materials-panel>
-        <customizer-side-bar-components-panel v-if="currentPanelIndex == 4"></customizer-side-bar-components-panel>
-        <div class="sidenav-controls">
-
-          <i class="btn btn-primary material-icons" @click="previousPanel()" v-if="canDisplayPreviousButton">arrow_back</i>
-          <i class="btn btn-primary material-icons" @click="nextPanel()" v-if="canDisplayNextButton">arrow_forward</i>
-         </div>
+  <div>
+    <i ref="menuButton" @click="openNav()" class="material-icons md-36 md-grey btn" style="visibility:hidden">menu</i>
+    <div ref="customizerSidenav" class="sidenav" :v-model="sideNav"  v-if="currentPanelIndex != 5">
+      <i class="closebtn material-icons md-18 md-grey" @click="closeNav()">close</i>
+      <h3>{{panels[currentPanelIndex].title}}</h3>
+      <!--The child component changes dinamically depending on tthe currently selected component.-->
+      <!--The Sidebar component listens out for any event that the child may trigger-->
+      <customizer-side-bar-products-panel v-if="currentPanelIndex == 0" @advance="selectProduct"></customizer-side-bar-products-panel>
+      <customizer-side-bar-dimensions-panel v-if="currentPanelIndex == 1"></customizer-side-bar-dimensions-panel>
+      <customizer-side-bar-slots-panel v-if="currentPanelIndex == 2"></customizer-side-bar-slots-panel>
+      <customizer-side-bar-materials-panel v-if="currentPanelIndex == 3"></customizer-side-bar-materials-panel>
+      <customizer-side-bar-components-panel v-if="currentPanelIndex == 4"></customizer-side-bar-components-panel>
+      
+      
+      <div class="sidenav-controls">
+        <i class="btn btn-primary material-icons" @click="previousPanel()" v-if="canDisplayPreviousButton">arrow_back</i>
+        <i class="btn btn-primary material-icons" @click="nextPanel()" v-if="canDisplayNextButton">arrow_forward</i>
       </div>
     </div>
+    <div v-if="currentPanelIndex == 5"  class="sidenavSpecial">
+      <h3>{{panels[currentPanelIndex].title}}</h3>
+      <customizer-check-out v-if="currentPanelIndex == 5" ></customizer-check-out>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import CustomizerSideBarProductsPanel from "./CustomizerSideBarProductsPanel";
-import CustomizerSideBarDimensionsPanel from "./CustomizerSideBarDimensionsPanel";
-import CustomizerSideBarMaterialsPanel from "./CustomizerSideBarMaterialsPanel";
-import CustomizerSideBarSlotsPanel from "./CustomizerSideBarSlotsPanel";
-import CustomizerSideBarComponentsPanel from "./CustomizerSideBarComponentsPanel";
-import Store from "./../store/index.js";
-
-export default {
-  name: "CustomizerSideBar",
-  data() {
-    return {
-      currentPanelIndex: 0,
-      panels: [
-        {
-          title: "Structures",
-          enabled: true
-        },
-        {
-          title: "Dimensions",
-          enabled: true
-        },
-        {
-          title: "Divisions",
-          enabled: false
-        },
-        {
-          title: "Materials",
-          enabled: true
-        },
-        {
-          title: "Components",
-          enabled: false
-        },
-        {
-          title: "Payment",
-          enabled: true
+  import CustomizerSideBarProductsPanel from "./CustomizerSideBarProductsPanel";
+  import CustomizerSideBarDimensionsPanel from "./CustomizerSideBarDimensionsPanel";
+  import CustomizerSideBarMaterialsPanel from "./CustomizerSideBarMaterialsPanel";
+  import CustomizerSideBarSlotsPanel from "./CustomizerSideBarSlotsPanel";
+  import CustomizerSideBarComponentsPanel from "./CustomizerSideBarComponentsPanel";
+  import CustomizerCheckOut from "./CustomizerCheckOut.vue";
+  import Store from "./../store/index.js";
+  
+  export default {
+    name: "CustomizerSideBar",
+    data() {
+      return {
+        sideNav:"",
+        currentPanelIndex: 0,
+        panels: [{
+            title: "Structures",
+            enabled: true
+          },
+          {
+            title: "Dimensions",
+            enabled: true
+          },
+          {
+            title: "Divisions",
+            enabled: false
+          },
+          {
+            title: "Materials",
+            enabled: true
+          },
+          {
+            title: "Components",
+            enabled: false
+          },
+          {
+            title: "Payment",
+            enabled: true
+          }
+        ]
+      };
+    },
+    components: {
+      CustomizerSideBarProductsPanel,
+      CustomizerSideBarDimensionsPanel,
+      CustomizerSideBarMaterialsPanel,
+      CustomizerSideBarSlotsPanel,
+      CustomizerSideBarComponentsPanel,
+      CustomizerCheckOut
+    },
+    computed: {
+       
+      /**
+       * Checks if the previous button can be displayed.
+       */
+      canDisplayPreviousButton() {
+        return this.currentPanelIndex > 0;
+      },
+      /**
+       * Checks if the next button can be displayed.
+       */
+      canDisplayNextButton() {
+        //Do not display next button for the Products tab nor for the last tab
+        return (
+          this.currentPanelIndex > 0 &&
+          this.currentPanelIndex < this.panels.length - 1
+        );
+      }
+    },
+    methods: {
+      /**
+       * Fades out the menu button and opens the sidenav
+       */
+      openNav() {
+        this.$refs.menuButton.style.visibility = "hidden";
+        this.$refs.customizerSidenav.style.width = "250px";
+      },
+      /**
+       * Closes the sidenav and fades in the menu button
+       */
+      closeNav() {
+        this.$refs.customizerSidenav.style.width = "0";
+        this.$refs.menuButton.style.visibility = "visible";
+      },
+      /**
+       * Go to the previous panel.
+       */
+      previousPanel() {
+        if (this.currentPanelIndex > 0) {
+          while (this.currentPanelIndex > 0 && !this.panels[--this.currentPanelIndex].enabled);
         }
-      ]
-    };
-  },
-  components: {
-    CustomizerSideBarProductsPanel,
-    CustomizerSideBarDimensionsPanel,
-    CustomizerSideBarMaterialsPanel,
-    CustomizerSideBarSlotsPanel,
-    CustomizerSideBarComponentsPanel
-  },
-  computed: {
-    /**
-     * Checks if the previous button can be displayed.
-     */
-    canDisplayPreviousButton() {
-      return this.currentPanelIndex > 0;
-    },
-    /**
-     * Checks if the next button can be displayed.
-     */
-    canDisplayNextButton() {
-      //Do not display next button for the Products tab nor for the last tab
-      return (
-        this.currentPanelIndex > 0 &&
-        this.currentPanelIndex < this.panels.length - 1
-      );
+      },
+      /**
+       * Go to the next panel.
+       */
+      nextPanel() {
+        if (this.currentPanelIndex < this.panels.length - 1) {
+          while (this.currentPanelIndex < this.panels.length && !this.panels[++this.currentPanelIndex].enabled);
+        }
+      },
+      /**
+       * Checks the product's slots and components properties and sets the flags accordingly and advances to the next stage.
+       */
+      selectProduct() {
+        //enable the slots panel if the product has slots
+        if (Store.getters.productSlotWidths != undefined) {
+          this.panels[2].enabled = true;
+        } else {
+          this.panels[2].enabled = false;
+        }
+        //enable the components panel if the product has components
+        if (Store.getters.productComponents != undefined) {
+          this.panels[4].enabled = true;
+        } else {
+          this.panels[4].enabled = false;
+        }
+        this.nextPanel();
+      }
+     
     }
-  },
-  methods: {
-    /**
-     * Fades out the menu button and opens the sidenav
-     */
-    openNav() {
-      this.$refs.menuButton.style.visibility = "hidden";
-      this.$refs.customizerSidenav.style.width = "250px";
-    },
-    /**
-     * Closes the sidenav and fades in the menu button
-     */
-    closeNav() {
-      this.$refs.customizerSidenav.style.width = "0";
-      this.$refs.menuButton.style.visibility = "visible";
-    },
-    /**
-     * Go to the previous panel.
-     */
-    previousPanel() {
-      if (this.currentPanelIndex > 0) {
-        while(this.currentPanelIndex > 0  && !this.panels[--this.currentPanelIndex].enabled);
-      }
-    },
-    /**
-     * Go to the next panel.
-     */
-    nextPanel() {
-      if (this.currentPanelIndex < this.panels.length - 1) {
-        while(this.currentPanelIndex < this.panels.length && !this.panels[++this.currentPanelIndex].enabled);
-      }
-    },
-    /**
-     * Checks the product's slots and components properties and sets the flags accordingly and advances to the next stage.
-     */
-    selectProduct(){
-      //enable the slots panel if the product has slots
-      if(Store.getters.productSlotWidths != undefined){
-        this.panels[2].enabled = true;
-      }else{
-        this.panels[2].enabled = false;
-      }
-      //enable the components panel if the product has components
-      if(Store.getters.productComponents != undefined){
-        this.panels[4].enabled = true;
-      }else{
-        this.panels[4].enabled = false;
-      }
-      this.nextPanel();
-    }
-  }
-};
+  };
 </script>
 
 <style scoped>
-.sidenav {
-  height: 100%; /* Full height */
-  width: 300px; /*full width on initial load, changed with Vue*/
-  position: fixed; /*stay in place*/
-  z-index: 1; /*stay on top*/
-  top: 15%; /*Display from top left corner*/
-  left: 0;
-  overflow-x: hidden; /*Disable horizontal scroll*/
-  padding-top: 60px;
-  margin: 2%;
-  transition: 0.3s;
-  background-color: #e9e9e9a0;
-}
+  .sidenav {
+    height: 100%;
+    /* Full height */
+    width: 300px;
+    /*full width on initial load, changed with Vue*/
+    position: fixed;
+    /*stay in place*/
+    z-index: 1;
+    /*stay on top*/
+    top: 15%;
+    /*Display from top left corner*/
+    left: 0;
+    overflow-x: hidden;
+    /*Disable horizontal scroll*/
+    padding-top: 60px;
+    margin: 2%;
+    transition: 0.3s;
+    background-color: #e9e9e9a0;
+  }
 
-.sidenav h3 {
-  font-size: 24px;
-  color: #797979 !important;
-  cursor: default;
-  position: absolute;
-  top: 15px;
-  left: 15px;
-  margin-right: 50px;
-  cursor: pointer;
-}
+  .sidenavSpecial{
+    height: 100%;
+    /* Full height */
+    width: 1000px;
+    /*full width on initial load, changed with Vue*/
+    position: fixed;
+    
+    /*stay in place*/
+    z-index: 1;
+    /*stay on top*/
+    top: 15%;
+    /*Display from top left corner*/
+    left: 0;
+    overflow-x: hidden;
+    /*Disable horizontal scroll*/
+    padding-top: 60px;
+    margin: 2%;
+    transition: 0.3s;
+    background-color: #e9e9e9a0;
+  }
 
-/*Center primary buttons on the side bar*/
-.sidenav .sidenav-controls {
-  text-align: center;
-  margin: auto;
+.sidenavSpecial h3{
+   font-size: 24px;
+    color: #797979 !important;
+    cursor: default;
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    margin-right: 50px;
+    cursor: pointer;
 }
-
-/* Position and style the close button (top right corner) */
-.sidenav .closebtn {
-  position: absolute;
-  top: 15px;
-  right: 15px;
-  margin-left: 50px;
-  cursor: pointer;
-}
+  
+  .sidenav h3 {
+    font-size: 24px;
+    color: #797979 !important;
+    cursor: default;
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    margin-right: 50px;
+    cursor: pointer;
+  }
+  
+  
+  /*Center primary buttons on the side bar*/
+  
+  .sidenav .sidenav-controls {
+    text-align: center;
+    margin: auto;
+  }
+  
+  
+  /* Position and style the close button (top right corner) */
+  
+  .sidenav .closebtn {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    margin-left: 50px;
+    cursor: pointer;
+  }
 </style>
 
