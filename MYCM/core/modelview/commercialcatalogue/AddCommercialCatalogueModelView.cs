945c30e2c8e1191457065dc48272bbc3881eb3e9
António Sousa <1161371@isep.ipp.pt>
2018-12-09 22:49:21 +0000

    Refactor CommercialCatalogue's modelviews [re #15].

--- MYCM/core/modelview/commercialcatalogue/AddCommercialCatalogueModelView.cs
+++ MYCM/core/modelview/commercialcatalogue/AddCommercialCatalogueModelView.cs
@@ -1,50 +1,35 @@
-
 using System.Collections.Generic;
 using System.Runtime.Serialization;
+using core.modelview.cataloguecollection;
+
 namespace core.modelview.commercialcatalogue
 {
     /// <summary>
     /// Class representing a ModelView used for adding a instance of a Commercial Catalogue
     /// </summary>
     [DataContract]
-
-    public class AddCommercialCatalogue
+    public class AddCommercialCatalogueModelView
     {
         /// <summary>
         /// Commercial Catalogue reference.
         /// </summary>
         /// <value>Gets/sets the reference.</value>
         [DataMember]
-        string reference { get; set; }
+        public string reference { get; set; }
 
         /// <summary>
         /// Commercial Catalogue designation.
         /// </summary>
         /// <value>Gets/sets the designation.</value>
         [DataMember]
-        string designation { get; set; }
+        public string designation { get; set; }
 
         /// <summary>
         /// List of Commercial Catalogue Catalogue Collection.
         /// </summary>
         /// <value>Gets/sets the name.</value>
         [DataMember]
-        public List<CatalogueCollectionModelViewId> catalogueCollectionList { get; set; }
+        public List<AddCatalogueCollectionModelView> catalogueCollectionList { get; set; }
 
-    }
-
-    /// <summary>
-    /// Class representing a ModelView used for retrieving basic data from instances of GetCommercialCatalogueCatalogueCollection
-    /// </summary>
-    [DataContract]
-    public class CatalogueCollectionModelViewId
-    {
-
-        /// <summary>
-        /// Catalogue Collection id
-        /// </summary>
-        /// <value>Gets/sets the reference.</value>
-        [DataMember]
-        long catalogueCollectionId { get; set; }
     }
 }
