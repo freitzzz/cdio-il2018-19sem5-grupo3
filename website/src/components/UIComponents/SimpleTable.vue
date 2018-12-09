5a0f5201aaadfc62f2ea7b0c0adf8c2d09de68b8
João Freitas <1160907@isep.ipp.pt>
2018-12-09 04:16:49 +0000

    Added SimpleTable components which represents a simple vue table
    see #61

---
+++ website/src/components/UIComponents/SimpleTable.vue
@@ -0,0 +1,21 @@
+<template>
+    <vuetable
+        :api-mode="false"
+        :data="data"
+        :fields="columns"
+    >
+    </vuetable>
+</template>
+
+<script>
+
+export default {
+    /**
+     * Component properties
+     */
+    props:{
+        columns:[],
+        data:[]
+    }
+}
+</script>
