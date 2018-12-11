35268921cc560c4bb23220036cbeac2d6b51b724
João Freitas <1160907@isep.ipp.pt>
2018-12-11 19:31:34 +0000

    BaseProduct.js now holds the slot id which the product is based on
    see #69

--- website/src/3d/BaseProduct.js
+++ website/src/3d/BaseProduct.js
@@ -1,22 +1,36 @@
+//@ts-check
+
 /**
  * Class that represents the base properties of a product
  */
 export default class BaseProduct{
     
     /**
-     * Builds a new BaseProduct with a random identifier
-     */
-    constructor(){super('_' + Math.random().toString(36).substr(2,9));}
-    
-    /**
-     * Builds a new BaseProduct with the product identifier
+     * Builds a new BaseProduct
      * @param {Object} id Object with the product identifer
+     * @param {Object} slotId Object with the slot identifier which the product is based on 
      */
-    constructor(id){this.id=id;}
+    constructor(id,slotId=null){
+        this.id=id;
+        this.slotId=slotId;
+    }
+
+    /**
+     * Changes the slot which the product is currently based on
+     * @param {Number} slotId 
+     */
+    changeSlot(slotId){
+        this.slotId=slotId;
+    }
 
     /**
      * Returns the product identifier
      */
-    id(){return this.id};
+    id(){return this.id;}
+
+    /**
+     * Returns the identifier of the slot which product is based on
+     */
+    getSlotId(){return this.slotId;}
 
 }
