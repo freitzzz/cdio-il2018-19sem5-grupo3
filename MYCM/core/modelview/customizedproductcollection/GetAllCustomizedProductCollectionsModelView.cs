6fa972e52bc402804ce00c10efc23b535514d9fe
Gil Durao <1160936@isep.ipp.pt>
2018-12-11 17:39:30 +0000

    Updates GetAllCustomizedProductCollectionsModelView to extend from List
    see #4
    see #15

--- MYCM/core/modelview/customizedproductcollection/GetAllCustomizedProductCollectionsModelView.cs
+++ MYCM/core/modelview/customizedproductcollection/GetAllCustomizedProductCollectionsModelView.cs
@@ -6,13 +6,6 @@ namespace core.modelview.customizedproductcollection
     /// <summary>
     /// Class representing a ModelView used for retrieving all instances of Customized Product Collection
     /// </summary>
-    [DataContract]
-    public class GetAllCustomizedProductCollectionsModelView
-    {
-        /// <summary>
-        /// List of model views representing basic info about a customized product collection
-        /// </summary>
-        [DataMember(Name = "customizedProductCollections")]
-        public List<GetBasicCustomizedProductCollectionModelView> customizedProductCollections { get; set; }
-    }
+    [CollectionDataContract]
+    public class GetAllCustomizedProductCollectionsModelView : List<GetBasicCustomizedProductCollectionModelView> { }
 }
