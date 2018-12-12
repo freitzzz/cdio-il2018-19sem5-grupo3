d2b1b167106c6b38ed6779fbfeeb957543aa3f12
桜ちゃん <1160941@isep.ipp.pt>
2018-12-12 22:17:15 +0000

    Creation of column parent category name
    see #60

--- website/src/components/management/category/CategoriesTable.vue
+++ website/src/components/management/category/CategoriesTable.vue
@@ -1,38 +1,38 @@
 <template>
     <vuetable :api-mode="false" :data="this.data" :fields="columns">
         <template slot="actions" slot-scope="props">
-                <div class="custom-actions">
-                     <button
-                        class="button is-danger"
-                        @click="openCategoryDetails(props.rowData.id)">
-                        <b-icon icon="magnify"/>
-                    </button>
-                    <button
-                        class="button is-danger"
-                        @click="editCategoryDetails(props.rowData.id)">
-                        <b-icon icon="pencil"/>
-                    </button>
-                    <button
-                        class="button is-danger"
-                        @click="deleteCategory(props.rowData.id)">
-                        <b-icon icon="minus"/>
-                    </button>
-                </div>
-                <div v-if="showCategoryDetails">
-                    <b-modal :active.sync="showCategoryDetails" has-modal-card scroll="keep">
-                        <category-details
-                            :category="currentSelectedCategory"
-                        />
-                    </b-modal>
-                </div>
-                <div v-if="showEditCategoryDetails">
-                    <b-modal :active.sync="showEditCategoryDetails" has-modal-card scroll="keep">
-                        <edit-category
-                            @emitCategory="updateCategory"
-                            :category="currentSelectedCategory2"
-                        />
-                    </b-modal>
-                </div>
+        <div class="custom-actions">
+             <button
+                class="button is-danger"
+                @click="openCategoryDetails(props.rowData.id)">
+                <b-icon icon="magnify"/>
+            </button>
+            <button
+                class="button is-danger"
+                @click="editCategoryDetails(props.rowData.id)">
+                <b-icon icon="pencil"/>
+            </button>
+            <button
+                class="button is-danger"
+                @click="deleteCategory(props.rowData.id)">
+                <b-icon icon="minus"/>
+            </button>
+        </div>
+        <div v-if="showCategoryDetails">
+            <b-modal :active.sync="showCategoryDetails" has-modal-card scroll="keep">
+                <category-details
+                    :category="currentSelectedCategory"
+                />
+            </b-modal>
+        </div>
+        <div v-if="showEditCategoryDetails">
+            <b-modal :active.sync="showEditCategoryDetails" has-modal-card scroll="keep">
+                <edit-category
+                    @emitCategory="updateCategory"
+                    :category="currentSelectedCategory2"
+                     />
+                 </b-modal>
+             </div>
 </template>
     </vuetable>
 </template>
@@ -56,7 +56,9 @@
     /**
      * Requires App Configuration for accessing MYCM API URL
      */
-    import Config, {MYCM_API_URL} from '../../../config.js';
+    import Config, {
+        MYCM_API_URL
+    } from '../../../config.js';
     
     export default {
         /**
@@ -82,6 +84,11 @@
                         title: "Name"
                     },
                     {
+                        name: "parentcategory",
+                        title: "Parent Category",
+                        callback: this.booleansAsIcons
+                    },
+                    {
                         name: "__slot:actions", // <----
                         title: "Actions",
                         titleClass: "center aligned",
@@ -98,7 +105,7 @@
              */
             booleansAsIcons(value) {
                 return value ?
-                    '<span class="ui teal label"><i class="material-icons">check</i></span>' :
+                    '<span class="ui teal label"> value </span>' :
                     '<span class="ui teal label"><i class="material-icons">close</i></span>';
             },
             /**
@@ -124,10 +131,10 @@
              */
             editCategoryDetails(categoryId) {
                 this.getCategoryDetails(categoryId)
-                .then((material)=>{
-                    this.showEditCategoryDetails = true;
-                });
-                
+                    .then((category) => {
+                        this.showEditCategoryDetails = true;
+                    });
+    
             },
             /**
              * Fetches the details of a certain category 
@@ -136,17 +143,69 @@
                 return new Promise((accept, reject) => {
                     Axios.get(MYCM_API_URL + '/categories/' + categoriesId)
                         .then((category) => {
-                            this.currentSelectedCategory = category.data;
-                            this.currentSelectedCategory2 = Object.assign({}, this.currentSelectedCategory);
-                            accept(category);
+                            alert("!!!")
+                            this
+                            .getParentCategory(category.data.id)
+                            .then((parentCategoryName)=>{
+                                 alert("angelo volta");
+                                this.currentSelectedCategory = `{
+                                                                 "id:"` + category.data.id + `,
+                                                                 "name":` + category.data.name + `,
+                                                                 "parentcategory":` + parentCategory + `}`;
+                                /* this.currentSelectedCategory.parentcategory = parentCategory; */
+                                alert(this.currentSelectedCategory.parentcategory);
+                                this.currentSelectedCategory2 = Object.assign({}, this.currentSelectedCategory);
+                                accept(category);
+                            })
+                            .catch(()=>{
+                                alert("!!!!!!!!!222!!!11")
+                                this.currentSelectedCategory = category.data;
+    
+                                this.currentSelectedCategory2 = Object.assign({}, this.currentSelectedCategory);
+                                accept(category);
+                            });
                         })
                         .catch((error_message) => {
+                            alert("!!!!!11")
                             this.$toast.open({
                                 message: error_message
                             });
                             reject();
                         });
                 });
+            },
+            /**
+             * Fetches parent category if exists
+             */
+            getParentCategory(childId) {
+                let parentCategory;
+                var parentCategories = [];
+                var childrenCategories = [];
+                return new Promise((accept, reject) => {
+                    /**Fetch all categories */
+                    Axios.get(MYCM_API_URL + '/categories')
+                        .then((response => {
+                            parentCategories.push(...response.data)
+                            /* Verify if there's at least a parent category */
+                            if (parentCategories.length > 0) {
+                                /* Get all sub categories for each parent */
+                                for (let i = 0; i < parentCategories.length; i++) {
+                                    Axios.get(MYCM_API_URL + '/categories/' + parentCategories[i].id + '/subcategories')
+                                        .then((response => {
+                                            childrenCategories.push(...response.data)
+                                            for (let i = 0; i < childrenCategories.length; i++) {
+                                                if (childrenCategories[i].id == childId) {
+                                                    accept(parentCategories[i].name);
+                                                }
+                                            }
+                                        }));
+    
+                                } /* Check if any of the children match the initial id */
+                            }
+                        }));
+                        reject();
+                });
+    
             },
             /**
              * Opens a modal with the category details
@@ -190,10 +249,10 @@
                 })
             }
         },
-         
+    
         props: {
             data: []
         }
-     
+    
     }
 </script>
