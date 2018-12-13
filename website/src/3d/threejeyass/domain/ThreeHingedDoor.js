51c9f78f843225cca4651355e8e81ce9cda9095d
João Freitas <1160907@isep.ipp.pt>
2018-12-13 23:29:16 +0000

    Added ThreeHingedDoor.js which represents a HingedDoor with Three.js properties
    see #69

---
+++ website/src/3d/threejeyass/domain/ThreeHingedDoor.js
@@ -0,0 +1,36 @@
+//@ts-check
+
+/**
+ * Requires HingedDoor base properties
+ */
+import HingedDoor from '../../api/domain/HingedDoor';
+
+/**
+ * Requires Face properties
+ */
+import Face from '../../api/domain/Face';
+
+/**
+ * Requires Three.js Object3D for representing Three.js hinged door face
+ */
+import {Object3D} from 'three';
+
+export default class ThreeHingeedDoor extends HingedDoor{
+
+    /**
+     * Builds a new ThreeHingedDoor
+     * @param {Face} face Face with the hinged door face
+     * @param {Object3D} three_face Object3D with Three.js hinged door face
+     * @param {Number} productId Number with the product id
+     * @param {Number} slotId Number with the slot id
+     */
+    constructor(face,three_face,productId,slotId=null){
+        super(face,productId,slotId);
+        this.three_face=three_face;
+    }
+
+    /**
+     * Returns the current hinged door Three.js face
+     */
+    getThreeFace(){return this.three_face;}
+}
