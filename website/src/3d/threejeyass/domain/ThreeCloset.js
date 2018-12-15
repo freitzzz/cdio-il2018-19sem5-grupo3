88d689a2016bbeba30fb39726b5e7b527342c801
João Freitas <1160907@isep.ipp.pt>
2018-12-15 19:29:33 +0000

    Three.js can now be drawn
    see #69

--- website/src/3d/threejeyass/domain/ThreeCloset.js
+++ website/src/3d/threejeyass/domain/ThreeCloset.js
@@ -6,14 +6,14 @@
 import Closet from '../../api/domain/Closet';
 
 /**
- * Requires Face properties
+ * Requires ThreeFace properties
  */
-import Face from '../../api/domain/Face';
+import ThreeFace from '../../threejeyass/domain/ThreeFace';
 
 /**
- * Requires Three.js Object3D for representing Three.js closet faces
+ * Requires Three.js Group and Mesh
  */
-import {Object3D} from 'three';
+import {Group,Mesh} from 'three';
 
 /**
  * Represents a Closet built with Three.js properties
@@ -22,18 +22,26 @@ export default class ThreeCloset extends Closet{
 
     /**
      * Builds a new ThreeCloset with its defined faces
-     * @param {Map<String,Face>} closet_faces Map with the closet faces
-     * @param {Map<String,Object3D>} three_closet_faces Map with the Three.js closet faces
+     * @param {Map<String,ThreeFace>} closet_faces Map with the closet faces
      * @param {Number} productId Number with the product id
      * @param {Number} slotId Number with the slot id
      */
-    constructor(closet_faces,three_closet_faces,productId,slotId=null){
+    constructor(closet_faces,productId,slotId=null){
         super(closet_faces,productId,slotId);
-        this.three_closet_faces=three_closet_faces;
+        this.faceGroup=null;
     }
 
     /**
-     * Returns the current closet Three.js faces
+     * Draws the current Three.js closet
+     * @returns {Group} Group with the created closet
      */
-    getThreeFaces(){return this.three_closet_faces};
+    draw(){
+        let closetGroup=new Group();
+        let closetFaces=this.getClosetFaces().entries();
+        for(let closetFace of closetFaces)
+            closetGroup.add(closetFace["1"].draw());
+        this.faceGroup=closetGroup;
+        this.baseId=closetGroup.id;
+        return closetGroup;
+    }
 }
