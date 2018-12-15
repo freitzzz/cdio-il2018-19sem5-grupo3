b7fc4f12bf136475cc04583e41f451a931d397c4
António Sousa <1161371@isep.ipp.pt>
2018-12-15 16:39:39 +0000

    Add configuration file for eslint [re #4].

---
+++ website/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "parser": "babel-eslint",
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "strict": 0
+    }
+};
