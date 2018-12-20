3795327a1b7d5ce11f72023355d36ad523acefaa
João Freitas <1160907@isep.ipp.pt>
2018-12-20 04:37:07 +0000

    Fixed draw functionality on ThreeDrawer.js
    see #69

--- website/src/3d/threejeyass/domain/ThreeDrawer.js
+++ website/src/3d/threejeyass/domain/ThreeDrawer.js
@@ -23,13 +23,12 @@ export default class ThreeDrawer extends Drawer{
     /**
      * Builds a new ThreeDrawer with its defined faces
      * @param {Map<String,Face>} drawer_faces Map with the drawer faces
-     * @param {Map<String,Object3D>} three_drawer_faces Map with the Three.js drawer faces
      * @param {Number} productId Number with the product id
      * @param {Number} slotId Number with the slot id
      */
-    constructor(drawer_faces,three_drawer_faces,productId,slotId=null){
+    constructor(drawer_faces,productId=null,slotId=null){
         super(drawer_faces,productId,slotId);
-        this.three_drawer_faces=three_drawer_faces;
+        this.three_drawer_faces=null;
         this.drawerGroup=null;
     }
 
@@ -50,8 +49,12 @@ export default class ThreeDrawer extends Drawer{
     draw(){
         let drawerGroup=new Group();
         let drawerFaces=this.getDrawerFaces().entries();
-        for(let drawerFace of drawerFaces)
-            drawerGroup.add(drawerFace["1"].draw());
+        this.three_drawer_faces=new Map();
+        for(let drawerFace of drawerFaces){
+            let drawnFace=drawerFace["1"].draw();
+            drawerGroup.add(drawnFace);
+            this.three_drawer_faces.set(drawerFace["0"],drawnFace);
+        }
         this.drawerGroup=drawerGroup;
         this.baseId=drawerGroup.id;
         return drawerGroup;
