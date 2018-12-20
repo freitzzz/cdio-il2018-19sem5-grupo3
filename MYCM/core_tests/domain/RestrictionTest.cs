4056cb2bf90865c12c1828888ac63304f2bea1c7
António Sousa <1161371@isep.ipp.pt>
2018-12-20 10:29:30 +0000

    Update unit tests with changes made to Restriction's constructor [re #4].

--- MYCM/core_tests/domain/RestrictionTest.cs
+++ MYCM/core_tests/domain/RestrictionTest.cs
@@ -10,97 +10,90 @@ namespace core_tests.domain {
 
         [Fact]
         public void ensureConstructorDetectsNullDescription() {
-            Action action = () => new Restriction(null);
+            Action action = () => new Restriction(null, new WidthPercentageAlgorithm());
 
             Assert.Throws<ArgumentException>(action);
         }
 
         [Fact]
         public void ensureConstructorDetectsEmptyDescription() {
-            Action action = () => new Restriction(String.Empty);
+            Action action = () => new Restriction(String.Empty, new SameMaterialAndFinishAlgorithm());
 
             Assert.Throws<ArgumentException>(action);
         }
 
         [Fact]
         public void ensureConstructorDetectsWhitespacesDescription() {
-            Action action = () => new Restriction("         ");
+            Action action = () => new Restriction("         ", new WidthPercentageAlgorithm());
 
             Assert.Throws<ArgumentException>(action);
         }
 
         [Fact]
         public void ensureInstanceIsCreated() {
-            Restriction instance = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new SameMaterialAndFinishAlgorithm());
 
             Assert.NotNull(instance);
         }
 
         [Fact]
         public void ensureSameInstanceIsEqual() {
-            Restriction instance = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new SameMaterialAndFinishAlgorithm());
 
             Assert.True(instance.Equals(instance));
         }
 
         [Fact]
         public void ensureNullValueIsntEqual() {
-            Restriction instance = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new SameMaterialAndFinishAlgorithm());
 
             Assert.False(instance.Equals(null));
         }
 
         [Fact]
         public void ensureInstanceOfDifferentTypeIsntEqual() {
-            Restriction instance = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new WidthPercentageAlgorithm());
 
             Assert.False(instance.Equals("bananas"));
         }
 
         [Fact]
         public void ensureInstanceWithDifferentDescriptionIsntEqual() {
-            Restriction instance = new Restriction("restriction");
-            Restriction other = new Restriction("bananas");
+            Restriction instance = new Restriction("restriction", new WidthPercentageAlgorithm());
+            Restriction other = new Restriction("bananas", new WidthPercentageAlgorithm());
 
             Assert.False(instance.Equals(other));
         }
 
         [Fact]
         public void ensureInstanceWithSameDescriptionIsEqual() {
-            Restriction instance = new Restriction("restriction");
-            Restriction other = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new WidthPercentageAlgorithm());
+            Restriction other = new Restriction("restriction", new SameMaterialAndFinishAlgorithm());
 
             Assert.True(instance.Equals(other));
         }
 
         [Fact]
         public void ensureEqualRestrictionsHaveSameHashCode() {
-            Restriction instance = new Restriction("restriction");
-            Restriction other = new Restriction("restriction");
+            Restriction instance = new Restriction("restriction", new SameMaterialAndFinishAlgorithm());
+            Restriction other = new Restriction("restriction", new WidthPercentageAlgorithm());
 
             Assert.True(instance.GetHashCode().Equals(other.GetHashCode()));
         }
 
         [Fact]
         public void ensureDifferentRestrictionsHaveDifferentHashCode() {
-            Restriction instance = new Restriction("restriction");
-            Restriction other = new Restriction("bananas");
+            Restriction instance = new Restriction("restriction", new WidthPercentageAlgorithm());
+            Restriction other = new Restriction("bananas", new WidthPercentageAlgorithm());
 
             Assert.False(instance.GetHashCode().Equals(other.GetHashCode()));
         }
 
         [Fact]
         public void ensureToStringWorks() {
-            Restriction instance = new Restriction("oh hi mark");
-            Restriction other = new Restriction("oh hi mark");
+            Restriction instance = new Restriction("oh hi mark", new WidthPercentageAlgorithm());
+            Restriction other = new Restriction("oh hi mark", new WidthPercentageAlgorithm());
             Assert.Equal(instance.ToString(), other.ToString());
-        }
-
-        [Fact]
-        public void ensureToDTOWorks() {
-            Restriction instance = new Restriction("lil pong");
-            Restriction other = new Restriction("lil pong");
-            Assert.Equal(instance.toDTO().description, other.toDTO().description);
         }
     }
 }
