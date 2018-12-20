bfd07bdefd5cf7da48db9edae72c3bf2edf6fcdd
João Freitas <1160907@isep.ipp.pt>
2018-12-20 14:53:39 +0000

    Fixed Watcher.js watch and trigger functionalities
    see #69

--- website/src/3d/api/domain/Watcher.js
+++ website/src/3d/api/domain/Watcher.js
@@ -26,6 +26,8 @@ export default class Watcher{
     constructor(){
         this.products=new Map();
         this.productsActions=new Map();
+        this.actionsWatchers=new Map();
+        this.watchersActions=new Map();
     }
 
     /**
@@ -43,24 +45,58 @@ export default class Watcher{
      */
     get(productId){return this.products.get(productId);}
 
+    ///**
+    // * Watches a new product
+    // * @param {BaseProduct} product BaseProduct with the product being watched
+    // * @param {Map<String,Action>} actionsMap Map with the product actions
+    // */
+    //watch(product,actionsMap){
+    //    this.products.set(product.id(),product);
+    //    this.productsActions.set(product.id(),actionsMap);
+    //}
+
     /**
-     * Watches a new product
-     * @param {BaseProduct} product BaseProduct with the product being watched
-     * @param {Map<String,Action>} actionsMap Map with the product actions
+     * Watches an action
+     * @param {String} actionType String with the action type being watched
+     * @param {Action} action Action with the action being executed after watched
      */
-    watch(product,actionsMap){
-        this.products.set(product.id(),product);
-        this.productsActions.set(product.id(),actionsMap);
+    watch(actionType,action){
+        this.actionsWatchers.set(actionType,action);
+        this.watchersActions.set(actionType,[]);
     }
 
     /**
-     * Triggers a product action
-     * @param {BaseProduct} product BaseProduct with the product which actions will be executed
-     * @param {String} action String with the product action identifier
+     * Queues an action in a certain action being watched
+     * @param {String} actionType String with the action type being watched
+     * @param {Action} action Action with the action being queued in the watched action
      */
-    trigger(product,action){
-        let productActions=this.productsActions.get(product);
-        let productAction=productActions.get(action);
-        productAction.execute();
+    queueAction(actionType,action){
+        this.watchersActions.get(actionType).unshift(action);//Unshift => Pushs an element on the first position of the array (Necessary for Queue (FIFO) )
+    }
+    
+    ///**
+    // * Triggers a product action
+    // * @param {BaseProduct} product BaseProduct with the product which actions will be executed
+    // * @param {String} action String with the product action identifier
+    // */
+    //trigger(product,action){
+    //    let productActions=this.productsActions.get(product);
+    //    let productAction=productActions.get(action);
+    //    productAction.execute();
+    //}
+
+    /**
+     * Triggers an action
+     * @param {String} action String with the action type of the action being triggered
+     */
+    trigger(action){
+        let watcherAction=this.actionsWatchers.get(action);
+        let actionsWatched=this.watchersActions.get(action);
+        watcherAction
+            .execute()
+            .then(()=>{
+                for(let i=0;i<actionsWatched.length;i++)
+                    actionsWatched.pop().execute();
+            });
     }
 }
