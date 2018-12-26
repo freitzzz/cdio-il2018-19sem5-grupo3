a9ff701174503a29342b9aa4316144429efe6713
João Freitas <1160907@isep.ipp.pt>
2018-12-26 22:28:03 +0000

    Added LoadingDialog.vue which represents a Vue Component that
    acts as a "loading component" (shows the state of an action which is
    being loaded)
    
    see #39

---
+++ website/src/components/UIComponents/LoadingDialog.vue
@@ -0,0 +1,35 @@
+<template>
+    <section>
+        <b-modal :active.sync="active" :width="200">
+            <div class="modal-card" style="height:150px;width:auto">
+                <b-field>
+                    <b-loading :active.sync="active" :is-full-page="false" :canCancel="true"/>
+                </b-field>
+                <b-field :label="message" class="has-text-centered"/>
+            </div>
+        </b-modal>
+    </section>
+</template>
+
+<script>
+
+export default {
+    /**
+     * Component data
+     */
+    data(){
+        return {
+            active:true,
+            isLoading:true,
+            isFullPage:true
+        }
+    },
+    name:"LoadingDialog",
+    /**
+     * Component props
+     */
+    props:{
+        message:String
+    }
+}
+</script>
