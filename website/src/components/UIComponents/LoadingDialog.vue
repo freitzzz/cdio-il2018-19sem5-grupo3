c725b5909fd44af1abbb3c897f90a2cc90af7853
João Freitas <1160907@isep.ipp.pt>
2018-12-27 00:57:39 +0000

    LoadingDialog.vue now supports initial timeouts
    (initial timeout is the time which the loading dialog will remain closed
    before turning active)
    
    see #39

--- website/src/components/UIComponents/LoadingDialog.vue
+++ website/src/components/UIComponents/LoadingDialog.vue
@@ -1,11 +1,12 @@
 <template>
     <section>
-        <b-modal :active.sync="active" :width="200">
+        <!-- v-model on active property is required due to syncing active with isActive -->
+        <b-modal v-model="active" :active.sync="isActive" :width="200">
             <div class="modal-card" style="height:150px;width:auto">
                 <b-field>
-                    <b-loading :active.sync="active" :is-full-page="false" :canCancel="true"/>
+                    <b-loading :active.sync="isActive" :is-full-page="false" :canCancel="true"/>
                 </b-field>
-                <b-field :label="message" class="has-text-centered"/>
+                <b-field :label.sync="message" class="has-text-centered"/>
             </div>
         </b-modal>
     </section>
@@ -15,21 +16,71 @@
 
 export default {
     /**
+     * Component call when component is created
+     */
+    created(){
+        this.timeoutLoadingDialog();
+    },
+    /**
      * Component data
      */
     data(){
         return {
-            active:true,
-            isLoading:true,
-            isFullPage:true
+            /**
+             * Indicates if the current dialog & loading components are active
+             */
+            isActive:false
         }
     },
+    /**
+     * Component methods
+     */
+    methods:{
+        /**
+         * Times the opening of the loading dialog
+         */
+        timeoutLoadingDialog(){
+            if(this.$props.active){
+                new Promise((accept)=>{
+                    setTimeout(accept,this.$props.timeout);
+                }).then(()=>{
+                    this.isActive=this.$props.active;
+                });
+            }else{
+                this.isActive=false;
+            }
+        }
+    },
+    /**
+     * Component Name
+     */
     name:"LoadingDialog",
     /**
      * Component props
      */
     props:{
-        message:String
+        /**
+         * Synced property which indicates if the loading dialog is active
+         */
+        active:Boolean,
+        /**
+         * Synced property which represents the loading dialog message
+         */
+        message:String,
+        /**
+         * Property which indicates if the loading dialog should timeout X time before turning active
+         * (Value is in milliseconds, default = 0 => no timeout)
+         */
+        timeout:{
+            type:Number,
+            default:0
+        }
+    },
+    /**
+     * Component call when component is updated
+     */
+    updated(){
+        this.timeoutLoadingDialog();
     }
 }
 </script>
