cc21d287b1d08d24ba38f54a03d65cff02d102a6
João Freitas <1160907@isep.ipp.pt>
2019-01-02 17:49:53 +0000

    UserMVService can now create ActivateUserMV and AddUserRolesMV
    based on their authentication details, activation code and new roles to
    add
    
    see #64

--- MYCA/myca/core/src/main/java/cdiomyc/core/mv/users/UserMVService.java
+++ MYCA/myca/core/src/main/java/cdiomyc/core/mv/users/UserMVService.java
@@ -1,8 +1,12 @@
 package cdiomyc.core.mv.users;
 
+import cdiomyc.core.domain.Role;
 import cdiomyc.core.domain.auth.Auth;
+import cdiomyc.core.mv.authentication.AuthenticationMV;
 import cdiomyc.core.mv.authentication.CredentialsAuthenticationMV;
 import cdiomyc.support.utils.JWTUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Service for creating and manipulating User Model Views
@@ -41,5 +45,41 @@ public class UserMVService {
         CreatedUserMV createdUserMV = new CreatedUserMV();
         createdUserMV.token = JWTUtils.encode(auth.id());
         return createdUserMV;
+    }
+    
+    /**
+     * Creates an ActivateUserMV based on the user to authenticate and his activation code
+     * @param authenticationMV AuthenticationMV with the user authentication details
+     * @param activationCode String with the user activation code
+     * @return ActivateUserMV with the user to activate details
+     */
+    public static ActivateUserMV fromUserMVToActivateUserMV(AuthenticationMV authenticationMV,String activationCode){
+        ActivateUserMV activateUserMV=new ActivateUserMV();
+        activateUserMV.activationCode=activationCode;
+        activateUserMV.userAuthentication=authenticationMV;
+        return activateUserMV;
+    }
+    
+    /**
+     * Creates an AddUserRolesMV based on the user to add roles and his new roles
+     * @param authenticationMV AuthenticationMV with the user authentication details
+     * @param roles List with the new user roles
+     * @return AddUserRolesMV with the add roles to user details
+     */
+    public static AddUserRolesMV fromUserMVToAddUserRolesMV(AuthenticationMV authenticationMV,List<String> roles){
+        AddUserRolesMV addUserRolesMV=new AddUserRolesMV();
+        List<Role> userRoles=new ArrayList<>(roles.size());
+        roles.forEach(role->{
+            Role[] existingRoles=Role.values();
+            for(int i=0;i<existingRoles.length;i++){
+                if(existingRoles[i].name().equals(role) || existingRoles[i].toString().equals(role)){
+                    userRoles.add(existingRoles[i]);
+                    i=existingRoles.length;
+                }
+            }
+        });
+        addUserRolesMV.userAuthentication=authenticationMV;
+        addUserRolesMV.userRoles=userRoles;
+        return addUserRolesMV;
     }
 }
