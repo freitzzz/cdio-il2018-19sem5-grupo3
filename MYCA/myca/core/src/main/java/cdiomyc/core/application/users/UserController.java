4d21743ec1da8976287b9f66c2b303c3dcabe584
João Freitas <1160907@isep.ipp.pt>
2019-01-02 17:43:38 +0000

    UserController can now add roles to a user
    see #64

--- MYCA/myca/core/src/main/java/cdiomyc/core/application/users/UserController.java
+++ MYCA/myca/core/src/main/java/cdiomyc/core/application/users/UserController.java
@@ -1,10 +1,12 @@
 package cdiomyc.core.application.users;
 
+import cdiomyc.core.domain.Role;
 import cdiomyc.core.domain.User;
 import cdiomyc.core.domain.auth.Auth;
 import cdiomyc.core.domain.auth.AuthFactory;
 import cdiomyc.core.mv.authentication.AuthenticationMV;
 import cdiomyc.core.mv.users.ActivateUserMV;
+import cdiomyc.core.mv.users.AddUserRolesMV;
 import cdiomyc.core.mv.users.CreateUserMV;
 import cdiomyc.core.mv.users.CreatedUserMV;
 import cdiomyc.core.mv.users.UserMVService;
@@ -44,10 +46,22 @@ public class UserController {
      * @param activateUserMV ActivateUserMV with the user to activate details 
      */
     public static void activateUser(ActivateUserMV activateUserMV){
-        Auth userAuth=AuthFactory.createAuth(activateUserMV);
+        Auth userAuth=AuthFactory.createAuth(activateUserMV.userAuthentication);
         UserRepository userRepo=PersistenceContext.repositories().createUserRepository();
-        User userToActivate=userRepo.findUserByAuthenticationDetails(activateUserMV);
+        User userToActivate=userRepo.findEID(userAuth);
         userToActivate.activate(activateUserMV.activationCode);
         userRepo.update(userToActivate);
+    }
+    
+    /**
+     * Adds roles to an user
+     * @param addUserRolesMV AddUserRolesMV with the user new roles details 
+     */
+    public static void addUserRoles(AddUserRolesMV addUserRolesMV){
+        Auth userAuth=AuthFactory.createAuth(addUserRolesMV.userAuthentication);
+        UserRepository userRepo=PersistenceContext.repositories().createUserRepository();
+        User userToAddRoles=userRepo.findEID(userAuth);
+        userToAddRoles.addRoles(addUserRolesMV.userRoles);
+        userRepo.update(userToAddRoles);
     }
 }
