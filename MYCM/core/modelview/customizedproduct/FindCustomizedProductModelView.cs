d1a257482329d1a21b3bd69515b06a33cfab6e89
António Sousa <1161371@isep.ipp.pt>
2019-01-06 23:02:04 +0000

    Add options to FindCustomizedProductModelView in order to specifiy the units in which the CustomizedProduct's dimensions will be displayed [re #16].

--- MYCM/core/modelview/customizedproduct/FindCustomizedProductModelView.cs
+++ MYCM/core/modelview/customizedproduct/FindCustomizedProductModelView.cs
@@ -5,8 +5,30 @@ namespace core.modelview.customizedproduct
     /// </summary>
     public class FindCustomizedProductModelView
     {
+        /// <summary>
+        /// Identifier of the CustomizedProduct being retrieved.
+        /// </summary>
+        /// <value>Gets/Sets the persistence identifier.</value>
         public long customizedProductId { get; set; }
 
-        //TODO: add units in which the customized product will be retrieved
+        /// <summary>
+        /// Additional options used for retrieving a CustomizedProduct.
+        /// </summary>
+        /// <returns>Gets/Sets the options.</returns>
+        public FindCustomizedProductModelViewOptions options { get; set; } = new FindCustomizedProductModelViewOptions();
     }
+
+
+    /// <summary>
+    /// Class representing the options used for retrieving and instance of CustomizedProduct.
+    /// </summary>
+    public class FindCustomizedProductModelViewOptions
+    {
+        /// <summary>
+        /// Unit to which the CustomizedProduct's dimensions will be converted.
+        /// </summary>
+        /// <value>Gets/Sets the unit.</value>
+        public string unit { get; set; }
+    }
+
 }
