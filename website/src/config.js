2b05826f86fbc1a51482cfa183dec2e687001104
João Freitas <1160907@isep.ipp.pt>
2019-01-06 21:45:36 +0000

    Updated MYCA URL being used during development stage
    see #39

--- website/src/config.js
+++ website/src/config.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     mycm = "http://localhost:5000/mycm/api";
     myco = "http://localhost:4000/myco/api";
-    myca = "http://localhost:8080/myca/api";
+    myca = "http://localhost:8081/myca/api";
 }
 
 export const MYCM_API_URL = mycm;
