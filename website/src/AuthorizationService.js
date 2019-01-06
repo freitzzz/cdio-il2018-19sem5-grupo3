acaf24923d7da75ee729295b84718edcceab551a
João Freitas <1160907@isep.ipp.pt>
2019-01-06 21:52:52 +0000

    Added AuthorizationService.js which holds a collection of MYCA
    authorization related services
    
    see #39

---
+++ website/src/AuthorizationService.js
@@ -0,0 +1,76 @@
+
+/**
+ * Requires Axios for HTTP requests
+ */
+import Axios from "axios";
+
+/**
+ * Sets all Axios requests to include cookies
+ */
+Axios.defaults.withCredentials=true;
+
+/**
+ * Sets all Axios requests with the "Secrete" header with "Secrete" as the value (used on signup)
+ */
+//TODO:Deprecate secrete function on future
+Axios.defaults.headers["secrete"]="Secrete";
+
+/**
+ * Requires MYCA API url
+ */
+import {MYCA_API_URL} from "./config";
+
+/**
+ * Grants that a user is an administrator in a promise way
+ */
+export const grantUserIsAdministrator=()=>{
+    return Axios.get(MYCA_API_URL+"/autho?contentmanager=true");
+};
+
+/**
+ * Grants that a user is a client in a promise way
+ */
+export const grantUserIsClient=()=>{
+    return Axios.get(MYCA_API_URL+"/autho");
+};
+
+/**
+ * Grants that a user is a content manager in a promise way
+ */
+export const grantUserIsContentManager=()=>{
+    return Axios.get(MYCA_API_URL+"/autho?contentmanager=true");
+};
+
+/**
+ * Grants that a user is a logistic manager in a promise way
+ */
+export const grantUserIsLogisticManager=()=>{
+    return Axios.get(MYCA_API_URL+"/autho?logisticmanager=true");
+};
+
+/**
+ * Returns all user authorizations in a object in a promise way
+ */
+export const getUserAuthorizations=()=>{
+    return new Promise((accept,reject)=>{
+        let userAuthorizations={
+            administrator:false,
+            client:false,
+            contentManager:false,
+            logisticManager:false
+        };
+        Axios
+            .all([grantUserIsAdministrator(),grantUserIsClient(),grantUserIsContentManager(),grantUserIsLogisticManager()])
+            .then(([isAdminstrator,isClient,isContentManager,isLogisticManager])=>{
+                userAuthorizations.administrator=isAdminstrator.status==204;
+                userAuthorizations.client=isClient.status==204;
+                userAuthorizations.contentManager=isContentManager.status==204;
+                userAuthorizations.logisticManager=isLogisticManager.status==204;
+                accept(userAuthorizations);
+            })
+            .catch(()=>{
+                //TODO: Investigate
+                reject(userAuthorizations);
+            });
+    });
+};
