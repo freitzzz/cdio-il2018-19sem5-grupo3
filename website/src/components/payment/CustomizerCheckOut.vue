25db5f8fcc04163006ff6632c7db0a6fbbc00f2e
桜ちゃん <1160941@isep.ipp.pt>
2019-01-08 13:33:18 +0000

    Undo customizer check out
    see #70 #39 #15

--- website/src/components/payment/CustomizerCheckOut.vue
+++ website/src/components/payment/CustomizerCheckOut.vue
@@ -1,232 +1,184 @@
 <template>
-  <main class="main-content">
-    <div class="invoice-app">
-      <div class="header">
-        <div>
-          <!-- h1 with the name of the company ??? -->
-          <p>Date: <input readonly v-model="currentDate"></p>
-        </div>
-        <div class="section-spacer">
-          <p><strong>Bill to:</strong></p>
-          <!-- client data it will not be a text area -->
-          <input v-model="clientData" readonly>
-        </div>
+  <div class="row">
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <div class="col-75">
+      <div class="container">
+        <form action="/action_page.php">
+  
+          <div class="row">
+            <div class="col-50">
+              <h3>Billing Address</h3>
+              <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+              <input type="text" id="fname" name="firstname" placeholder="John M. Doe">
+              <label for="email"><i class="fa fa-envelope"></i> Email</label>
+              <input type="text" id="email" name="email" placeholder="john@example.com">
+              <label for="adr"> <!-- <i class="fa fa-address-card-o"></i>  -->Address</label>
+              <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+              <label for="city"><!-- <i class="fa fa-institution"> --><!-- </i> --> City</label>
+              <input type="text" id="city" name="city" placeholder="New York">
+              <label for="zip">Zip</label>
+              <input type="text" id="zip" name="zip" placeholder="10001">
+  
+  
+            </div>
+  
+            <div class="col-50">
+  
+              <label for="cname">Name on Card</label>
+              <input type="text" id="cname" name="cardname" placeholder="John More Doe">
+              <label for="ccnum">Credit card number</label>
+              <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+              <label for="expmonth">Exp Month</label>
+              <input type="text" id="expmonth" name="expmonth" placeholder="September">
+  
+              <div class="row">
+                <div class="col-50">
+                  <label for="expyear">Exp Year</label>
+                  <input type="text" id="expyear" name="expyear" placeholder="2018">
+                </div>
+                <div class="col-50">
+                  <label for="cvv">CVV</label>
+                  <input type="text" id="cvv" name="cvv" placeholder="352">
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
+        </form>
+  
+ 
       </div>
-      <div>
-        <label for="currency-picker">Currency:</label>
-        <select v-model="currency">
-          <option v-for="currencyValue in currencies" :key="currencyValue.currency" :value="currencyValue">{{currencyValue.currency}}</option>
-        </select>
+     
+    </div>
+  
+    <div class="col-25">
+      <div class="container">
+        <h4>Cart
+          <span class="price" style="color:black">
+                <i class="fa fa-shopping-cart"></i>
+                <b>4</b>
+              </span>
+        </h4>
+        <p><a>Pole By Stephano</a> <span class="price">$499.99</span></p>
+        <p><a>Drawer</a> <span class="price">$5</span></p>
+        <p><a>Closet</a> <span class="price">$5</span></p>
+        <p><a>Matte Finish</a> <span class="price">$1.99</span></p>
+        <hr>
+        <p>Total <span class="price" style="color:black"><b>$511.98</b></span></p>
       </div>
-      <table class="responsive-table"></table>
-      <!-- button add new item (not necessary) -->
-      <table></table>
+          <input type="checkbox" name="checkbox" value="check" id="agree" /> I have read and agree to the Terms and Conditions and Privacy Policy
+        <p></p>
+        <i class="btn btn-primary">Continue to checkout</i>
+
     </div>
-    <div class="btn-section">
-      <i class="btn btn-primary" @click="previousPanel()">Go back</i>
-      <i class="btn btn-primary" @click="payment()">Payment</i>
-    </div>
-  </main>
+  </div>
 </template>
 
 <script>
-  import CurrencyPerArea from './../../services/mycm_api/requests/currenciesperarea.js'
   import Vue from 'vue';
   /* import { library } from '@fortawesome/fontawesome-svg-core';
   import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; */
   import store from "./../../store";
-  import {
-    DEACTIVATE_CAN_MOVE_COMPONENTS
-  } from "./../../store/mutation-types.js";
-  
-  /* Constants: */
-  
-  const TAX_RATE_PORTUGAL = 23;
-  
-  
+  import { DEACTIVATE_CAN_MOVE_COMPONENTS } from "./../../store/mutation-types.js";
+
   export default {
     name: "CustomizerCheckOut",
-    data() {
-      return {
-        currentDate: this.getCurrentDate(),
-        taxRate: TAX_RATE_PORTUGAL, //TODO:
-        clientData: "Fernando Mendes",
-        /* Client data is: name, address and phone number */
-        currency: "",
-        currencies: [],
-        items: [],
-        item: []
-      }
-    },
-    created() {
+    created(){
       store.dispatch(DEACTIVATE_CAN_MOVE_COMPONENTS);
-      
-      /**Get list of all the currencies */
-      CurrencyPerArea.getCurrencies()
-      .then(response =>this.currencies.push(...response.data))
-      .catch(error =>{
-        this.$toast.open("An error occured trying to fetch the currencies.");
-      });
-    },
-    methods: {
-      getCurrentDate() {
-        var today = new Date();
-  
-        var dd, mm, yyyy;
-        dd = today.getDate();
-        mm = today.getMonth() + 1;
-        yyyy = today.getFullYear();
-  
-        dd < 10 ? dd = '0' + dd : dd;
-        mm < 10 ? mm = '0' + mm : mm;
-  
-        return dd + '/' + mm + '/' + yyyy;
-      },
-      previousPanel() {
-        this.$emit("back");
-      },
-      payment() {
-        this.$toast.open('Payment Successful');
-      }
-    }
+    }  
   }
 </script>
 
 <style>
-  .btn-section {
-    width:300px;
-
-  }
-  .center-controls {
-    text-align: center;
-    margin: auto;
-  }
-  
-  .main-content {
-    min-height: 100vh;
-    padding: 3%;
+  .row {
+    display: -ms-flexbox;
+    /* IE10 */
     display: flex;
-    justify-content: center;
-    align-items: center;
+    -ms-flex-wrap: wrap;
+    /* IE10 */
+    flex-wrap: wrap;
+    margin: 13px;
+    font-size: 10px;
+    height:70%;
   }
   
-  .invoice-app {
-    background-color: white;
-    padding: 2%;
-    border-radius: 0.5rem;
-    width: 350px;
+  .col-25 {
+    -ms-flex: 25%;
+    /* IE10 */
+    flex: 25%;
   }
   
-  
-  /* 
-  .header {
-  
-  
-       div {
-          &:nth-child(-n+1){
-              @media (min-width: 761px) {
-                  order: 1;
-                  flex: 1;
-                  text-align: right;
-                  padding-left: 1rem;
-              }
-          }
-      }
-  } */
-  
-  .section-spacer {
-    margin: 1rem 0;
+  .col-50 {
+    -ms-flex: 50%;
+    /* IE10 */
+    flex: 50%;
   }
   
-  input,
-  select,
-  textarea {
-    background-color: transparentize(color=white, amount=0.7);
-    border: none;
-    display: inline-block;
-    transition: background-color 0.3s ease-in-out;
+  .col-75 {
+    -ms-flex: 75%;
+    /* IE10 */
+    flex: 75%;
+  }
+  
+  .col-25,
+  .col-50,
+  .col-75 {
+    padding: 0 5px;
+  }
+  
+  .container {
+    background-color: #f2f2f2;
+    padding: 5px 20px 15px 20px;
+    border: 1px solid lightgrey;
+    border-radius: 3px;
     width: 100%;
   }
   
-  textarea {
+  input[type=text] {
     width: 100%;
-    min-height: 80px;
+    margin-bottom: 5px;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+  }
+  
+  label {
+    margin-bottom: 10px;
+    display: block;
+  }
+  
+  .icon-container {
+    margin-bottom: 20px;
+    padding: 7px 0;
+  }
+  
+  .btn {
+    background-color: #0ba2db;
+    color: white;
+    margin: 10px 0;
+    border-radius: 3px;
+  }
+  
+  .btn:hover {
+    background-color: #0ba2db;
+  }
+  
+  span.price {
+    float: right;
+    color: grey;
   }
   
   
-  /* select {
-      only screen and (max-width: 760px) {
-          width: 100%;
-      }
+  /* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
   
-      @media print {
-          appearance: none;
-      }
-  } */
-  
-  table {
-    width: auto;
-    border-collapse: collapse;
-    margin: 2rem 0;
-  }
-  
-  
-  /* 
-  .responsive-table {
-      width: 100%;
-      @media 
-      only screen and (max-width: 760px) {
-  
-          table, thead, tbody, th, td, tr { 
-              display: block; 
-          }
-  
-          thead tr { 
-              position: absolute;
-              top: -9999px;
-              left: -9999px;
-          }
-  
-          tr {
-              padding: 2rem 0;
-          }
-  
-          
-          td[data-label] {
-              position: relative;
-              padding-left: 40%; 
-              display: flex;
-              align-items: center;
-  
-              &:before { 
-                  content: attr(data-label);
-                  position: absolute;
-                  top: 0.5rem;
-                  left: 0;
-                  width: 35%; 
-                  padding-right: 10px; 
-                  white-space: nowrap;
-                  font-weight: bold;
-              }
-          }
-      }
-  } */
-  
-  button {
-    background-color: green;
-    border: none;
-    border-radius: 100px;
-    padding: 0.5rem 1rem;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-  }
-  
-  .text-right {
-    text-align: right;
-  }
-  
-  .text-bold {
-    font-weight: bold;
+  @media (max-width: 300px) {
+    .row {
+      flex-direction: column-reverse;
+    }
+    .col-25 {
+      margin-bottom: 20px;
+    }
   }
 </style>
