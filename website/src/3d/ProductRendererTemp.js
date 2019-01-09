fff5efb9ec6a7cfc869747ea451ae0c62b3cf285
Ana Margarida Tavares Guerra <1161191@isep.ipp.pt>
2019-01-09 12:41:40 +0000

    Correction in the move drawer. see !5 #39

--- website/src/3d/ProductRendererTemp.js
+++ website/src/3d/ProductRendererTemp.js
@@ -164,6 +164,8 @@ export default class ProductRenderer {
    */
   selected_component;
 
+  selected_module;
+
   /**
    * Instance variable that represents the object being hovered (null if none)
    */
@@ -222,6 +224,8 @@ export default class ProductRenderer {
   /**Number of dimensions in question */
   NUMBER_DIMENSIONS;
 
+  module_group;
+
 
   // ---------------- End of resize control --------------------------
 
@@ -230,6 +234,8 @@ export default class ProductRenderer {
    * @param {HTMLCanvasElement} htmlCanvasElement - HTMLCanvasElement to which the renderer will be attached.
    */
   constructor(htmlCanvasElement) {
+
+    this.module_group = new THREE.Group();
 
     /* Create vector for resizing purposes: */
 
@@ -265,6 +271,7 @@ export default class ProductRenderer {
     this.selected_slot = null;
     this.selected_face = null;
     this.selected_component = null;
+    this.selected_module = null;
     this.hovered_object = null;
     this.plane = null;
     this.mouse = new THREE.Vector2();
@@ -556,6 +563,7 @@ export default class ProductRenderer {
       slot: component.slot
     });
     this.selected_component = null;
+    this.selected_module = null;
     this.controls.enabled = true;
   }
 
@@ -1191,6 +1199,24 @@ export default class ProductRenderer {
             this.offset = this.intersection.x - this.selected_slot.position.x;
           }
         }
+      }
+      //Move drawer
+      
+      for (let j = 0; j < this.closet_modules_ids.length; j++) {
+        let top_module = this.group.getObjectById(this.closet_modules_ids[j]);
+        if (top_module == intersected_object) {
+              this.controls.enabled = false;
+              this.selected_component = intersected_object;
+              this.selected_module = intersected_object;
+               if (this.raycaster.ray.intersectPlane(this.plane, this.intersection)) {
+                this.offset = this.intersection.y - this.selected_component.position.y;
+                /* for(let a = 0; a< this.closet_drawers_ids.length; a++){
+                    //this.controls.enabled = false;
+                    this.selected_component = this.group.getObjectById(this.closet_drawers_ids[a]);
+                    this.offset = this.intersection.y - this.selected_component.position.y;
+                }   */
+              }
+          }
       }
 
       //Checks if the selected object is a shelf
@@ -1832,6 +1858,7 @@ export default class ProductRenderer {
     this.selected_face = null;
     //Sets the selected closet component to null (the component stops being selected)
     this.selected_component = null;
+    this.selected_module = null;
   }
 
   /**
@@ -1973,13 +2000,22 @@ export default class ProductRenderer {
         computedYPosition >= this.group.getObjectById(this.closet_faces_ids[0]).position.y &&
         computedXPosition < this.group.getObjectById(this.closet_faces_ids[3]).position.x &&
         computedXPosition >= this.group.getObjectById(this.closet_faces_ids[2]).position.x) {
-        this.selected_component.position.y = computedYPosition; //Sets the new position as long as the component stays within the closet boundaries
+          if(this.selected_component == this.selected_module){
+            for(let i = 0; i < this.closet_drawers_ids.length; i++){
+              this.group.getObjectById(this.closet_drawers_ids[i]).position.y = computedYPosition;
+            }
+            for(let i = 0; i < this.closet_modules_ids.length; i++){
+              var old_position = this.group.getObjectById(this.closet_modules_ids[i]).position.y;
+              this.group.getObjectById(this.closet_modules_ids[i]).position.y = old_position+computedYPosition;
+            }
+          } else this.selected_component.position.y = computedYPosition; //Sets the new position as long as the component stays within the closet boundaries
       } else {
         store.dispatch(SET_COMPONENT_TO_REMOVE, {
           model: this.selected_component.userData.model,
           slot: this.selected_component.userData.slot
         });
       }
+
     }
 
     var intersects = this.raycaster.intersectObjects(this.scene.children[0].children);
