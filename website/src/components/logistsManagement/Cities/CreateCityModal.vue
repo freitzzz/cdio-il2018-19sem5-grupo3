4d9929ae5096f48ff710d1bb6999bcd6141d563e
桜ちゃん <1160941@isep.ipp.pt>
2019-01-10 23:56:59 +0000

    Refactor of modal
    see #15 #39 #75

--- website/src/components/logistsManagement/Cities/CreateCityModal.vue
+++ website/src/components/logistsManagement/Cities/CreateCityModal.vue
@@ -6,77 +6,75 @@
         <section class="modal-card-body">
             <!-- Name -->
             <b-field label="Name">
-                <b-input
-                type="String"
-                v-model="nameItem.value"
-                :placeholder="placeholders.name"
-                icon="format-quote-open"
-                required>
+                <b-input type="String" v-model="nameItem.value" :placeholder="placeholders.name" icon="format-quote-open" required>
                 </b-input>
             </b-field>
             <!-- Latitude -->
             <b-field>
-                <b-input
-                type="number"
-                v-model="latitudeItem.value"
-                :placeholder="placeholders.latitude"
-                icon="crosshairs"
-                required>
+                <b-input step="any" type="text" pattern="[0-9]+" v-model="latitudeItem.value" :placeholder="placeholders.latitude" icon="crosshairs" required>
                 </b-input>
             </b-field>
             <!-- Longitude -->
             <b-field>
-                <b-input
-                type="number"
-                v-model="longitudeItem.value"
-                :placeholder="placeholders.longitude"
-                icon="crosshairs-gps"
-                required>
+                <b-input step="any" type="text" pattern="[0-9]+" v-model="longitudeItem.value" :placeholder="placeholders.longitude" icon="crosshairs-gps" required>
                 </b-input>
             </b-field>
         </section>
-        <footer class="moda-card-foot">
+        <footer class="modal-card-foot">
             <div class="has-text-centered">
-                <button class="btn-primary" @click="emitCity($parent)">Create</button>
+                <button class="btn-primary" @click="this.postCity">Create</button>
             </div>
         </footer>
     </div>
 </template>
 
 <script>
+    import CityRequests from "../../../services/myco_api/requests/cities.js"
+    
     export default {
         /**
          * Component data
          */
         data() {
             return {
-                nameItem:{
-                    value:null
+                nameItem: {
+                    value: null
                 },
-                latitudeItem:{
-                    value:null
+                latitudeItem: {
+                    value: null
                 },
-                longitudeItem:{
-                    value:null
+                longitudeItem: {
+                    value: null
                 },
-                placeholders:{
-                    name:"Lisbon",
-                    latitude:"123",
-                    longitude:"12356"
+                placeholders: {
+                    name: "Lisbon",
+                    latitude: "123",
+                    longitude: "12356"
                 }
-                
+    
             }
         },
-         /**
-         * Emits the product to the father component
-         */
-        emitCity(modal){
-            let cityDetails ={
-                name: this.nameItem.value,
-                latitude:this.latitude.value,
-                longitude:this.longitude.value
-            };
-            this.$emit('emitCity',cityDetails);
+        methods: {
+            /**
+             * Emits the product to the father component
+             */
+            postCity() {
+            
+                var cityDetails = {
+                    name: this.nameItem.value,
+                    latitude: this.latitudeItem.value,
+                    longitude: this.longitudeItem.value
+                };
+                CityRequests.createCity(cityDetails)
+                    .then((response) => {
+                        alert("teste");
+                        this.$toast.open("The product was created with success!");
+                    })
+                    .catch((error_message) => {
+                        this.$toast.open("There was an error creating the city.");
+                    });
+    
+            }
         }
     }
 </script>
