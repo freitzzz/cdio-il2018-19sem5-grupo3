1d4b55068dc3d7891a07b533e787f375696f174e
桜ちゃん <1160941@isep.ipp.pt>
2019-01-10 22:59:45 +0000

    Finished elaboration of CreateCityModal
    see #15 #39 #75

---
+++ website/src/components/logistsManagement/Cities/CreateCityModal.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="modal-card" style="width:auto">
+        <header class="modal-card-head">
+            <p class="modal-card-title">New City</p>
+        </header>
+        <section class="modal-card-body">
+            <!-- Name -->
+            <b-field label="Name">
+                <b-input
+                type="String"
+                v-model="nameItem.value"
+                :placeholder="placeholders.name"
+                icon="format-quote-open"
+                required>
+                </b-input>
+            </b-field>
+            <!-- Latitude -->
+            <b-field>
+                <b-input
+                type="number"
+                v-model="latitudeItem.value"
+                :placeholder="placeholders.latitude"
+                icon="crosshairs"
+                required>
+                </b-input>
+            </b-field>
+            <!-- Longitude -->
+            <b-field>
+                <b-input
+                type="number"
+                v-model="longitudeItem.value"
+                :placeholder="placeholders.longitude"
+                icon="crosshairs-gps"
+                required>
+                </b-input>
+            </b-field>
+        </section>
+        <footer class="moda-card-foot">
+            <div class="has-text-centered">
+                <button class="btn-primary" @click="emitCity($parent)">Create</button>
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        /**
+         * Component data
+         */
+        data() {
+            return {
+                nameItem:{
+                    value:null
+                },
+                latitudeItem:{
+                    value:null
+                },
+                longitudeItem:{
+                    value:null
+                },
+                placeholders:{
+                    name:"Lisbon",
+                    latitude:"123",
+                    longitude:"12356"
+                }
+                
+            }
+        },
+         /**
+         * Emits the product to the father component
+         */
+        emitCity(modal){
+            let cityDetails ={
+                name: this.nameItem.value,
+                latitude:this.latitude.value,
+                longitude:this.longitude.value
+            };
+            this.$emit('emitCity',cityDetails);
+        }
+    }
+</script>
+
+<style>
+    
+</style>
