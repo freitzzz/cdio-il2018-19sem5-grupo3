3a7ad88b3b74865ddf63261b33895046a1502518
António Sousa <1161371@isep.ipp.pt>
2019-01-10 23:33:46 +0000

    NotAuthorizedException is now correctly instantiated with the given message [re #15].

--- MYCM/backend/AuthorizationServices/exceptions/NotAuthorizedException.cs
+++ MYCM/backend/AuthorizationServices/exceptions/NotAuthorizedException.cs
@@ -1,25 +1,48 @@
 using System;
+using System.Runtime.Serialization;
 
-namespace backend.AuthorizationServices.exceptions{
-    public sealed class NotAuthorizedException:InvalidOperationException{
+namespace backend.AuthorizationServices.exceptions
+{
+
+    /// <summary>
+    /// Class representing the exception throw when a user is not authorized to perform a certain action.
+    /// </summary>
+    public class NotAuthorizedException : Exception
+    {
         /// <summary>
-        /// String with the authorization rejection reason
+        /// Creates a new instance of NotAuthorizedException.
         /// </summary>
-        public string reason{get;}
+        public NotAuthorizedException()
+        {
+        }
 
         /// <summary>
-        /// Integer with the authorization rejection request status code
+        /// Creates a new instance of NotAuthorizedException with a specified error message.
         /// </summary>
-        public int statusCode{get;}
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public NotAuthorizedException(string message) : base(message)
+        {
+        }
 
         /// <summary>
-        /// Builds a new NotAuthorizedException
+        /// Initializes a new instance of the NotAuthorizedException class with a specified error
+        /// message and a reference to the inner exception that is the cause of this exception.
         /// </summary>
-        /// <param name="reason">String with the authorization rejection reason</param>
-        /// <param name="statusCode">Integer with the authorization reject request status code</param>
-        public NotAuthorizedException(string reason,int statusCode){
-            this.reason=reason;
-            this.statusCode=statusCode;
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public NotAuthorizedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the NotAuthorizedException class with serialized data.
+        /// </summary>
+        /// <param name="info">The System.Runtime.Serialization.SerializationInfo that holds the serialized
+        /// object data about the exception being thrown</param>
+        /// <param name="context">The System.Runtime.Serialization.StreamingContext that contains contextual information
+        /// about the source or destination.</param>
+        protected NotAuthorizedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
         }
     }
 }
