51aac6a52f222b7e7ee2951fe6dc18266b90a59d
桜ちゃん <1160941@isep.ipp.pt>
2019-01-10 19:21:15 +0000

    Elaboration of table for city
    see #15 #39 #75

---
+++ website/src/components/logistsManagement/Cities/CitiesTable.vue
@@ -0,0 +1,56 @@
+<template>
+    <section>
+        <vuetable :api-mode="false" :data="data" :fields="columns">
+            <!-- <template slot="actions" slot-scope="props">
+                                    
+          <div class ="custom-actions">
+              <button
+              class ="btn-primary"
+              @click ="deleteCity(props.rowData.id)">
+              <b-icon icon="minus"></b-icon>
+              </button>
+          </div>
+</template>-->
+        </vuetable>
+        <b-modal>
+
+        </b-modal>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "CitiesTable",
+        data() {
+            return {
+                columns: [{
+                        name: "id",
+                        title: "City ID"
+                    },
+                    {
+                        name: "name",
+                        title: "Name"
+                    },
+                    {
+                        name: "longitude",
+                        title: "Longitude"
+                    },
+                    {
+                        name: "latitude",
+                        title: "Latitude"
+                    }
+                ],
+            }
+        },
+        /**
+         * Component properties
+         */
+        props: {
+            data: Array
+        }
+    }
+</script>
+
+<style>
+    
+</style>
